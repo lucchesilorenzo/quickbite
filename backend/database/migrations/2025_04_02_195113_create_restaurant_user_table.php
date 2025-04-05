5d1c7122f864fa05ede36c626e0faf8cef1ad64d
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-04-05 10:21:04 +0200

    feat: add restaurant_user pivot table

---
+++ backend/database/migrations/2025_04_02_195113_create_restaurant_user_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('restaurant_user', function (Blueprint $table) {
+            $table->foreignUuid('user_id')->constrained()->onCascadeDelete();
+            $table->foreignUuid('restaurant_id')->constrained()->onCascadeDelete();
+            $table->enum('role', ['OWNER', 'CO-OWNER', 'RIDER'])->default('OWNER');
+            $table->date('contract_start')->nullable();
+            $table->date('contract_end')->nullable();
+            $table->boolean('is_active')->default(true);
+
+            $table->primary(['user_id', 'restaurant_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('restaurant_user');
+    }
+};
