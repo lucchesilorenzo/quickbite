3fb3b6a99cc25e3771aefe222dc8d4daef8b697b
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-04-10 22:28:33 +0200

    refactor: update AppLayout

--- frontend/src/layouts/AppLayout.tsx
+++ frontend/src/layouts/AppLayout.tsx
@@ -1,10 +1,19 @@
-import { Box } from "@mui/material";
+import { Box, Stack } from "@mui/material";
 import { Outlet } from "react-router-dom";
+
+import Footer from "@/components/common/Footer";
+import Header from "@/components/common/Header";
 
 export default function AppLayout() {
   return (
-    <Box component="main">
-      <Outlet />
-    </Box>
+    <Stack sx={{ minHeight: "100vh" }}>
+      <Header />
+
+      <Box component="main">
+        <Outlet />
+      </Box>
+
+      <Footer />
+    </Stack>
   );
 }
