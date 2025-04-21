10f969ab2c72160389df1383571c84d351726602
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-04-21 12:03:44 +0200

    feat: display dynamic title in area page

--- frontend/src/pages/AreaPage.tsx
+++ frontend/src/pages/AreaPage.tsx
@@ -1,9 +1,22 @@
+import { useEffect } from "react";
+
 import { Box } from "@mui/material";
 import { useCookies } from "react-cookie";
 
 export default function AreaPage() {
   const [cookie] = useCookies(["address"]);
-  console.log(cookie.address.address?.house_number);
+
+  const city = cookie.address.address?.city;
+  const postalCode = cookie.address.address?.postcode;
+
+  const displayName =
+    !city || !postalCode
+      ? cookie.address.display_name
+      : `${city}, ${postalCode}`;
+
+  useEffect(() => {
+    document.title = `Restaurants and takeaways in ${displayName} | QuickBite`;
+  }, [displayName]);
 
   return <Box component="main">AreaPage</Box>;
 }
