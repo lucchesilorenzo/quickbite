3a99146c6f330b0bf937a5b8989f402bd58f01c6
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-04-25 16:07:27 +0200

    feat: add HeadingWithTooltip component

---
+++ frontend/src/components/common/HeadingWithTooltip.tsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+
+import InfoOutlineIcon from "@mui/icons-material/InfoOutline";
+import {
+  ClickAwayListener,
+  IconButton,
+  Stack,
+  Tooltip,
+  TooltipProps,
+  Typography,
+} from "@mui/material";
+
+type HeadingWithTooltipProps = {
+  headingText: string;
+  tooltipMessage: string;
+  placement?: TooltipProps["placement"];
+};
+
+export default function HeadingWithTooltip({
+  headingText,
+  tooltipMessage,
+  placement = "top",
+}: HeadingWithTooltipProps) {
+  const [showTooltip, setShowTooltip] = useState(false);
+
+  return (
+    <Stack direction="row" spacing={1} sx={{ alignItems: "center" }}>
+      <Typography component="span" variant="h6" sx={{ fontWeight: "700" }}>
+        {headingText}
+      </Typography>
+
+      <ClickAwayListener onClickAway={() => setShowTooltip(false)}>
+        <Tooltip
+          placement={placement}
+          title={tooltipMessage}
+          open={showTooltip}
+          onClose={() => setShowTooltip(false)}
+          disableFocusListener
+          disableHoverListener
+          disableTouchListener
+        >
+          <IconButton
+            color="primary"
+            onClick={() => setShowTooltip(!showTooltip)}
+          >
+            <InfoOutlineIcon />
+          </IconButton>
+        </Tooltip>
+      </ClickAwayListener>
+    </Stack>
+  );
+}
