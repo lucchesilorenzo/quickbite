6103df4f691907cc9fc535208c35583e2478ae69
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-04-28 14:48:21 +0200

    feat: add UserSeeder, CategorySeeder, and RestaurantSeeder to DatabaseSeeder

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -2,8 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(CategorySeeder::class);
+        $this->call([
+            UserSeeder::class,
+            CategorySeeder::class,
+            RestaurantSeeder::class,
+        ]);
     }
 }
