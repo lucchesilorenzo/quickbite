39bef0cf90f2c67bf5244b552feff87aa297b883
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-04-28 14:42:32 +0200

    feat: add configure and getMenuItemsForCategory methods, update MenuCategoryFactory definition

--- backend/database/factories/MenuCategoryFactory.php
+++ backend/database/factories/MenuCategoryFactory.php
@@ -2,7 +2,7 @@
 
 namespace Database\Factories;
 
-use App\Models\Restaurant;
+use App\Models\MenuCategory;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,10 +17,151 @@ class MenuCategoryFactory extends Factory
      */
     public function definition(): array
     {
-        return [
-            'restaurant_id' => Restaurant::inRandomOrder()->first()->id,
-            'name' => fake()->words(2, true),
-            'order' => fake()->numberBetween(0, 5),
+        static $menuCategories = [
+            'Starters',
+            'Main Courses',
+            'Desserts',
+            'Drinks',
+            'Sides',
+            'Salads',
         ];
+
+        static $i = 1;
+
+        return [
+            'name' => $menuCategories[$i % count($menuCategories)],
+            'order' => $i++,
+        ];
+    }
+
+    /**
+     * Configure the model factory.
+     *
+     * @return self
+     */
+    public function configure(): self
+    {
+        return $this->afterCreating(function (MenuCategory $menuCategory) {
+            $menuItems = $this->getMenuItemsForCategory($menuCategory->name);
+
+            $menuCategory->menuItems()->createMany($menuItems);
+        });
+    }
+
+    /**
+     * Get the menu items for the menu category.
+     *
+     * @param string $categoryName
+     * @return array
+     */
+    private function getMenuItemsForCategory(string $categoryName): array
+    {
+        $menuItems = [];
+
+        switch ($categoryName) {
+            case 'Starters':
+                $menuItems = [
+                    [
+                        'name' => 'Bruschetta',
+                        'description' => 'Toasted bread with tomatoes and basil',
+                        'price' => 5.99,
+                        'is_available' => true,
+                    ],
+                    [
+                        'name' => 'Garlic Bread',
+                        'description' => 'Garlic butter spread on warm bread',
+                        'price' => 4.50,
+                        'is_available' => true,
+                    ],
+                ];
+                break;
+
+            case 'Main Courses':
+                $menuItems = [
+                    [
+                        'name' => 'Lasagna',
+                        'description' => 'Classic Italian lasagna with beef and ricotta',
+                        'price' => 12.99,
+                        'is_available' => true,
+                    ],
+                    [
+                        'name' => 'Grilled Chicken',
+                        'description' => 'Tender grilled chicken with seasonal vegetables',
+                        'price' => 15.50,
+                        'is_available' => true,
+                    ],
+                ];
+                break;
+
+            case 'Desserts':
+                $menuItems = [
+                    [
+                        'name' => 'Tiramisu',
+                        'description' => 'Coffee-flavored Italian dessert',
+                        'price' => 6.50,
+                        'is_available' => true,
+                    ],
+                    [
+                        'name' => 'Panna Cotta',
+                        'description' => 'Creamy Italian custard topped with berries',
+                        'price' => 5.00,
+                        'is_available' => true,
+                    ],
+                ];
+                break;
+
+            case 'Drinks':
+                $menuItems = [
+                    [
+                        'name' => 'Espresso',
+                        'description' => 'Strong Italian coffee',
+                        'price' => 2.50,
+                        'is_available' => true,
+                    ],
+                    [
+                        'name' => 'Lemonade',
+                        'description' => 'Refreshing lemon drink',
+                        'price' => 3.00,
+                        'is_available' => true,
+                    ],
+                ];
+                break;
+
+            case 'Sides':
+                $menuItems = [
+                    [
+                        'name' => 'French Fries',
+                        'description' => 'Crispy fried potatoes',
+                        'price' => 3.99,
+                        'is_available' => true,
+                    ],
+                    [
+                        'name' => 'Coleslaw',
+                        'description' => 'Creamy cabbage salad',
+                        'price' => 4.20,
+                        'is_available' => true,
+                    ],
+                ];
+                break;
+
+            case 'Salads':
+                $menuItems = [
+                    [
+                        'name' => 'Caesar Salad',
+                        'description' => 'Romaine lettuce with Caesar dressing',
+                        'price' => 6.99,
+                        'is_available' => true,
+                    ],
+                    [
+                        'name' => 'Greek Salad',
+                        'description' => 'Cucumbers, tomatoes, and feta cheese',
+                        'price' => 7.50,
+                        'is_available' => true,
+                    ],
+                ];
+                break;
+        }
+
+        return $menuItems;
     }
 }
