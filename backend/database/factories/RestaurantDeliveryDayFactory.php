47848cfb98c2cab803f457aae33dc6584776be8d
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-04-28 14:38:41 +0200

    feat: add fullWeek method to RestaurantDeliveryDayFactory

--- backend/database/factories/RestaurantDeliveryDayFactory.php
+++ backend/database/factories/RestaurantDeliveryDayFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\Restaurant;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Database\Eloquent\Factories\Sequence;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\RestaurantDeliveryDay>
@@ -20,8 +21,26 @@ class RestaurantDeliveryDayFactory extends Factory
         return [
             'restaurant_id' => Restaurant::inRandomOrder()->first()->id,
             'day' => fake()->randomElement(['MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY', 'SUNDAY']),
-            'start_time' => fake()->time(),
-            'end_time' => fake()->time(),
+            'start_time' => fake()->time('H:i'),
+            'end_time' => fake()->time('H:i'),
         ];
+    }
+
+    /**
+     * Indicate that the model should be full week.
+     *
+     * @return self
+     */
+    public function fullWeek(): self
+    {
+        return $this->state(new Sequence(
+            ['day' => 'MONDAY', 'start_time' => '10:00:00', 'end_time' => '22:00:00'],
+            ['day' => 'TUESDAY', 'start_time' => '10:00:00', 'end_time' => '22:00:00'],
+            ['day' => 'WEDNESDAY', 'start_time' => '10:00:00', 'end_time' => '22:00:00'],
+            ['day' => 'THURSDAY', 'start_time' => '10:00:00', 'end_time' => '22:00:00'],
+            ['day' => 'FRIDAY', 'start_time' => '10:00:00', 'end_time' => '23:00:00'],
+            ['day' => 'SATURDAY', 'start_time' => '10:00:00', 'end_time' => '23:00:00'],
+            ['day' => 'SUNDAY', 'start_time' => null, 'end_time' => null],
+        ));
     }
 }
