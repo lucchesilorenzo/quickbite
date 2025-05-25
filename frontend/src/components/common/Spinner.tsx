271ed9e4d0e4df6b6de748cd7fa7f9542026c3ca
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-05-25 11:20:08 +0200

    feat: add spinner component

---
+++ frontend/src/components/common/Spinner.tsx
@@ -0,0 +1,15 @@
+import { CircularProgress, Stack } from "@mui/material";
+
+export default function Spinner() {
+  return (
+    <Stack
+      sx={{
+        alignItems: "center",
+        justifyContent: "center",
+        minHeight: "60vh",
+      }}
+    >
+      <CircularProgress />
+    </Stack>
+  );
+}
