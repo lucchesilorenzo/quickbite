bda4546daa1b95c783800857ac331548ce3e0ae5
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-06-06 10:52:09 +0200

    feat: add truncateWords helper

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -8,3 +8,11 @@ export function formatCurrency(price: number) {
 export function capitalize(text: string) {
   return text[0].toUpperCase() + text.slice(1).toLowerCase();
 }
+
+export function truncateWords(text: string, wordCount: number) {
+  const words = text.split(" ");
+
+  return words.length > wordCount
+    ? words.slice(0, wordCount).join(" ") + "..."
+    : text;
+}
