7228ace7ba84dc0b10d297083f4134109630eba4
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-06-09 12:34:16 +0200

    feat: add MultiCartProvider with initial isEmpty and addItem utils

---
+++ frontend/src/contexts/MultiCartProvider.tsx
@@ -0,0 +1,98 @@
+import { createContext, useEffect, useState } from "react";
+
+import { MenuItem } from "@/types";
+import { Cart, RestaurantCart } from "@/types/cart-types";
+
+type MultiCartProviderProps = {
+  children: React.ReactNode;
+};
+
+type MultiCartContext = {
+  isEmpty: (restaurantId: string) => boolean;
+  addItem: (restaurantId: string, menuItem: MenuItem, quantity: number) => void;
+};
+
+const initialState: RestaurantCart = {
+  items: [],
+  totalItems: 0,
+  totalUniqueItems: 0,
+  cartTotal: 0,
+};
+
+export const MultiCartContext = createContext<MultiCartContext | null>(null);
+
+export default function MultiCartProvider({
+  children,
+}: MultiCartProviderProps) {
+  const [carts, setCarts] = useState<Cart>(() => {
+    const stored = localStorage.getItem("carts");
+    return stored ? JSON.parse(stored) : {};
+  });
+
+  useEffect(() => {
+    localStorage.setItem("carts", JSON.stringify(carts));
+  }, [carts]);
+
+  function isEmpty(restaurantId: string) {
+    return !carts[restaurantId] || carts[restaurantId].items.length === 0;
+  }
+
+  function addItem(restaurantId: string, menuItem: MenuItem, quantity: number) {
+    setCarts((prev) => {
+      const existingCart = prev[restaurantId] ?? initialState;
+
+      const existingItem = existingCart.items.some(
+        (item) => item.id === menuItem.id,
+      );
+
+      const updatedItems = existingItem
+        ? existingCart.items.map((item) =>
+            item.id === menuItem.id
+              ? {
+                  ...item,
+                  quantity: item.quantity + quantity,
+                  item_total: Number(
+                    ((item.quantity + quantity) * item.price).toFixed(2),
+                  ),
+                }
+              : item,
+          )
+        : [
+            ...existingCart.items,
+            {
+              ...menuItem,
+              quantity,
+              item_total: Number((quantity * menuItem.price).toFixed(2)),
+            },
+          ];
+
+      const totalItems = updatedItems.reduce(
+        (acc, curr) => acc + curr.quantity,
+        0,
+      );
+
+      const cartTotal = updatedItems.reduce(
+        (acc, curr) => acc + curr.item_total,
+        0,
+      );
+
+      const totalUniqueItems = updatedItems.length;
+
+      return {
+        ...prev,
+        [restaurantId]: {
+          items: updatedItems,
+          totalItems,
+          totalUniqueItems,
+          cartTotal: Number(cartTotal.toFixed(2)),
+        },
+      };
+    });
+  }
+
+  return (
+    <MultiCartContext.Provider value={{ isEmpty, addItem }}>
+      {children}
+    </MultiCartContext.Provider>
+  );
+}
