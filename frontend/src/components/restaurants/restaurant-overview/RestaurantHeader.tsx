9624b06657651a7556239337bb86b8cc5c15ca58
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-06-11 21:39:41 +0200

    style: make RestaurantHeader responsive

--- frontend/src/components/restaurants/restaurant-overview/RestaurantHeader.tsx
+++ frontend/src/components/restaurants/restaurant-overview/RestaurantHeader.tsx
@@ -1,5 +1,5 @@
 import InfoOutlineIcon from "@mui/icons-material/InfoOutline";
-import { IconButton, Stack, Typography } from "@mui/material";
+import { IconButton, Stack, Typography, useMediaQuery } from "@mui/material";
 import { grey } from "@mui/material/colors";
 
 import RestaurantAboutDialog from "./RestaurantAboutDialog";
@@ -7,6 +7,8 @@ import RestaurantAboutDialog from "./RestaurantAboutDialog";
 import { useSingleRestaurant } from "@/hooks/contexts/useSingleRestaurant";
 
 export default function RestaurantHeader() {
+  const isMobile = useMediaQuery((theme) => theme.breakpoints.down("lg"));
+
   const {
     restaurant,
     openRestaurantAboutDialog,
@@ -18,7 +20,11 @@ export default function RestaurantHeader() {
       direction="row"
       sx={{ justifyContent: "space-between", alignItems: "center", mb: 1 }}
     >
-      <Typography component="h1" variant="h4" sx={{ fontWeight: 700 }}>
+      <Typography
+        component="h1"
+        variant={isMobile ? "h5" : "h4"}
+        sx={{ fontWeight: 700 }}
+      >
         {restaurant.name}
       </Typography>
 
@@ -26,8 +32,9 @@ export default function RestaurantHeader() {
         color="inherit"
         sx={{ bgcolor: grey[100], "&:hover": { bgcolor: grey[200] } }}
         onClick={() => setOpenRestaurantAboutDialog(true)}
+        size={isMobile ? "small" : "medium"}
       >
-        <InfoOutlineIcon />
+        <InfoOutlineIcon fontSize={isMobile ? "small" : "medium"} />
       </IconButton>
 
       <RestaurantAboutDialog
