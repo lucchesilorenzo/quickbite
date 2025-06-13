306ed7c6cfba6abfa259a50c1cc38e0358c0890e
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-06-13 14:44:01 +0200

    feat: add RestaurantHeader component for restaurant page navigation

---
+++ frontend/src/components/restaurants/RestaurantHeader.tsx
@@ -0,0 +1,32 @@
+import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
+import RestaurantMenuIcon from "@mui/icons-material/RestaurantMenu";
+import { AppBar, IconButton, Stack, Toolbar } from "@mui/material";
+import { Link, useNavigate } from "react-router-dom";
+
+import HeaderDialog from "../common/HeaderDialog";
+
+export default function RestaurantHeader() {
+  const navigate = useNavigate();
+
+  return (
+    <AppBar position="relative" id="back-to-top" color="inherit" elevation={3}>
+      <Toolbar sx={{ justifyContent: "space-between" }}>
+        <Stack
+          direction="row"
+          spacing={1}
+          component={Link}
+          to="/"
+          sx={{ alignItems: "center", textDecoration: "none" }}
+        >
+          <IconButton onClick={() => navigate(-1)}>
+            <KeyboardArrowLeftIcon color="primary" />
+          </IconButton>
+
+          <RestaurantMenuIcon color="primary" />
+        </Stack>
+
+        <HeaderDialog />
+      </Toolbar>
+    </AppBar>
+  );
+}
