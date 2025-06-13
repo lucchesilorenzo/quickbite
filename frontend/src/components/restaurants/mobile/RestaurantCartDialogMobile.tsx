a5345b3f4a6c307524ad535aac136380ec211eb7
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-06-13 11:34:59 +0200

    feat: add RestaurantCartDialogMobile component for mobile cart functionality

---
+++ frontend/src/components/restaurants/mobile/RestaurantCartDialogMobile.tsx
@@ -0,0 +1,52 @@
+import CloseIcon from "@mui/icons-material/Close";
+import {
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  IconButton,
+  Stack,
+} from "@mui/material";
+
+import RestaurantCartFooter from "../restaurant-cart/RestaurantCartFooter";
+import RestaurantCartList from "../restaurant-cart/RestaurantCartList";
+
+type RestaurantCartDialogMobileProps = {
+  openRestaurantCartDialogMobile: boolean;
+  setOpenRestaurantCartDialogMobile: React.Dispatch<
+    React.SetStateAction<boolean>
+  >;
+};
+
+export default function RestaurantCartDialogMobile({
+  openRestaurantCartDialogMobile,
+  setOpenRestaurantCartDialogMobile,
+}: RestaurantCartDialogMobileProps) {
+  return (
+    <Dialog
+      open={openRestaurantCartDialogMobile}
+      onClose={() => setOpenRestaurantCartDialogMobile(false)}
+      fullScreen
+      disableRestoreFocus
+    >
+      <Stack spacing={2} sx={{ p: 2 }}>
+        <Stack direction="row" sx={{ justifyContent: "space-between" }}>
+          <DialogTitle sx={{ p: 0, fontWeight: 700 }}>Cart</DialogTitle>
+
+          <IconButton
+            color="inherit"
+            aria-label="close"
+            onClick={() => setOpenRestaurantCartDialogMobile(false)}
+            sx={{ p: 0 }}
+          >
+            <CloseIcon />
+          </IconButton>
+        </Stack>
+
+        <DialogContent sx={{ p: 0 }}>
+          <RestaurantCartList />
+          <RestaurantCartFooter />
+        </DialogContent>
+      </Stack>
+    </Dialog>
+  );
+}
--- frontend/src/components/restaurants/mobile/RestaurantCartMobile.tsx
+++ frontend/src/components/restaurants/mobile/RestaurantCartMobile.tsx
@@ -1,5 +1,9 @@
+import { useState } from "react";
+
 import ShoppingBasketOutlinedIcon from "@mui/icons-material/ShoppingBasketOutlined";
 import { Badge, Button, Paper } from "@mui/material";
+
+import RestaurantCartDialogMobile from "./RestaurantCartDialogMobile";
 
 import { useMultiCart } from "@/hooks/contexts/useMultiCart";
 import { useSingleRestaurant } from "@/hooks/contexts/useSingleRestaurant";
@@ -9,40 +13,51 @@ export default function RestaurantCartMobile() {
   const { restaurant } = useSingleRestaurant();
   const { cartTotal, totalItems } = useMultiCart();
 
+  const [openRestaurantCartDialogMobile, setOpenRestaurantCartDialogMobile] =
+    useState(false);
+
   const subtotal = cartTotal(restaurant.id);
   const total = subtotal + restaurant.shipping_cost;
 
   return (
-    <Paper
-      sx={{
-        position: "fixed",
-        bottom: 0,
-        left: 0,
-        right: 0,
-        p: 2,
-        zIndex: 1300,
-      }}
-    >
-      <Button
-        variant="contained"
-        size="large"
-        fullWidth
-        sx={{ fontWeight: 700, fontSize: 16, p: 1.5 }}
+    <>
+      <Paper
+        sx={{
+          position: "fixed",
+          bottom: 0,
+          left: 0,
+          right: 0,
+          p: 2,
+          zIndex: 1300,
+        }}
       >
-        <Badge
-          badgeContent={totalItems(restaurant.id)}
-          anchorOrigin={{ vertical: "top", horizontal: "left" }}
-          sx={{
-            "& .MuiBadge-badge": {
-              backgroundColor: "#212121",
-              color: "#fff",
-            },
-          }}
+        <Button
+          variant="contained"
+          size="large"
+          fullWidth
+          sx={{ fontWeight: 700, fontSize: 16, p: 1.5 }}
+          onClick={() => setOpenRestaurantCartDialogMobile(true)}
         >
-          <ShoppingBasketOutlinedIcon sx={{ mr: 1 }} />
-        </Badge>
-        Cart ({formatCurrency(total)})
-      </Button>
-    </Paper>
+          <Badge
+            badgeContent={totalItems(restaurant.id)}
+            anchorOrigin={{ vertical: "top", horizontal: "left" }}
+            sx={{
+              "& .MuiBadge-badge": {
+                backgroundColor: "#212121",
+                color: "#fff",
+              },
+            }}
+          >
+            <ShoppingBasketOutlinedIcon sx={{ mr: 1 }} />
+          </Badge>
+          Cart ({formatCurrency(total)})
+        </Button>
+      </Paper>
+
+      <RestaurantCartDialogMobile
+        openRestaurantCartDialogMobile={openRestaurantCartDialogMobile}
+        setOpenRestaurantCartDialogMobile={setOpenRestaurantCartDialogMobile}
+      />
+    </>
   );
 }
