74cd310d241eb4d297c050e6761e2260861e78f4
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-06-13 14:44:07 +0200

    feat: add RestaurantFooter component for restaurant page layout

---
+++ frontend/src/components/restaurants/RestaurantFooter.tsx
@@ -0,0 +1,71 @@
+import {
+  Container,
+  List,
+  ListItem,
+  ListItemText,
+  Stack,
+  Typography,
+  useMediaQuery,
+} from "@mui/material";
+import { grey } from "@mui/material/colors";
+import { Link } from "react-router-dom";
+
+import { useMultiCart } from "@/hooks/contexts/useMultiCart";
+import { useSingleRestaurant } from "@/hooks/contexts/useSingleRestaurant";
+import { footerLinks } from "@/lib/data";
+
+export default function RestaurantFooter() {
+  const isMobile = useMediaQuery((theme) => theme.breakpoints.down("md"));
+
+  const { restaurant } = useSingleRestaurant();
+  const { isEmpty } = useMultiCart();
+
+  return (
+    <Stack
+      id="footer"
+      component="footer"
+      sx={{
+        mt: "auto",
+        bgcolor: grey[200],
+        pb: isMobile && !isEmpty(restaurant.id) ? 10 : "",
+      }}
+    >
+      <Container>
+        <List
+          sx={{
+            display: "flex",
+            flexDirection: { xs: "column", md: "row" },
+            justifyContent: "space-between",
+          }}
+        >
+          {footerLinks.map((link) => (
+            <ListItem
+              disableGutters
+              key={link.href}
+              component={Link}
+              to={link.href}
+              sx={{
+                width: "fit-content",
+                textDecoration: "none",
+                color: "inherit",
+                "&:hover": { textDecoration: "underline" },
+              }}
+            >
+              <ListItemText
+                primary={<Typography variant="body2">{link.label}</Typography>}
+              />
+            </ListItem>
+          ))}
+        </List>
+
+        <Typography
+          variant="body2"
+          sx={{ textAlign: { md: "center" } }}
+          gutterBottom
+        >
+          &copy; {new Date().getFullYear()} QuickBite
+        </Typography>
+      </Container>
+    </Stack>
+  );
+}
