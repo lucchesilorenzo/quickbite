abe842cea1e4064af112bb44e607277024eba08f
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-06-18 22:35:38 +0200

    feat: add navigation on successful customer registration

--- frontend/src/hooks/react-query/private/customers/auth/useRegisterCustomer.ts
+++ frontend/src/hooks/react-query/private/customers/auth/useRegisterCustomer.ts
@@ -1,15 +1,20 @@
 import { useMutation } from "@tanstack/react-query";
 import { useNotifications } from "@toolpad/core/useNotifications";
+import { useNavigate } from "react-router-dom";
 
 import { postData } from "@/lib/api-client";
 import { TCustomerRegisterFormSchema } from "@/validations/customer-auth-validations";
 
 export function useRegisterCustomer() {
   const notifications = useNotifications();
+  const navigate = useNavigate();
 
   return useMutation({
     mutationFn: (data: TCustomerRegisterFormSchema) =>
       postData("/customer/auth/register", data),
+    onSuccess: () => {
+      navigate("/");
+    },
     onError: (error) => {
       notifications.show(error.message, {
         key: "register-customer-error",
