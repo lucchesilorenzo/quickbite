990c23ce59867e89184590a0be9f52202f4ec376
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-06-19 22:30:20 +0200

    feat: implement useUpdateCustomerProfile hook for updating customer profile data

---
+++ frontend/src/hooks/react-query/private/customers/useUpdateCustomerProfile.ts
@@ -0,0 +1,37 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useNotifications } from "@toolpad/core/useNotifications";
+
+import { updateData } from "@/lib/api-client";
+import {
+  TPersonalInfoEditEmailForm,
+  TPersonalInfoEditFullNameForm,
+  TPersonalInfoEditPhoneNumberForm,
+} from "@/validations/personal-info-validations";
+
+type PersonalInfoField =
+  | TPersonalInfoEditFullNameForm
+  | TPersonalInfoEditEmailForm
+  | TPersonalInfoEditPhoneNumberForm;
+
+export function useUpdateCustomerProfile() {
+  const queryClient = useQueryClient();
+  const notifications = useNotifications();
+
+  return useMutation({
+    mutationFn: (data: PersonalInfoField) =>
+      updateData("/customer/profile", data),
+    onSuccess: (response) => {
+      queryClient.invalidateQueries({ queryKey: ["auth"] });
+      notifications.show(response.message, {
+        key: "update-customer-profile-success",
+        severity: "success",
+      });
+    },
+    onError: (error) => {
+      notifications.show(error.message, {
+        key: "update-customer-profile-error",
+        severity: "error",
+      });
+    },
+  });
+}
