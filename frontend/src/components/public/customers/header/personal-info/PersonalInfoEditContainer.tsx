06c379573604c420fc0ffe2e85b6ebcd942b6f86
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-06-19 21:35:55 +0200

    feat: implement PersonalInfoEditEmailForm for user email editing

--- frontend/src/components/public/customers/header/personal-info/PersonalInfoEditContainer.tsx
+++ frontend/src/components/public/customers/header/personal-info/PersonalInfoEditContainer.tsx
@@ -1,5 +1,13 @@
+import { Stack } from "@mui/material";
+
+import PersonalInfoEditEmailForm from "./PersonalInfoEditEmailForm";
 import PersonalInfoEditFullNameForm from "./PersonalInfoEditFullNameForm";
 
 export default function PersonalInfoEditContainer() {
-  return <PersonalInfoEditFullNameForm />;
+  return (
+    <Stack spacing={4}>
+      <PersonalInfoEditFullNameForm />
+      <PersonalInfoEditEmailForm />
+    </Stack>
+  );
 }
---
+++ frontend/src/components/public/customers/header/personal-info/PersonalInfoEditEmailForm.tsx
@@ -0,0 +1,74 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button, Stack, TextField } from "@mui/material";
+import { Controller, useForm } from "react-hook-form";
+
+import { FormHelperTextError } from "@/components/common/FormHelperTextError";
+import { useAuth } from "@/hooks/contexts/useAuth";
+import { isCustomer } from "@/lib/utils";
+import {
+  TPersonalInfoEditEmailForm,
+  personalInfoEditEmailForm,
+} from "@/validations/personal-info-validations";
+
+export default function PersonalInfoEditEmailForm() {
+  const { user } = useAuth();
+
+  const {
+    handleSubmit,
+    control,
+    formState: { isSubmitting, errors, isDirty },
+  } = useForm({
+    resolver: zodResolver(personalInfoEditEmailForm),
+    defaultValues: {
+      email: isCustomer(user) ? user.email : "",
+    },
+  });
+
+  async function onSubmit(data: TPersonalInfoEditEmailForm) {
+    console.log(data);
+  }
+
+  return (
+    <Stack
+      component="form"
+      autoComplete="off"
+      noValidate
+      spacing={2}
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <Controller
+        name="email"
+        control={control}
+        render={({ field }) => (
+          <TextField
+            {...field}
+            type="email"
+            autoComplete="off"
+            required
+            label="Email address"
+            error={!!errors.email}
+            helperText={
+              errors.email?.message && (
+                <FormHelperTextError message={errors.email.message} />
+              )
+            }
+            fullWidth
+            sx={{ minWidth: 150 }}
+          />
+        )}
+      />
+
+      {isDirty && (
+        <Button
+          type="submit"
+          disabled={isSubmitting}
+          loading={isSubmitting}
+          loadingIndicator="Editing..."
+          variant="contained"
+        >
+          Edit email
+        </Button>
+      )}
+    </Stack>
+  );
+}
