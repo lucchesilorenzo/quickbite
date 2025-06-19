90964216363f20d7800f3d297500d51c58227978
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-06-19 21:23:31 +0200

    feat: add validation schemas for personal info edit full name form

---
+++ frontend/src/validations/personal-info-validations.ts
@@ -0,0 +1,48 @@
+import { matchIsValidTel } from "mui-tel-input";
+import { z } from "zod/v4";
+
+export const editPersonalInfoForm = z.object({
+  first_name: z
+    .string()
+    .trim()
+    .min(1, "Please fill out your first name.")
+    .max(50, "First name is too long."),
+  last_name: z
+    .string()
+    .trim()
+    .min(1, "Please fill out your last name.")
+    .max(50, "Last name is too long."),
+  email: z
+    .email({ error: "Please enter a valid email address." })
+    .min(1, "Email is required."),
+  phone_number: z
+    .string()
+    .trim()
+    .min(1, "Please fill out your phone number.")
+    .refine(
+      (phone_number) =>
+        matchIsValidTel(phone_number, { onlyCountries: ["IT"] }),
+      {
+        error: "Please enter a valid phone number.",
+      },
+    ),
+});
+
+export const personalInfoEditFullNameForm = z.object({
+  first_name: z
+    .string()
+    .trim()
+    .min(1, "Please fill out your first name.")
+    .max(50, "First name is too long."),
+  last_name: z
+    .string()
+    .trim()
+    .min(1, "Please fill out your last name.")
+    .max(50, "Last name is too long."),
+});
+
+export type TEditPersonalInfoForm = z.infer<typeof editPersonalInfoForm>;
+
+export type TPersonalInfoEditFullNameForm = z.infer<
+  typeof personalInfoEditFullNameForm
+>;
