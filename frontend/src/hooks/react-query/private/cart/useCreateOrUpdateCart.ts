70cca92d67674d8f1d42812d530cb51964d0a923
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-06-20 19:38:36 +0200

    feat: implement useCreateOrUpdateCart hook for managing cart creation and updates

---
+++ frontend/src/hooks/react-query/private/cart/useCreateOrUpdateCart.ts
@@ -0,0 +1,23 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useNotifications } from "@toolpad/core/useNotifications";
+
+import { postData } from "@/lib/api-client";
+import { RestaurantCart } from "@/types";
+
+export function useCreateOrUpdateCart() {
+  const queryClient = useQueryClient();
+  const notifications = useNotifications();
+
+  return useMutation({
+    mutationFn: (data: RestaurantCart) => postData("/carts", data),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["carts"] });
+    },
+    onError: () => {
+      notifications.show("Failed to create or update cart.", {
+        key: "create-or-update-cart-error",
+        severity: "error",
+      });
+    },
+  });
+}
