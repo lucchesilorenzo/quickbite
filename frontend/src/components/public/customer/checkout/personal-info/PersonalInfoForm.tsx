9a292b27fa6e8d1a671ebed160aa00af2990e43b
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-06-21 22:08:57 +0200

    feat: add initial PersonalInfoForm component

---
+++ frontend/src/components/public/customer/checkout/personal-info/PersonalInfoForm.tsx
@@ -0,0 +1,116 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button, Stack, TextField } from "@mui/material";
+import { MuiTelInput } from "mui-tel-input";
+import { Controller, useForm } from "react-hook-form";
+
+import { FormHelperTextError } from "@/components/common/FormHelperTextError";
+import { useAuth } from "@/hooks/contexts/useAuth";
+import { isCustomer } from "@/lib/utils";
+import {
+  TCheckoutPersonalInfoForm,
+  checkoutPersonalInfoForm,
+} from "@/validations/checkout-validations";
+
+export default function PersonalInfoForm() {
+  const { user } = useAuth();
+
+  const {
+    handleSubmit,
+    control,
+    formState: { isSubmitting, errors },
+  } = useForm({
+    resolver: zodResolver(checkoutPersonalInfoForm),
+    defaultValues: {
+      first_name: isCustomer(user) ? user.first_name : "",
+      last_name: isCustomer(user) ? user.last_name : "",
+      phone_number: isCustomer(user) ? user.phone_number : "",
+    },
+  });
+
+  async function onSubmit(data: TCheckoutPersonalInfoForm) {
+    console.log(data);
+  }
+
+  return (
+    <Stack
+      component="form"
+      autoComplete="off"
+      noValidate
+      spacing={4}
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <Controller
+        name="first_name"
+        control={control}
+        render={({ field }) => (
+          <TextField
+            {...field}
+            required
+            autoComplete="off"
+            label="First name"
+            error={!!errors.first_name}
+            helperText={
+              errors.first_name?.message && (
+                <FormHelperTextError message={errors.first_name.message} />
+              )
+            }
+            fullWidth
+            sx={{ minWidth: 150 }}
+          />
+        )}
+      />
+
+      <Controller
+        name="last_name"
+        control={control}
+        render={({ field }) => (
+          <TextField
+            {...field}
+            required
+            label="Last name"
+            error={!!errors.last_name}
+            helperText={
+              errors.last_name?.message && (
+                <FormHelperTextError message={errors.last_name.message} />
+              )
+            }
+            fullWidth
+            sx={{ minWidth: 150 }}
+          />
+        )}
+      />
+
+      <Controller
+        name="phone_number"
+        control={control}
+        render={({ field }) => (
+          <MuiTelInput
+            {...field}
+            required
+            label="Phone number"
+            defaultCountry="IT"
+            onlyCountries={["IT"]}
+            forceCallingCode
+            disableDropdown
+            error={!!errors.phone_number}
+            helperText={
+              errors.phone_number?.message && (
+                <FormHelperTextError message={errors.phone_number.message} />
+              )
+            }
+          />
+        )}
+      />
+
+      <Button
+        type="submit"
+        disabled={isSubmitting}
+        loading={isSubmitting}
+        loadingIndicator="Saving..."
+        variant="contained"
+      >
+        Save
+      </Button>
+    </Stack>
+  );
+}
