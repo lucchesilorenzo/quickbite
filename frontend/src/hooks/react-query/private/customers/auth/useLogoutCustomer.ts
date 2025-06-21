de6029778c7d60332a1c41c3ee5a98a8d086da79
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-06-21 18:27:06 +0200

    refactor: remove emptyAllCarts from useLogoutCustomer hook

--- frontend/src/hooks/react-query/private/customers/auth/useLogoutCustomer.ts
+++ frontend/src/hooks/react-query/private/customers/auth/useLogoutCustomer.ts
@@ -2,15 +2,12 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useNotifications } from "@toolpad/core/useNotifications";
 import { useNavigate } from "react-router-dom";
 
-import { useMultiCart } from "@/hooks/contexts/useMultiCart";
 import { postData } from "@/lib/api-client";
 
 export function useLogoutCustomer() {
   const queryClient = useQueryClient();
   const notifications = useNotifications();
   const navigate = useNavigate();
-
-  const { emptyAllCarts } = useMultiCart();
 
   return useMutation({
     mutationFn: () => postData("/customer/auth/logout"),
@@ -19,7 +16,6 @@ export function useLogoutCustomer() {
       localStorage.removeItem("token");
 
       navigate("/customer/auth/login");
-      emptyAllCarts();
     },
     onError: (error) => {
       notifications.show(error.message, {
