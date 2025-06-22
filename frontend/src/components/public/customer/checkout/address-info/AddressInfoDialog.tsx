2abd6b29bf7cf108fd56507b0602a9b5aadfc0fa
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-06-22 10:48:58 +0200

    feat: add AddressInfoDialog component for editing address information

---
+++ frontend/src/components/public/customer/checkout/address-info/AddressInfoDialog.tsx
@@ -0,0 +1,50 @@
+import CloseIcon from "@mui/icons-material/Close";
+import {
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  IconButton,
+  Stack,
+} from "@mui/material";
+
+import AddressInfoForm from "./AddressInfoForm";
+
+type AddressInfoDialogProps = {
+  openAddressInfoDialog: boolean;
+  setOpenAddressInfoDialog: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+export default function AddressInfoDialog({
+  openAddressInfoDialog,
+  setOpenAddressInfoDialog,
+}: AddressInfoDialogProps) {
+  return (
+    <Dialog
+      open={openAddressInfoDialog}
+      onClose={() => setOpenAddressInfoDialog(false)}
+      fullWidth
+      disableRestoreFocus
+    >
+      <Stack spacing={2} sx={{ p: 2 }}>
+        <Stack direction="row" sx={{ justifyContent: "space-between" }}>
+          <DialogTitle sx={{ p: 0, fontWeight: 700 }}>Edit address</DialogTitle>
+
+          <IconButton
+            color="inherit"
+            aria-label="close"
+            onClick={() => setOpenAddressInfoDialog(false)}
+            sx={{ p: 0 }}
+          >
+            <CloseIcon />
+          </IconButton>
+        </Stack>
+
+        <DialogContent sx={{ p: 1 }}>
+          <AddressInfoForm
+            setOpenAddressInfoDialog={setOpenAddressInfoDialog}
+          />
+        </DialogContent>
+      </Stack>
+    </Dialog>
+  );
+}
