f11fe7f27411092d05ff49ee805658389435df68
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-06-22 21:43:18 +0200

    refactor: remove unused route for retrieving all carts

--- backend/routes/api/private/carts.php
+++ backend/routes/api/private/carts.php
@@ -4,7 +4,6 @@ use App\Http\Controllers\Customer\CartController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('carts')->middleware(['auth:sanctum', 'role:customer'])->group(function () {
-  Route::get('/', [CartController::class, 'getCarts']);
   Route::get('/{cartId}', [CartController::class, 'getCart']);
   Route::post('/', [CartController::class, 'createOrUpdateCart']);
 });
