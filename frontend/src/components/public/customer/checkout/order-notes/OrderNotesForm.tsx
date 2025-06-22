0a4a7ac00e4bc7cd68d08459ab22dfa1182c9311
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-06-22 14:55:47 +0200

    feat: add OrderNotesForm component for managing order notes in checkout

---
+++ frontend/src/components/public/customer/checkout/order-notes/OrderNotesForm.tsx
@@ -0,0 +1,81 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button, Stack, TextField } from "@mui/material";
+import { Controller, useForm } from "react-hook-form";
+
+import { FormHelperTextError } from "@/components/common/FormHelperTextError";
+import { useCheckout } from "@/hooks/contexts/useCheckout";
+import {
+  TCheckoutOrderNotesForm,
+  checkoutOrderNotesForm,
+} from "@/validations/checkout-validations";
+
+type OrderNotesFormProps = {
+  setOpenOrderNotesDialog: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+export default function OrderNotesForm({
+  setOpenOrderNotesDialog,
+}: OrderNotesFormProps) {
+  const { orderNotes, setOrderNotes } = useCheckout();
+
+  const {
+    handleSubmit,
+    control,
+    formState: { isSubmitting, errors },
+  } = useForm({
+    resolver: zodResolver(checkoutOrderNotesForm),
+    defaultValues: {
+      notes: orderNotes?.notes ?? "",
+    },
+  });
+
+  function onSubmit(data: TCheckoutOrderNotesForm) {
+    setOrderNotes(data);
+    setOpenOrderNotesDialog(false);
+  }
+
+  return (
+    <Stack
+      component="form"
+      autoComplete="off"
+      noValidate
+      spacing={4}
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <Controller
+        name="notes"
+        control={control}
+        render={({ field }) => (
+          <TextField
+            {...field}
+            multiline
+            autoComplete="off"
+            label="Add a note (optional)"
+            placeholder={
+              'For example: "Very spicy, please" or "Leave the order on the porch".'
+            }
+            maxRows={4}
+            error={!!errors.notes}
+            helperText={
+              errors.notes?.message && (
+                <FormHelperTextError message={errors.notes.message} />
+              )
+            }
+            fullWidth
+            sx={{ minWidth: 150 }}
+          />
+        )}
+      />
+
+      <Button
+        type="submit"
+        disabled={isSubmitting}
+        loading={isSubmitting}
+        loadingIndicator="Saving..."
+        variant="contained"
+      >
+        Save
+      </Button>
+    </Stack>
+  );
+}
