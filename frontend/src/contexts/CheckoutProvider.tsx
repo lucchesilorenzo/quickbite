08566e14b82b4951acac1003707dcafa8864446a
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-06-22 10:48:38 +0200

    feat: add addressInfo state in CheckoutProvider

--- frontend/src/contexts/CheckoutProvider.tsx
+++ frontend/src/contexts/CheckoutProvider.tsx
@@ -5,7 +5,7 @@ import { useParams } from "react-router-dom";
 import FullPageSpinner from "@/components/common/FullPageSpinner";
 import { useGetCart } from "@/hooks/react-query/private/cart/useGetCart";
 import { Cart } from "@/types";
-import { PersonalInfo } from "@/types/order-types";
+import { AddressInfo, PersonalInfo } from "@/types/order-types";
 
 type CheckoutProviderProps = {
   children: React.ReactNode;
@@ -14,13 +14,16 @@ type CheckoutProviderProps = {
 type CheckoutContext = {
   cart: Cart;
   personalInfo: PersonalInfo | null;
+  addressInfo: AddressInfo | null;
   setPersonalInfo: React.Dispatch<React.SetStateAction<PersonalInfo | null>>;
+  setAddressInfo: React.Dispatch<React.SetStateAction<AddressInfo | null>>;
 };
 
 export const CheckoutContext = createContext<CheckoutContext | null>(null);
 
 export default function CheckoutProvider({ children }: CheckoutProviderProps) {
   const [personalInfo, setPersonalInfo] = useState<PersonalInfo | null>(null);
+  const [addressInfo, setAddressInfo] = useState<AddressInfo | null>(null);
 
   const { cartId } = useParams();
   const { data: cart = {}, isLoading: isCartLoading } = useGetCart(cartId);
@@ -28,7 +31,15 @@ export default function CheckoutProvider({ children }: CheckoutProviderProps) {
   if (isCartLoading) return <FullPageSpinner />;
 
   return (
-    <CheckoutContext.Provider value={{ cart, personalInfo, setPersonalInfo }}>
+    <CheckoutContext.Provider
+      value={{
+        cart,
+        personalInfo,
+        addressInfo,
+        setPersonalInfo,
+        setAddressInfo,
+      }}
+    >
       {children}
     </CheckoutContext.Provider>
   );
