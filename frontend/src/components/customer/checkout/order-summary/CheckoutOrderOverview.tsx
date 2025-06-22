06f927341ff7cd0384e479922a60863768908452
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-06-22 22:11:56 +0200

    feat: implement initial CheckoutOrderOverview component for displaying restaurant order summary

---
+++ frontend/src/components/customer/checkout/order-summary/CheckoutOrderOverview.tsx
@@ -0,0 +1,48 @@
+import { Box, Stack, Typography } from "@mui/material";
+
+import { useCheckout } from "@/hooks/contexts/useCheckout";
+import env from "@/lib/env";
+
+export default function CheckoutOrderOverview() {
+  const { cart } = useCheckout();
+
+  const restaurantCart = Object.values(cart)[0];
+
+  return (
+    <Stack
+      direction="row"
+      sx={{ alignItems: "center", justifyContent: "space-between" }}
+    >
+      <Box>
+        <Typography variant="body1" sx={{ fontWeight: 500 }}>
+          {restaurantCart.restaurant.name}
+        </Typography>
+
+        <Typography
+          role="button"
+          variant="body1"
+          sx={{
+            cursor: "pointer",
+            textDecoration: "underline",
+            "&:hover": { textDecoration: "none" },
+          }}
+        >
+          Show {restaurantCart.total_items} articles
+        </Typography>
+      </Box>
+
+      <Box
+        component="img"
+        src={`${env.VITE_BASE_URL}${restaurantCart.restaurant.logo}`}
+        alt={restaurantCart.restaurant.name}
+        sx={{
+          objectFit: "cover",
+          width: 50,
+          height: 50,
+          border: "2px solid #fff",
+          borderRadius: 2,
+        }}
+      />
+    </Stack>
+  );
+}
