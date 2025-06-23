d2aba47e3900131d98633e672ea2f2dfc7c0f2ec
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-06-23 22:46:41 +0200

    fix: ensure checkout data is cleared after successful order creation

--- frontend/src/hooks/react-query/private/orders/useCreateOrder.ts
+++ frontend/src/hooks/react-query/private/orders/useCreateOrder.ts
@@ -2,12 +2,15 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useNotifications } from "@toolpad/core/useNotifications";
 import { useNavigate } from "react-router-dom";
 
+import { useCheckout } from "@/hooks/contexts/useCheckout";
 import { useMultiCart } from "@/hooks/contexts/useMultiCart";
 import { postData } from "@/lib/api-client";
 import { CreateOrder } from "@/types/order-types";
 
 export function useCreateOrder(restaurantId: string) {
   const { emptyCart } = useMultiCart();
+  const { emptyCheckoutData } = useCheckout();
+
   const queryClient = useQueryClient();
   const notifications = useNotifications();
   const navigate = useNavigate();
@@ -18,6 +21,7 @@ export function useCreateOrder(restaurantId: string) {
       queryClient.invalidateQueries({ queryKey: ["orders"] });
       navigate("/checkout/success");
       emptyCart(restaurantId);
+      emptyCheckoutData(restaurantId);
     },
     onError: (error) => {
       notifications.show(error.message, {
