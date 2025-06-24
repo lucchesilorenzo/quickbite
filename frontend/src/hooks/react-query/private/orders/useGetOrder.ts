89cddf477686433cf61507dc2708135805c05a74
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-06-24 22:22:51 +0200

    feat: implement useGetOrder hook for fetching order data by orderId

---
+++ frontend/src/hooks/react-query/private/orders/useGetOrder.ts
@@ -0,0 +1,11 @@
+import { useQuery } from "@tanstack/react-query";
+
+import { fetchData } from "@/lib/api-client";
+import { Order } from "@/types/order-types";
+
+export function useGetOrder(orderId?: string) {
+  return useQuery({
+    queryKey: ["order", orderId],
+    queryFn: (): Promise<Order> => fetchData(`/orders/${orderId}`),
+  });
+}
