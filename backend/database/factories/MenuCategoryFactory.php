1ab4cd4c0a15fe798f6eb9da73a96ac80cdaa59f
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-06-26 17:00:33 +0200

    feat: add image paths for menu items in MenuCategoryFactory

--- backend/database/factories/MenuCategoryFactory.php
+++ backend/database/factories/MenuCategoryFactory.php
@@ -67,12 +67,14 @@ class MenuCategoryFactory extends Factory
                         'name' => 'Bruschetta',
                         'description' => 'Toasted bread with tomatoes and basil',
                         'price' => 5.99,
+                        'image' => Storage::url('restaurants/menu-items/bruschetta.jpg'),
                         'is_available' => true,
                     ],
                     [
                         'name' => 'Garlic Bread',
                         'description' => 'Garlic butter spread on warm bread',
                         'price' => 4.50,
+                        'image' => Storage::url('restaurants/menu-items/garlic-bread.jpg'),
                         'is_available' => true,
                     ],
                 ];
@@ -103,12 +105,14 @@ class MenuCategoryFactory extends Factory
                         'name' => 'Tiramisu',
                         'description' => 'Coffee-flavored Italian dessert',
                         'price' => 6.50,
+                        'image' => Storage::url('restaurants/menu-items/tiramisu.jpg'),
                         'is_available' => true,
                     ],
                     [
                         'name' => 'Panna Cotta',
                         'description' => 'Creamy Italian custard topped with berries',
                         'price' => 5.00,
+                        'image' => Storage::url('restaurants/menu-items/panna-cotta.jpg'),
                         'is_available' => true,
                     ],
                 ];
@@ -120,12 +124,14 @@ class MenuCategoryFactory extends Factory
                         'name' => 'Espresso',
                         'description' => 'Strong Italian coffee',
                         'price' => 2.50,
+                        'image' => Storage::url('restaurants/menu-items/espresso.jpg'),
                         'is_available' => true,
                     ],
                     [
                         'name' => 'Lemonade',
                         'description' => 'Refreshing lemon drink',
                         'price' => 3.00,
+                        'image' => Storage::url('restaurants/menu-items/lemonade.jpg'),
                         'is_available' => true,
                     ],
                 ];
@@ -137,12 +143,14 @@ class MenuCategoryFactory extends Factory
                         'name' => 'French Fries',
                         'description' => 'Crispy fried potatoes',
                         'price' => 3.99,
+                        'image' => Storage::url('restaurants/menu-items/french-fries.jpg'),
                         'is_available' => true,
                     ],
                     [
                         'name' => 'Coleslaw',
                         'description' => 'Creamy cabbage salad',
                         'price' => 4.20,
+                        'image' => Storage::url('restaurants/menu-items/coleslaw.jpg'),
                         'is_available' => true,
                     ],
                 ];
@@ -154,12 +162,14 @@ class MenuCategoryFactory extends Factory
                         'name' => 'Caesar Salad',
                         'description' => 'Romaine lettuce with Caesar dressing',
                         'price' => 6.99,
+                        'image' => Storage::url('restaurants/menu-items/caesar-salad.jpg'),
                         'is_available' => true,
                     ],
                     [
                         'name' => 'Greek Salad',
                         'description' => 'Cucumbers, tomatoes, and feta cheese',
                         'price' => 7.50,
+                        'image' => Storage::url('restaurants/menu-items/greek-salad.jpg'),
                         'is_available' => true,
                     ],
                 ];
