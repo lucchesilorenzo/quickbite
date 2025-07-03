ce9b47049488125d3f8a781cecdd83efb51c67cd
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-07-03 14:47:05 +0200

    feat: add TermsAndConditionsBreadcrumb component and integrate it into TermsAndConditionsPage

--- frontend/src/pages/public/TermsAndConditionsPage.tsx
+++ frontend/src/pages/public/TermsAndConditionsPage.tsx
@@ -1,5 +1,17 @@
-import { Box } from "@mui/material";
+import { useEffect } from "react";
+
+import { Container } from "@mui/material";
+
+import TermsAndConditionsBreadcrumb from "@/components/terms-and-conditions/TermsAndConditionsBreadcrumb";
 
 export default function TermsAndConditionsPage() {
-  return <Box component="main">TermsAndConditionsPage</Box>;
+  useEffect(() => {
+    document.title = "Terms and conditions | QuickBite";
+  }, []);
+
+  return (
+    <Container component="main" maxWidth="lg" disableGutters sx={{ p: 2 }}>
+      <TermsAndConditionsBreadcrumb />
+    </Container>
+  );
 }
