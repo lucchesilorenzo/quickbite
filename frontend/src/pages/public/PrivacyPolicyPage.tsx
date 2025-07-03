bf9b06d621119439572b0e2f337e4a2f7de6113f
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-07-03 23:03:02 +0200

    feat: implement PrivacyPolicyPage component with structured layout and dynamic title

--- frontend/src/pages/public/PrivacyPolicyPage.tsx
+++ frontend/src/pages/public/PrivacyPolicyPage.tsx
@@ -1,5 +1,23 @@
-import { Box } from "@mui/material";
+import { useEffect } from "react";
+
+import { Container } from "@mui/material";
+
+import Breadcrumb from "@/components/privacy-policy/Breadcrumb";
+import Paragraphs from "@/components/privacy-policy/Paragraphs";
+import SaveToPDFButton from "@/components/privacy-policy/SaveToPDFButton";
+import TitleAndEffectiveDate from "@/components/privacy-policy/TitleAndEffectiveDate";
 
 export default function PrivacyPolicyPage() {
-  return <Box component="main">PrivacyPolicyPage</Box>;
+  useEffect(() => {
+    document.title = "Privacy Policy | QuickBite";
+  }, []);
+
+  return (
+    <Container component="main" maxWidth="lg" disableGutters sx={{ p: 2 }}>
+      <Breadcrumb />
+      <TitleAndEffectiveDate />
+      <Paragraphs />
+      <SaveToPDFButton />
+    </Container>
+  );
 }
