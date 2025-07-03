b0f8e18b5264723685a75638646b6fcca5d810aa
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-07-03 23:02:23 +0200

    feat: add Breadcrumb component for Privacy Policy navigation

---
+++ frontend/src/components/privacy-policy/Breadcrumb.tsx
@@ -0,0 +1,32 @@
+import ChevronRightOutlinedIcon from "@mui/icons-material/ChevronRightOutlined";
+import { Breadcrumbs, Link as MuiLink, Typography } from "@mui/material";
+import { grey } from "@mui/material/colors";
+import { Link } from "react-router-dom";
+
+export default function Breadcrumb() {
+  return (
+    <Breadcrumbs
+      separator={
+        <ChevronRightOutlinedIcon fontSize="small" sx={{ color: "#FFFFFF" }} />
+      }
+      aria-label="breadcrumb"
+      sx={{
+        bgcolor: grey[600],
+        px: 2,
+        borderRadius: 5,
+        width: "fit-content",
+      }}
+    >
+      <MuiLink
+        component={Link}
+        to="/"
+        underline="hover"
+        sx={{ color: "#FFFFFF", fontWeight: 700 }}
+      >
+        Home
+      </MuiLink>
+
+      <Typography sx={{ color: "#FFFFFF" }}>Privacy Policy</Typography>
+    </Breadcrumbs>
+  );
+}
