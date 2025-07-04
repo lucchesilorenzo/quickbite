a674e055e7eafdbc8a358fbf914412bda75fdae5
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-07-04 19:44:20 +0200

    feat: add RestaurantOfferFactory for generating restaurant offer data

---
+++ backend/database/factories/RestaurantOfferFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\RestaurantOffer>
+ */
+class RestaurantOfferFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $discountRates = [
+            '0.05' => 30,
+            '0.1' => 50,
+            '0.15' => 70,
+        ];
+
+        static $i = 0;
+
+        return [
+            'discount_rate' => array_keys($discountRates)[($i % count($discountRates))],
+            'min_discount_amount' => array_values($discountRates)[($i++ % count($discountRates))],
+        ];
+    }
+}
