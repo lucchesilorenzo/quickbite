b1b4d183cc829adbd67bce6ed4a2c89bb1e50041
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-07-08 22:05:11 +0200

    refactor: update useCreateOrUpdateCart and useDeleteCart hooks

--- frontend/src/hooks/react-query/private/cart/useCreateOrUpdateCart.ts
+++ frontend/src/hooks/react-query/private/cart/useCreateOrUpdateCart.ts
@@ -1,15 +1,18 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useNotifications } from "@toolpad/core/useNotifications";
 
 import { postData } from "@/lib/api-client";
 import { RestaurantCart } from "@/types";
 
 export function useCreateOrUpdateCart() {
+  const queryClient = useQueryClient();
   const notifications = useNotifications();
 
   return useMutation({
     mutationFn: (data: RestaurantCart) => postData("/carts", data),
-
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["carts"] });
+    },
     onError: () => {
       notifications.show("Failed to create or update cart.", {
         key: "create-or-update-cart-error",
--- frontend/src/hooks/react-query/private/cart/useDeleteCart.ts
+++ frontend/src/hooks/react-query/private/cart/useDeleteCart.ts
@@ -1,13 +1,17 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useNotifications } from "@toolpad/core/useNotifications";
 
 import { deleteData } from "@/lib/api-client";
 
 export function useDeleteCart(cartId?: string) {
+  const queryClient = useQueryClient();
   const notifications = useNotifications();
 
   return useMutation({
     mutationFn: () => deleteData(`/carts/${cartId}`),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["carts"] });
+    },
     onError: (error) => {
       notifications.show(error.message, {
         key: "delete-cart-error",
