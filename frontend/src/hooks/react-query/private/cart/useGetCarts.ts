eb97ce7c8b63b56884c71dfb227b6e3f08136b13
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-07-08 22:04:23 +0200

    feat: add useGetCarts hook

---
+++ frontend/src/hooks/react-query/private/cart/useGetCarts.ts
@@ -0,0 +1,12 @@
+import { useQuery } from "@tanstack/react-query";
+
+import { fetchData } from "@/lib/api-client";
+import { RestaurantCart } from "@/types";
+
+export function useGetCarts(customer: boolean) {
+  return useQuery({
+    queryKey: ["carts"],
+    queryFn: (): Promise<RestaurantCart[]> => fetchData("/carts"),
+    enabled: !!customer,
+  });
+}
