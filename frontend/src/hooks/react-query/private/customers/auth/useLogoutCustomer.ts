1fc7946d09cfe13abcfbe647b02f4e9591ea2d01
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-07-10 15:48:15 +0200

    feat: remove checkout_data_by_restaurant item in the localStorage after log out

--- frontend/src/hooks/react-query/private/customers/auth/useLogoutCustomer.ts
+++ frontend/src/hooks/react-query/private/customers/auth/useLogoutCustomer.ts
@@ -14,6 +14,7 @@ export function useLogoutCustomer() {
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["auth"] });
       localStorage.removeItem("token");
+      localStorage.removeItem("checkout_data_by_restaurant");
 
       navigate("/customer/auth/login");
     },
