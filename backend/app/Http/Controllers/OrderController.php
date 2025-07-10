c484bdec430eafad560f079829e496c1d0a52f7b
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-07-10 11:44:05 +0200

    refactor: move CreateOrderRequest to Order folder

--- backend/app/Http/Controllers/OrderController.php
+++ backend/app/Http/Controllers/OrderController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\CreateOrderRequest;
+use App\Http\Requests\Order\CreateOrderRequest;
 use App\Models\Order;
 use Illuminate\Http\JsonResponse;
 
