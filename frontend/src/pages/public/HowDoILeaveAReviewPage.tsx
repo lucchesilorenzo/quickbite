39793561098ff4013baeea5c15ab162f6bff31ac
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-07-13 18:30:31 +0200

    refactor: move how-do-i-leave-a-review folder to components folder

--- frontend/src/pages/public/HowDoILeaveAReviewPage.tsx
+++ frontend/src/pages/public/HowDoILeaveAReviewPage.tsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 
 import { Container, Typography, useMediaQuery } from "@mui/material";
 
-import Sections from "@/how-do-i-leave-a-review/Sections";
+import Sections from "@/components/how-do-i-leave-a-review/Sections";
 
 export default function HowDoILeaveAReviewPage() {
   useEffect(() => {
