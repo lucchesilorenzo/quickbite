a028096d6d568a1dace16d94c52b809934691e00
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-07-25 12:02:05 +0200

    feat: add PartnerRegisterFormCard and PartnerRegisterForm components

---
+++ frontend/src/components/partner/auth/register/hero/PartnerRegisterFormCard.tsx
@@ -0,0 +1,30 @@
+import { Box, Paper, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+
+import PartnerRegisterForm from "./PartnerRegisterForm";
+
+export default function PartnerRegisterFormCard() {
+  return (
+    <Paper elevation={3} sx={{ p: 3, width: "600px" }}>
+      <Typography variant="h5" sx={{ fontWeight: 700 }}>
+        Grow your orders, your customers and your brand
+      </Typography>
+
+      <Box sx={{ my: 2 }}>
+        <Typography variant="body2" component="div">
+          Already have an account?{" "}
+          <Typography
+            component={Link}
+            to="/partner/auth/login"
+            variant="body2"
+            color="info"
+            sx={{ fontWeight: 500, "&:hover": { textDecoration: "none" } }}
+          >
+            Log in
+          </Typography>
+        </Typography>
+      </Box>
+      <PartnerRegisterForm />
+    </Paper>
+  );
+}
