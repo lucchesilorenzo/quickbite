6d860c2e9f5e3fed6b2f16f5896a2aa584c4e431
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-07-25 11:50:47 +0200

    feat: add DesktopPartnerRegisterLayout and Hero components

---
+++ frontend/src/components/partner/auth/register/hero/Hero.tsx
@@ -0,0 +1,25 @@
+import { Box, Container, Stack } from "@mui/material";
+
+import HeroBackground from "./HeroBackground";
+import HeroTitle from "./HeroTitle";
+import PartnerRegisterFormCard from "./PartnerRegisterFormCard";
+
+export default function Hero() {
+  return (
+    <Box sx={{ my: 6 }}>
+      <HeroBackground />
+
+      <Container
+        component="main"
+        maxWidth={false}
+        sx={{ maxWidth: "1650px" }}
+        disableGutters
+      >
+        <Stack direction="row" sx={{ justifyContent: "space-between" }}>
+          <HeroTitle />
+          <PartnerRegisterFormCard />
+        </Stack>
+      </Container>
+    </Box>
+  );
+}
--- frontend/src/pages/public/PartnerRegisterPage.tsx
+++ frontend/src/pages/public/PartnerRegisterPage.tsx
@@ -1,11 +1,11 @@
 import { useEffect } from "react";
 
-import { Box } from "@mui/material";
+import DesktopPartnerRegisterLayout from "@/components/partner/auth/register/layouts/DesktopPartnerRegisterLayout";
 
 export default function PartnerRegisterPage() {
   useEffect(() => {
     document.title = "Become a partner | QuickBite";
   }, []);
 
-  return <Box component="main">PartnerRegisterPage</Box>;
+  return <DesktopPartnerRegisterLayout />;
 }
