da2f9859584dbfee8361630292a4fda50b8cb0e9
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-07-27 15:10:04 +0200

    feat: add useLoginPartner hook

---
+++ frontend/src/hooks/react-query/private/partners/auth/useLoginPartner.ts
@@ -0,0 +1,27 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useNotifications } from "@toolpad/core/useNotifications";
+import { useNavigate } from "react-router-dom";
+
+import { postData } from "@/lib/api-client";
+import { TPartnerLoginFormSchema } from "@/validations/partner-auth-validations";
+
+export function useLoginPartner() {
+  const queryClient = useQueryClient();
+  const notifications = useNotifications();
+  const navigate = useNavigate();
+
+  return useMutation({
+    mutationFn: (data: TPartnerLoginFormSchema) =>
+      postData("/partner/auth/login", data),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["auth"] });
+      navigate("/partner/dashboard");
+    },
+    onError: (error) => {
+      notifications.show(error.message, {
+        key: "login-partner-error",
+        severity: "error",
+      });
+    },
+  });
+}
