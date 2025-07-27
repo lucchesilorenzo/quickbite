f557f538fd6e7a9c41992aeebd20d8e3b4ab3da5
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-07-27 15:08:58 +0200

    feat: add PartnerLoginImage component

---
+++ frontend/src/components/partner/auth/login/PartnerLoginImage.tsx
@@ -0,0 +1,12 @@
+import { Box } from "@mui/material";
+
+export default function PartnerLoginImage() {
+  return (
+    <Box
+      component="img"
+      src="/partner-login-image.jpg"
+      alt="Partner Login"
+      sx={{ width: 1, display: "block" }}
+    />
+  );
+}
