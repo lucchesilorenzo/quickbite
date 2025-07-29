a0166c2554e915b39bb68e1f2075d59d63bcb409
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-07-29 14:59:33 +0200

    feat: add PartnerRestaurantHeader component

---
+++ frontend/src/components/partner/restaurants/PartnerRestaurantHeader.tsx
@@ -0,0 +1,57 @@
+import RestaurantMenuIcon from "@mui/icons-material/RestaurantMenu";
+import {
+  AppBar,
+  Button,
+  Stack,
+  Toolbar,
+  Typography,
+  useMediaQuery,
+} from "@mui/material";
+import { Link } from "react-router-dom";
+
+import { useLogoutPartner } from "@/hooks/react-query/private/partners/auth/useLogoutPartner";
+
+export default function PartnerRestaurantHeader() {
+  const { mutateAsync: logoutPartner } = useLogoutPartner();
+
+  const isMobile = useMediaQuery((theme) => theme.breakpoints.down("lg"));
+
+  async function handleLogoutPartner() {
+    await logoutPartner();
+  }
+
+  return (
+    <AppBar position="relative" color="inherit" elevation={3}>
+      <Toolbar sx={{ justifyContent: "space-between" }}>
+        <Stack
+          direction="row"
+          spacing={1}
+          component={Link}
+          to="/"
+          sx={{ alignItems: "center", textDecoration: "none" }}
+        >
+          <RestaurantMenuIcon color="primary" />
+
+          <Typography
+            variant="h6"
+            component="span"
+            color="primary"
+            sx={{ display: { xs: "none", md: "block" } }}
+          >
+            QuickBite
+          </Typography>
+        </Stack>
+        <Button
+          variant="contained"
+          color="primary"
+          size={isMobile ? "small" : "medium"}
+          sx={{ fontWeight: 700 }}
+          onClick={handleLogoutPartner}
+        >
+          Log out
+        </Button>
+        NAVIGATION
+      </Toolbar>
+    </AppBar>
+  );
+}
