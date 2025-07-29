fd662a90eb989768510face9c80c1df20969c7d4
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-07-29 15:01:45 +0200

    refactor: improve partner routes structure

--- frontend/src/pages/PartnerRoutes.tsx
+++ frontend/src/pages/PartnerRoutes.tsx
@@ -1,20 +1,28 @@
 import { Navigate, Route } from "react-router-dom";
 
 import ProtectedRoute from "./ProtectedRoute";
-import PartnerDashboardPage from "./private/partner/PartnerDashboardPage";
-import PartnerRestaurantsPage from "./private/partner/PartnerRestaurantsPage";
 
 import PartnerLayout from "@/layouts/PartnerLayout";
+import PartnerRestaurantLayout from "@/layouts/PartnerRestaurantLayout";
+import PartnerDashboardPage from "@/pages/private/partner/PartnerDashboardPage";
+import PartnerRestaurantsPage from "@/pages/private/partner/PartnerRestaurantsPage";
 import { Role } from "@/types";
 
 export const PartnerRoutes = [
-  <Route path="/" element={<PartnerLayout />}>
-    <Route
-      path="partner"
-      element={<ProtectedRoute allowedRoles={[Role.PARTNER]} />}
-    >
-      <Route index element={<Navigate to="restaurants" />} />
+  <Route
+    path="partner"
+    element={<ProtectedRoute allowedRoles={[Role.PARTNER]} />}
+  >
+    <Route element={<PartnerLayout />}>
+      <Route index element={<Navigate to="restaurants" replace />} />
       <Route path="restaurants" element={<PartnerRestaurantsPage />} />
+    </Route>
+
+    <Route
+      path="restaurants/:restaurantId"
+      element={<PartnerRestaurantLayout />}
+    >
+      <Route index element={<Navigate to="dashboard" replace />} />
       <Route path="dashboard" element={<PartnerDashboardPage />} />
     </Route>
   </Route>,
