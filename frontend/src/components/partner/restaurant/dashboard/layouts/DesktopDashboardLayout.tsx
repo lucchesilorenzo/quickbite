d3ac08522494bea8b118c6b2a4100cfea3875678
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-07-31 15:16:26 +0200

    feat: add PartnerDashboardStats and PartnerDashboardStatsTitle components

--- frontend/src/components/partner/restaurant/dashboard/layouts/DesktopDashboardLayout.tsx
+++ frontend/src/components/partner/restaurant/dashboard/layouts/DesktopDashboardLayout.tsx
@@ -1,22 +1,17 @@
-import { Container, Stack } from "@mui/material";
+import { Container } from "@mui/material";
 
-import PartnerDashboardRestaurantStatus from "../PartnerDashboardRestaurantStatus";
-import PartnerDashboardWelcome from "../PartnerDashboardWelcome";
+import PartnerDashboardHeader from "../header/PartnerDashboardHeader";
+import PartnerDashboardStats from "../stats/PartnerDashboardStats";
 
 export default function DesktopDashboardLayout() {
   return (
     <Container
       component="main"
       maxWidth="md"
-      sx={{ display: { xs: "none", lg: "block" }, my: 6 }}
+      sx={{ display: { xs: "none", lg: "block" }, mt: 6 }}
     >
-      <Stack
-        direction="row"
-        sx={{ justifyContent: "space-between", alignItems: "center" }}
-      >
-        <PartnerDashboardWelcome />
-        <PartnerDashboardRestaurantStatus />
-      </Stack>
+      <PartnerDashboardHeader />
+      <PartnerDashboardStats />
     </Container>
   );
 }
---
+++ frontend/src/components/partner/restaurant/dashboard/stats/PartnerDashboardStats.tsx
@@ -0,0 +1,11 @@
+import { Box } from "@mui/material";
+
+import PartnerDashboardStatsTitle from "./PartnerDashboardStatsTitle";
+
+export default function PartnerDashboardStats() {
+  return (
+    <Box component="section">
+      <PartnerDashboardStatsTitle />
+    </Box>
+  );
+}
