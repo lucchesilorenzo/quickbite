e53f96b3f66da1f4e1cacc32ee20016272e0ccb4
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-07-31 12:56:30 +0200

    feat: add PartnerDashboardRestaurantStatus component

---
+++ frontend/src/components/partner/restaurant/dashboard/PartnerDashboardRestaurantStatus.tsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+
+import { Card, Stack, Typography } from "@mui/material";
+
+import AntSwitch from "@/components/common/AntSwitch";
+import { usePartnerRestaurant } from "@/hooks/contexts/usePartnerRestaurant";
+import { useUpdatePartnerRestaurantStatus } from "@/hooks/react-query/private/partners/restaurants/useUpdatePartnerRestaurantStatus";
+
+export default function PartnerDashboardRestaurantStatus() {
+  const { restaurant } = usePartnerRestaurant();
+  const { mutateAsync: updatePartnerRestaurantStatus } =
+    useUpdatePartnerRestaurantStatus(restaurant.id);
+
+  const [restaurantStatus, setRestaurantStatus] = useState(restaurant.is_open);
+
+  async function handleUpdateRestaurantStatus(
+    e: React.ChangeEvent<HTMLInputElement>,
+  ) {
+    setRestaurantStatus(e.target.checked);
+
+    await updatePartnerRestaurantStatus({ force_close: e.target.checked });
+  }
+
+  return (
+    <Card variant="outlined" sx={{ width: 200, p: 2 }}>
+      <Stack
+        direction="row"
+        sx={{ justifyContent: "space-between", alignItems: "center" }}
+      >
+        <Typography
+          variant="body2"
+          color={restaurantStatus ? "success" : "error"}
+        >
+          {restaurantStatus ? "Open" : "Closed"}
+        </Typography>
+
+        <AntSwitch
+          checked={restaurantStatus}
+          onChange={handleUpdateRestaurantStatus}
+        />
+      </Stack>
+    </Card>
+  );
+}
