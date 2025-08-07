e21e0235c397b32aa8009bbe9d8de6d5fb1cdb17
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-08-07 22:36:37 +0200

    feat: add UpdateRestaurantOfferRequest

---
+++ backend/app/Http/Requests/Partner/UpdateRestaurantOfferRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Requests\Partner;
+
+use App\Enums\DiscountRate;
+use App\Rules\ValidMinDiscountAmount;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class UpdateRestaurantOfferRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        $restaurant = $this->user()->restaurants()
+            ->where('id', $this->route('restaurant')->id)
+            ->first();
+
+        return [
+            'discount_rate' => ['required', 'numeric', Rule::in(DiscountRate::values())],
+            'min_discount_amount' => ['required', 'numeric', new ValidMinDiscountAmount($restaurant->min_amount)],
+        ];
+    }
+}
