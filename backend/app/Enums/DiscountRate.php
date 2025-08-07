ca9e94ac572f1ffec5b86e2527bd82f7aa4810c5
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-08-07 21:53:07 +0200

    feat: add DiscountRate enum

---
+++ backend/app/Enums/DiscountRate.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Enums;
+
+enum DiscountRate: string
+{
+    case FIVE = 0.05;
+    case TEN = 0.10;
+    case FIFTEEN = 0.15;
+    case TWENTY = 0.20;
+    case TWENTY_FIVE = 0.25;
+    case THIRTY = 0.30;
+    case THIRTY_FIVE = 0.35;
+    case FOURTY = 0.40;
+    case FOURTY_FIVE = 0.45;
+    case FIFTY = 0.50;
+
+    /**
+     * Get the enum values.
+     */
+    public static function values(): array
+    {
+        return collect(self::cases())->pluck('value')->toArray();
+    }
+}
