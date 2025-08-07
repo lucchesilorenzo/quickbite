a0d1c915dbc347515fedd2ee3be31675194c1652
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-08-07 15:39:10 +0200

    feat: add discount rates data

--- frontend/src/lib/data.ts
+++ frontend/src/lib/data.ts
@@ -494,3 +494,16 @@ export const howDoILeaveAReviewSections = [
     ],
   },
 ];
+
+export const discountRates = [
+  { value: 0.05, label: "5%" },
+  { value: 0.1, label: "10%" },
+  { value: 0.15, label: "15%" },
+  { value: 0.2, label: "20%" },
+  { value: 0.25, label: "25%" },
+  { value: 0.3, label: "30%" },
+  { value: 0.35, label: "35%" },
+  { value: 0.4, label: "40%" },
+  { value: 0.45, label: "45%" },
+  { value: 0.5, label: "50%" },
+];
