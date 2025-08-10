f7524c92b4e7020884adca5cfb48c53608dccd98
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-08-10 16:31:36 +0200

    feat: add initial restaurant menu structure

--- frontend/src/pages/private/partner/PartnerRestaurantMenuPage.tsx
+++ frontend/src/pages/private/partner/PartnerRestaurantMenuPage.tsx
@@ -1,3 +1,21 @@
+import { useEffect } from "react";
+
+import { Container, Typography } from "@mui/material";
+
+import PartnerMenuMainCards from "@/components/partner/restaurant/menu/PartnerMenuMainCards";
+
 export default function PartnerRestaurantMenuPage() {
-  return <div>PartnerRestaurantMenuPage</div>;
+  useEffect(() => {
+    document.title = "Menu | QuickBite";
+  }, []);
+
+  return (
+    <Container component="main" maxWidth="md" sx={{ mt: 4 }}>
+      <Typography variant="h5" sx={{ fontWeight: 600, mb: 4 }}>
+        Menu
+      </Typography>
+
+      <PartnerMenuMainCards />
+    </Container>
+  );
 }
