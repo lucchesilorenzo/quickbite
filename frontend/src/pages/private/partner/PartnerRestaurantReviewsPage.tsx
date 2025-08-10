0a7f1a9ad88f5fe227fe125a9183abdedc7006a8
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-08-10 12:23:11 +0200

    feat: add title in PartnerRestaurantReviewsPage

--- frontend/src/pages/private/partner/PartnerRestaurantReviewsPage.tsx
+++ frontend/src/pages/private/partner/PartnerRestaurantReviewsPage.tsx
@@ -1,3 +1,11 @@
+import { Container, Typography } from "@mui/material";
+
 export default function PartnerRestaurantReviewsPage() {
-  return <div>PartnerRestaurantReviewsPage</div>;
+  return (
+    <Container component="main" maxWidth="md" sx={{ py: 4 }}>
+      <Typography variant="h5" sx={{ fontWeight: 600, my: 1 }}>
+        Ratings and reviews
+      </Typography>
+    </Container>
+  );
 }
