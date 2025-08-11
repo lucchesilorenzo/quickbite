4f539e5b90ed0cb213e2ec7c597fdca88a63a117
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-08-11 16:12:09 +0200

    feat: add useDeletePartnerRestaurantMenuCategory hook

---
+++ frontend/src/hooks/react-query/private/partners/restaurants/useDeletePartnerRestaurantMenuCategory.ts
@@ -0,0 +1,33 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useNotifications } from "@toolpad/core/useNotifications";
+
+import { deleteData } from "@/lib/api-client";
+
+export function useDeletePartnerRestaurantMenuCategory(
+  restaurantId: string,
+  menuCategoryId: string,
+) {
+  const queryClient = useQueryClient();
+  const notifications = useNotifications();
+
+  return useMutation({
+    mutationFn: () =>
+      deleteData(`/partner/restaurants/menu/categories/${menuCategoryId}`),
+    onSuccess: (response) => {
+      queryClient.invalidateQueries({
+        queryKey: ["partner-restaurant", restaurantId],
+      });
+
+      notifications.show(response.message, {
+        key: "delete-restaurant-menu-categories-success",
+        severity: "success",
+      });
+    },
+    onError: (error) => {
+      notifications.show(error.message, {
+        key: "delete-restaurant-menu-categories-error",
+        severity: "error",
+      });
+    },
+  });
+}
