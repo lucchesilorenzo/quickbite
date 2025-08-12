c768e75c1310bde32e0e0fceb63870c1bea0a563
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-08-12 16:01:29 +0200

    feat: add PartnerBackButton in PartnerRestaurantMenuCategoriesPage

--- frontend/src/pages/private/partner/PartnerRestaurantMenuCategoriesPage.tsx
+++ frontend/src/pages/private/partner/PartnerRestaurantMenuCategoriesPage.tsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 
 import { Container, Typography } from "@mui/material";
 
+import PartnerBackButton from "@/components/partner/restaurant/common/PartnerBackButton";
 import PartnerMenuCategoriesAddMenuCategoryDialog from "@/components/partner/restaurant/menu/menu-categories/PartnerMenuCategoriesAddMenuCategoryDialog";
 import PartnerMenuCategoriesList from "@/components/partner/restaurant/menu/menu-categories/PartnerMenuCategoriesList";
 
@@ -12,7 +13,9 @@ export default function PartnerRestaurantMenuCategoriesPage() {
 
   return (
     <Container component="main" maxWidth="md" sx={{ mt: 4 }}>
-      <Typography variant="h5" sx={{ fontWeight: 600, mb: 4 }}>
+      <PartnerBackButton />
+
+      <Typography variant="h5" sx={{ fontWeight: 600, mt: 1, mb: 2 }}>
         Menu categories
       </Typography>
 
