4b53552ca6338f43a26f7600e88cc9756a67d510
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-08-12 23:29:36 +0200

    feat: add unique constraint on menu_category_id and name in menu_items table

--- backend/database/migrations/2025_04_05_204730_create_menu_items_table.php
+++ backend/database/migrations/2025_04_05_204730_create_menu_items_table.php
@@ -22,6 +22,8 @@ return new class extends Migration
             $table->string('image')->nullable();
             $table->boolean('is_available')->default(true);
             $table->timestamps();
+
+            $table->unique(['menu_category_id', 'name']);
         });
     }
 
