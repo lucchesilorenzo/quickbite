a7c4ce4a501658e115fcc9a27dc1b8174dfcfee2
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-08-12 14:57:06 +0200

    feat: add useUpdatePartnerRestaurantMenuCategoriesOrder hook

---
+++ frontend/src/hooks/react-query/private/partners/restaurants/useUpdatePartnerRestaurantMenuCategoriesOrder.ts
@@ -0,0 +1,33 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useNotifications } from "@toolpad/core/useNotifications";
+
+import { updateData } from "@/lib/api-client";
+import { MenuCategory } from "@/types";
+
+export function useUpdatePartnerRestaurantMenuCategoriesOrder(
+  restaurantId: string,
+) {
+  const queryClient = useQueryClient();
+  const notifications = useNotifications();
+
+  return useMutation({
+    mutationFn: (data: MenuCategory[]) =>
+      updateData(`/partner/restaurants/menu/categories/order`, data),
+    onSuccess: (response) => {
+      queryClient.invalidateQueries({
+        queryKey: ["partner-restaurant", restaurantId],
+      });
+
+      notifications.show(response.message, {
+        key: "update-restaurant-menu-categories-success",
+        severity: "success",
+      });
+    },
+    onError: (error) => {
+      notifications.show(error.message, {
+        key: "update-restaurant-menu-categories-error",
+        severity: "error",
+      });
+    },
+  });
+}
