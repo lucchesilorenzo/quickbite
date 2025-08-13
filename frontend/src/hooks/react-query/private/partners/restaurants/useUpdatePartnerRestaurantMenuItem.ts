1f41ceccc397838113598dae70504b22b67cab88
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-08-13 16:15:16 +0200

    feat: add useUpdatePartnerRestaurantMenuItem hook

---
+++ frontend/src/hooks/react-query/private/partners/restaurants/useUpdatePartnerRestaurantMenuItem.ts
@@ -0,0 +1,33 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useNotifications } from "@toolpad/core/useNotifications";
+
+import { postData } from "@/lib/api-client";
+
+export function useUpdatePartnerRestaurantMenuItem(
+  restaurantId: string,
+  menuItemId: string,
+) {
+  const queryClient = useQueryClient();
+  const notifications = useNotifications();
+
+  return useMutation({
+    mutationFn: (data: FormData) =>
+      postData(`/partner/restaurants/menu/items/${menuItemId}`, data),
+    onSuccess: (response) => {
+      queryClient.invalidateQueries({
+        queryKey: ["partner-restaurant", restaurantId],
+      });
+
+      notifications.show(response.message, {
+        key: "update-restaurant-menu-item-success",
+        severity: "success",
+      });
+    },
+    onError: (error) => {
+      notifications.show(error.message, {
+        key: "update-restaurant-menu-item-error",
+        severity: "error",
+      });
+    },
+  });
+}
