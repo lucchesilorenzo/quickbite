eb2a398339d8f1758fd6e5e64a911d33a571d58f
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-08-13 16:17:16 +0200

    feat: add PartnerMenuEditMenuItemDialog component

---
+++ frontend/src/components/partner/restaurant/menu/edit/PartnerMenuEditMenuItemDialog.tsx
@@ -0,0 +1,61 @@
+import CloseIcon from "@mui/icons-material/Close";
+import {
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  IconButton,
+  Stack,
+  useMediaQuery,
+} from "@mui/material";
+
+import PartnerMenuEditMenuItemForm from "./PartnerMenuEditMenuItemForm";
+
+import { MenuItem } from "@/types";
+
+type PartnerMenuEditMenuItemDialogProps = {
+  menuItem: MenuItem;
+  openEditMenuItemDialog: boolean;
+  setOpenEditMenuItemDialog: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+export default function PartnerMenuEditMenuItemDialog({
+  menuItem,
+  openEditMenuItemDialog,
+  setOpenEditMenuItemDialog,
+}: PartnerMenuEditMenuItemDialogProps) {
+  const isMobile = useMediaQuery((theme) => theme.breakpoints.down("lg"));
+
+  return (
+    <Dialog
+      open={openEditMenuItemDialog}
+      onClose={() => setOpenEditMenuItemDialog(false)}
+      fullWidth={!isMobile}
+      fullScreen={isMobile}
+      disableRestoreFocus
+    >
+      <Stack spacing={2} sx={{ p: 2 }}>
+        <Stack direction="row" sx={{ justifyContent: "space-between" }}>
+          <DialogTitle sx={{ p: 0, fontWeight: 700 }}>
+            Edit menu item
+          </DialogTitle>
+
+          <IconButton
+            color="inherit"
+            aria-label="close"
+            onClick={() => setOpenEditMenuItemDialog(false)}
+            sx={{ p: 0 }}
+          >
+            <CloseIcon />
+          </IconButton>
+        </Stack>
+
+        <DialogContent sx={{ p: 1 }}>
+          <PartnerMenuEditMenuItemForm
+            menuItem={menuItem}
+            setOpenEditOfferDialog={setOpenEditMenuItemDialog}
+          />
+        </DialogContent>
+      </Stack>
+    </Dialog>
+  );
+}
