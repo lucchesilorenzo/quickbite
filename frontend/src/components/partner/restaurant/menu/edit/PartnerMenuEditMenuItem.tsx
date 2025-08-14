20d99b97ca230bc9261912e8f8202fd4d1c08130
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-08-14 15:55:02 +0200

    feat: implement drag & drop for menu items to allow order management

--- frontend/src/components/partner/restaurant/menu/edit/PartnerMenuEditMenuItem.tsx
+++ frontend/src/components/partner/restaurant/menu/edit/PartnerMenuEditMenuItem.tsx
@@ -1,10 +1,14 @@
 import { useState } from "react";
 
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
+import DragIndicatorIcon from "@mui/icons-material/DragIndicator";
 import {
   Box,
   Card,
   CardContent,
   CardMedia,
+  IconButton,
   Stack,
   Typography,
   useMediaQuery,
@@ -25,91 +29,114 @@ export default function PartnerMenuEditMenuItem({
   menuItem,
 }: PartnerMenuEditMenuItemProps) {
   const [openEditMenuItemDialog, setOpenEditMenuItemDialog] = useState(false);
+  const { attributes, listeners, setNodeRef, transform, transition } =
+    useSortable({ id: menuItem.id });
 
   const isMobile = useMediaQuery((theme) => theme.breakpoints.down("lg"));
 
   return (
-    <>
-      <Card variant="outlined" elevation={3}>
-        <Box
-          role="button"
+    <Box
+      {...attributes}
+      ref={setNodeRef}
+      sx={{ transform: CSS.Transform.toString(transform), transition }}
+    >
+      <Stack direction="row" sx={{ alignItems: "center" }}>
+        <IconButton
+          {...listeners}
+          size="small"
           sx={{
-            p: 2,
-            bgcolor: !menuItem.is_available ? grey[100] : "",
-            "&:hover": {
-              bgcolor: grey[100],
-              cursor: "pointer",
+            cursor: "move",
+            touchAction: "manipulation",
+            "&:focus-visible": {
+              bgcolor: grey[200],
             },
           }}
-          onClick={() => setOpenEditMenuItemDialog(true)}
+          aria-label="Drag to reorder"
         >
-          <Stack direction="row" sx={{ alignItems: "center" }}>
-            <Box sx={{ flex: 1, pr: 1 }}>
-              <Typography
-                component="h3"
-                variant={isMobile ? "body1" : "h6"}
-                sx={{
-                  fontWeight: 700,
-                  color: !menuItem.is_available ? grey[500] : "",
-                }}
-              >
-                {menuItem.name}
-              </Typography>
+          <DragIndicatorIcon fontSize="small" />
+        </IconButton>
 
-              <CardContent sx={{ p: 0 }}>
+        <Card variant="outlined" sx={{ flex: 1 }} elevation={3}>
+          <Box
+            role="button"
+            sx={{
+              p: 2,
+              bgcolor: !menuItem.is_available ? grey[100] : "",
+              "&:hover": {
+                bgcolor: grey[100],
+                cursor: "pointer",
+              },
+            }}
+            onClick={() => setOpenEditMenuItemDialog(true)}
+          >
+            <Stack direction="row" sx={{ alignItems: "center" }}>
+              <Box sx={{ flex: 1, pr: 1 }}>
                 <Typography
-                  component="h4"
-                  variant={isMobile ? "body2" : "body1"}
+                  component="h3"
+                  variant={isMobile ? "body1" : "h6"}
                   sx={{
-                    fontWeight: menuItem.is_available ? 700 : "",
+                    fontWeight: 700,
                     color: !menuItem.is_available ? grey[500] : "",
                   }}
-                  gutterBottom
                 >
-                  {menuItem.is_available
-                    ? formatCurrency(menuItem.price)
-                    : "Unavailable"}
+                  {menuItem.name}
                 </Typography>
 
-                {menuItem.description && (
+                <CardContent sx={{ p: 0 }}>
                   <Typography
-                    component="div"
-                    variant="body2"
-                    color={!menuItem.is_available ? "textDisabled" : ""}
+                    component="h4"
+                    variant={isMobile ? "body2" : "body1"}
+                    sx={{
+                      fontWeight: menuItem.is_available ? 700 : "",
+                      color: !menuItem.is_available ? grey[500] : "",
+                    }}
+                    gutterBottom
                   >
-                    {truncateWords(menuItem.description, 20)}
+                    {menuItem.is_available
+                      ? formatCurrency(menuItem.price)
+                      : "Unavailable"}
                   </Typography>
+
+                  {menuItem.description && (
+                    <Typography
+                      component="div"
+                      variant="body2"
+                      color={!menuItem.is_available ? "textDisabled" : ""}
+                    >
+                      {truncateWords(menuItem.description, 20)}
+                    </Typography>
+                  )}
+                </CardContent>
+              </Box>
+
+              <Box sx={{ flexShrink: 0, position: "relative" }}>
+                {menuItem.image ? (
+                  <CardMedia
+                    component="img"
+                    sx={{
+                      height: 100,
+                      width: 150,
+                      border: "1px solid #EDEDEC",
+                      borderRadius: 2,
+                    }}
+                    image={`${env.VITE_BASE_URL}${menuItem.image}`}
+                    alt={menuItem.name}
+                    title={menuItem.name}
+                  />
+                ) : (
+                  <Box sx={{ height: 100, width: 150 }}></Box>
                 )}
-              </CardContent>
-            </Box>
+              </Box>
+            </Stack>
+          </Box>
+        </Card>
 
-            <Box sx={{ flexShrink: 0, position: "relative" }}>
-              {menuItem.image ? (
-                <CardMedia
-                  component="img"
-                  sx={{
-                    height: 100,
-                    width: 150,
-                    border: "1px solid #EDEDEC",
-                    borderRadius: 2,
-                  }}
-                  image={`${env.VITE_BASE_URL}${menuItem.image}`}
-                  alt={menuItem.name}
-                  title={menuItem.name}
-                />
-              ) : (
-                <Box sx={{ height: 100, width: 150 }}></Box>
-              )}
-            </Box>
-          </Stack>
-        </Box>
-      </Card>
-
-      <PartnerMenuEditMenuItemDialog
-        menuItem={menuItem}
-        openEditMenuItemDialog={openEditMenuItemDialog}
-        setOpenEditMenuItemDialog={setOpenEditMenuItemDialog}
-      />
-    </>
+        <PartnerMenuEditMenuItemDialog
+          menuItem={menuItem}
+          openEditMenuItemDialog={openEditMenuItemDialog}
+          setOpenEditMenuItemDialog={setOpenEditMenuItemDialog}
+        />
+      </Stack>
+    </Box>
   );
 }
--- frontend/src/components/partner/restaurant/menu/edit/PartnerMenuEditMenuItemsList.tsx
+++ frontend/src/components/partner/restaurant/menu/edit/PartnerMenuEditMenuItemsList.tsx
@@ -1,17 +1,72 @@
-import { Stack, Typography } from "@mui/material";
+import { useEffect, useMemo, useState } from "react";
+
+import {
+  DndContext,
+  DragEndEvent,
+  KeyboardSensor,
+  MouseSensor,
+  TouchSensor,
+  useSensor,
+  useSensors,
+} from "@dnd-kit/core";
+import { SortableContext, arrayMove } from "@dnd-kit/sortable";
+import { Stack, Typography, debounce } from "@mui/material";
 
 import PartnerMenuEditMenuItem from "./PartnerMenuEditMenuItem";
 
 import { usePartnerRestaurant } from "@/hooks/contexts/usePartnerRestaurant";
 import { usePartnerRestaurantMenu } from "@/hooks/contexts/usePartnerRestaurantMenu";
+import { useUpdatePartnerRestaurantMenuItemsOrder } from "@/hooks/react-query/private/partners/restaurants/useUpdatePartnerRestaurantMenuItemsOrder";
 
 export default function PartnerMenuEditMenuItemsList() {
   const { restaurant } = usePartnerRestaurant();
   const { selectedMenuCategoryId } = usePartnerRestaurantMenu();
 
-  const menuItems = restaurant.menu_categories.find(
-    (menuCategory) => menuCategory.id === selectedMenuCategoryId,
-  )?.menu_items;
+  const { mutateAsync: updateRestaurantMenuItemsOrder } =
+    useUpdatePartnerRestaurantMenuItemsOrder(restaurant.id);
+
+  const debounceUpdateRestaurantMenuItemsOrder = useMemo(
+    () => debounce(updateRestaurantMenuItemsOrder, 500),
+    [updateRestaurantMenuItemsOrder],
+  );
+
+  const menuItems = useMemo(
+    () =>
+      restaurant.menu_categories.find(
+        (menuCategory) => menuCategory.id === selectedMenuCategoryId,
+      )?.menu_items || [],
+    [restaurant.menu_categories, selectedMenuCategoryId],
+  );
+
+  const [items, setItems] = useState(menuItems);
+  const sensors = useSensors(
+    useSensor(MouseSensor),
+    useSensor(TouchSensor),
+    useSensor(KeyboardSensor),
+  );
+
+  useEffect(() => {
+    setItems(menuItems);
+  }, [menuItems]);
+
+  async function handleMenuItemSort({ active, over }: DragEndEvent) {
+    if (active.id === over?.id) return;
+
+    setItems((prev) => {
+      const from = prev.findIndex((i) => i.id === active.id);
+      const to = prev.findIndex((i) => i.id === over?.id);
+
+      const newItems = arrayMove(prev, from, to);
+      const updatedItems = newItems.map((item, index) => ({
+        ...item,
+        order: index,
+      }));
+
+      debounceUpdateRestaurantMenuItemsOrder(updatedItems);
+
+      return updatedItems;
+    });
+  }
 
   if (!menuItems || !menuItems.length) {
     return (
@@ -24,10 +79,14 @@ export default function PartnerMenuEditMenuItemsList() {
   }
 
   return (
-    <Stack spacing={2} sx={{ mt: 4 }}>
-      {menuItems.map((menuItem) => (
-        <PartnerMenuEditMenuItem key={menuItem.id} menuItem={menuItem} />
-      ))}
-    </Stack>
+    <DndContext sensors={sensors} onDragEnd={handleMenuItemSort}>
+      <SortableContext items={items}>
+        <Stack spacing={2} sx={{ my: 4 }}>
+          {items.map((menuItem) => (
+            <PartnerMenuEditMenuItem key={menuItem.id} menuItem={menuItem} />
+          ))}
+        </Stack>
+      </SortableContext>
+    </DndContext>
   );
 }
