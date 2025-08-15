6e5da2c98d69e000b0878e228e21bee6f4d7cbc6
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-08-15 16:05:36 +0200

    feat: add PartnerOrdersList and PartnerOrdersItem components

---
+++ frontend/src/components/partner/restaurant/orders/PartnerOrdersItem.tsx
@@ -0,0 +1,88 @@
+import { useState } from "react";
+
+import RestaurantIcon from "@mui/icons-material/Restaurant";
+import {
+  Box,
+  Button,
+  Card,
+  Stack,
+  Typography,
+  useMediaQuery,
+} from "@mui/material";
+import { format } from "date-fns";
+
+import OrderStatusBadge from "@/components/common/OrderStatusBadge";
+import { formatCurrency } from "@/lib/utils";
+import { PartnerOrder } from "@/types/order-types";
+
+type PartnerOrdersItemProps = {
+  order: PartnerOrder;
+};
+
+export default function PartnerOrdersItem({ order }: PartnerOrdersItemProps) {
+  const [openViewOrderDialog, setOpenViewOrderDialog] = useState(false);
+
+  const isMobile = useMediaQuery((theme) => theme.breakpoints.down("lg"));
+
+  return (
+    <Card variant="outlined" sx={{ p: 1 }}>
+      <Stack
+        direction={isMobile ? "column" : "row"}
+        spacing={2}
+        sx={{ justifyContent: "space-between" }}
+      >
+        <Stack direction="row" spacing={2} sx={{ alignItems: "center" }}>
+          <RestaurantIcon color="primary" />
+
+          <Box sx={{ flex: 1 }}>
+            <Stack
+              direction="row"
+              spacing={2}
+              sx={{ alignItems: "center", mb: 1 }}
+            >
+              <Typography
+                variant={isMobile ? "body1" : "h6"}
+                sx={{ fontWeight: 700 }}
+              >
+                Order # {order.order_code}
+              </Typography>
+
+              <OrderStatusBadge order={order} />
+            </Stack>
+
+            <Stack spacing={0.5} sx={{ mb: 1 }}>
+              <Typography variant="body2">
+                {order.first_name} {order.last_name}
+              </Typography>
+
+              <Typography variant="body2">
+                {format(new Date(order.created_at), "d MMMM yyyy 'at' HH:mm")}
+              </Typography>
+
+              <Typography variant="body2" gutterBottom>
+                {order.street_address} {order.building_number}, {order.postcode}{" "}
+                {order.city}
+              </Typography>
+            </Stack>
+
+            <Typography variant="body1">
+              {formatCurrency(order.total)}
+            </Typography>
+          </Box>
+        </Stack>
+
+        <Box>
+          <Button
+            aria-label={`View details for order ${order.order_code}`}
+            variant="contained"
+            color="success"
+            onClick={() => setOpenViewOrderDialog(true)}
+            fullWidth
+          >
+            View order
+          </Button>
+        </Box>
+      </Stack>
+    </Card>
+  );
+}
---
+++ frontend/src/components/partner/restaurant/orders/PartnerOrdersList.tsx
@@ -0,0 +1,24 @@
+import { Stack } from "@mui/material";
+
+import PartnerOrdersItem from "./PartnerOrdersItem";
+
+import Spinner from "@/components/common/Spinner";
+import { usePartnerRestaurant } from "@/hooks/contexts/usePartnerRestaurant";
+import { useGetPartnerRestaurantOrders } from "@/hooks/react-query/private/partners/restaurants/useGetPartnerRestaurantOrders";
+
+export default function PartnerOrdersList() {
+  const { restaurant } = usePartnerRestaurant();
+
+  const { data: orders = [], isLoading: isLoadingOrders } =
+    useGetPartnerRestaurantOrders(restaurant.id);
+
+  if (isLoadingOrders) return <Spinner />;
+
+  return (
+    <Stack spacing={2}>
+      {orders.map((order) => (
+        <PartnerOrdersItem key={order.id} order={order} />
+      ))}
+    </Stack>
+  );
+}
