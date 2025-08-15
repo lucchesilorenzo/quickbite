9ec2df204adb24676476cab8297d9411ffa7ba2b
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-08-15 16:04:00 +0200

    feat: add initial PartnerRestaurantOrdersPage structure

--- frontend/src/pages/private/partner/PartnerRestaurantOrdersPage.tsx
+++ frontend/src/pages/private/partner/PartnerRestaurantOrdersPage.tsx
@@ -1,3 +1,21 @@
+import { useEffect } from "react";
+
+import { Container, Typography } from "@mui/material";
+
+import PartnerOrdersList from "@/components/partner/restaurant/orders/PartnerOrdersList";
+
 export default function PartnerRestaurantOrdersPage() {
-  return <div>PartnerRestaurantOrdersPage</div>;
+  useEffect(() => {
+    document.title = "Offers | QuickBite";
+  }, []);
+
+  return (
+    <Container component="main" maxWidth="md" sx={{ mt: 4 }}>
+      <Typography variant="h5" sx={{ fontWeight: 600, mb: 4 }}>
+        Orders
+      </Typography>
+
+      <PartnerOrdersList />
+    </Container>
+  );
 }
