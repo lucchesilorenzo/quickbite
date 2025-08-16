130f6ef6d7c9326fadd66652d0c959347255d277
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-08-16 17:29:18 +0200

    feat: add useUpdatePartnerRestaurantOrderStatus hook

--- frontend/src/hooks/react-query/private/partners/restaurants/useGetPartnerRestaurantOrders.ts
+++ frontend/src/hooks/react-query/private/partners/restaurants/useGetPartnerRestaurantOrders.ts
@@ -1,12 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
 
 import { fetchData } from "@/lib/api-client";
-import { PartnerOrder } from "@/types/order-types";
+import { Order } from "@/types/order-types";
 
 export function useGetPartnerRestaurantOrders(restaurantId: string) {
   return useQuery({
     queryKey: ["partner-orders"],
-    queryFn: (): Promise<PartnerOrder[]> =>
+    queryFn: (): Promise<Order[]> =>
       fetchData(`/partner/restaurants/${restaurantId}/orders`),
   });
 }
