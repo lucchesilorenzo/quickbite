94e189b6d98801282884d632fa2fb24e80fe7099
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-08-18 12:04:22 +0200

    feat: support preparing and cancelled statuses in updateOrderStatus

--- backend/app/Http/Controllers/Partner/PartnerOrderController.php
+++ backend/app/Http/Controllers/Partner/PartnerOrderController.php
@@ -4,11 +4,15 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers\Partner;
 
+use App\Enums\OrderStatus;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Partner\UpdateOrderStatus;
+use App\Models\Delivery;
 use App\Models\Order;
 use App\Models\Restaurant;
+use Exception;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Gate;
 
 class PartnerOrderController extends Controller
@@ -46,11 +50,39 @@ class PartnerOrderController extends Controller
         $data = $request->validated();
 
         try {
-            // TODO: handle order assignment to rider
+            DB::transaction(function () use ($order, $data) {
+                // TODO: move to rider order controller
+                if ($data['status'] === OrderStatus::CANCELLED->value) {
+                    $order?->delivery->update([
+                        'cancelled_at' => now(),
+                    ]);
+                }
 
-            $order->update($data);
+                if ($data['status'] === OrderStatus::PREPARING->value) {
+                    $rider = $order->restaurant->riders()
+                        ->where('is_active', true)
+                        ->get()
+                        ->first(fn ($rider) => ! Delivery::isRiderBusy($rider->id));
+
+                    if (! $rider) {
+                        throw new Exception('All active riders are currently busy.');
+                    }
+
+                    Delivery::create([
+                        'order_id' => $order->id,
+                        'rider_id' => $rider->id,
+                        'rider_first_name' => $rider->first_name,
+                        'rider_last_name' => $rider->last_name,
+                        'rider_phone_number' => $rider->phone_number,
+                    ]);
+                }
+
+                $order->update($data);
+            });
 
             return response()->json(['message' => 'Order status updated successfully.'], 200);
+        } catch (Exception $e) {
+            return response()->json(['message' => $e->getMessage()], 400);
         } catch (Throwable $e) {
             return response()->json(['message' => 'Could not update order status.'], 500);
         }
