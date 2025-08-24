bdc1a0a62124b60a5abdecf9c23d759860d5ec51
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-08-24 16:35:08 +0200

    feat: add OfferWithPagination type

--- frontend/src/types/offer-types.ts
+++ frontend/src/types/offer-types.ts
@@ -6,3 +6,23 @@ export type Offer = {
   created_at: string;
   updated_at: string;
 };
+
+export type OfferWithPagination = {
+  current_page: number;
+  data: Offer[];
+  first_page_url: string;
+  from: number;
+  last_page: number;
+  last_page_url: string;
+  links: {
+    url: string | null;
+    label: string;
+    active: boolean;
+  }[];
+  next_page_url: string | null;
+  path: string;
+  per_page: number;
+  prev_page_url: string | null;
+  to: number;
+  total: number;
+};
