5b76e849f9ad9826cc6b1706fcb0acfe4b4cdfb1
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-08-25 12:04:12 +0200

    feat: add partner menu types

--- frontend/src/types/menu-types.ts
+++ frontend/src/types/menu-types.ts
@@ -20,3 +20,29 @@ export type MenuItem = {
   created_at: string;
   updated_at: string;
 };
+
+// === PARTNER ===
+
+export type PartnerMenuCategoryWithMenuItemPagination = MenuCategory & {
+  menu_items: PartnerMenuItemWithPagination;
+};
+
+export type PartnerMenuItemWithPagination = {
+  current_page: number;
+  data: MenuItem[];
+  first_page_url: string;
+  from: number;
+  last_page: number;
+  last_page_url: string;
+  links: {
+    url: string | null;
+    label: string;
+    active: boolean;
+  }[];
+  next_page_url: string | null;
+  path: string;
+  per_page: number;
+  prev_page_url: string | null;
+  to: number;
+  total: number;
+};
