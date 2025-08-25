c74e748b291aec35c2e030884f16203af11cdf69
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-08-25 12:05:53 +0200

    feat: add pagination in useGetPartnerRestaurantMenuCategories hook

--- frontend/src/hooks/react-query/private/partners/restaurants/menu/categories/useGetPartnerRestaurantMenuCategories.ts
+++ frontend/src/hooks/react-query/private/partners/restaurants/menu/categories/useGetPartnerRestaurantMenuCategories.ts
@@ -1,12 +1,17 @@
 import { useQuery } from "@tanstack/react-query";
 
 import { fetchData } from "@/lib/api-client";
-import { MenuCategory } from "@/types";
+import { PartnerMenuCategoryWithMenuItemPagination } from "@/types";
 
-export function useGetPartnerRestaurantMenuCategories(restaurantId: string) {
+export function useGetPartnerRestaurantMenuCategories(
+  restaurantId: string,
+  page: number = 1,
+) {
   return useQuery({
-    queryKey: ["partner-menu", restaurantId],
-    queryFn: (): Promise<MenuCategory[]> =>
-      fetchData(`/partner/restaurants/${restaurantId}/menu/categories`),
+    queryKey: ["partner-menu", restaurantId, page],
+    queryFn: (): Promise<PartnerMenuCategoryWithMenuItemPagination[]> =>
+      fetchData(
+        `/partner/restaurants/${restaurantId}/menu/categories?page=${page}`,
+      ),
   });
 }
