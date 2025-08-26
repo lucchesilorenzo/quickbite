85687cb86946b18e4155db8db14a18abd6a2fc6f
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-08-26 22:23:52 +0200

    feat: add pagination in useGetRestaurant

--- frontend/src/hooks/react-query/public/restaurants/useGetRestaurant.ts
+++ frontend/src/hooks/react-query/public/restaurants/useGetRestaurant.ts
@@ -1,12 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
 
 import { fetchData } from "@/lib/api-client";
-import { RestaurantDetail } from "@/types/restaurant-types";
+import { SingleRestaurantDetail } from "@/types/restaurant-types";
 
-export function useGetRestaurant(restaurantSlug?: string) {
+export function useGetRestaurant(restaurantSlug?: string, page: number = 1) {
   return useQuery({
-    queryKey: ["restaurant", restaurantSlug],
-    queryFn: (): Promise<RestaurantDetail> =>
-      fetchData(`/restaurants/${restaurantSlug}`),
+    queryKey: ["restaurant", restaurantSlug, page],
+    queryFn: (): Promise<SingleRestaurantDetail> =>
+      fetchData(`/restaurants/${restaurantSlug}?page=${page}`),
   });
 }
