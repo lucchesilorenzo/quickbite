244cb8fefb34209b0a88a8e54c153b8ba764a59a
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-09-04 15:05:54 +0200

    refactor: extract updateOrderStatus in PartnerOrderService

--- backend/app/Http/Controllers/Partner/PartnerOrderController.php
+++ backend/app/Http/Controllers/Partner/PartnerOrderController.php
@@ -4,16 +4,13 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers\Partner;
 
-use App\Enums\OrderStatus;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Partner\UpdateOrderStatus;
-use App\Models\Delivery;
 use App\Models\Order;
 use App\Models\Restaurant;
 use App\Services\Partner\PartnerOrderService;
 use Exception;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Gate;
 
 class PartnerOrderController extends Controller
@@ -33,7 +30,7 @@ class PartnerOrderController extends Controller
             return response()->json($orders, 200);
         } catch (Throwable $e) {
             return response()->json([
-                'message' => 'Could not get orders.'
+                'message' => 'Could not get orders.',
             ], 500);
         }
     }
@@ -45,48 +42,25 @@ class PartnerOrderController extends Controller
         Order $order,
         UpdateOrderStatus $request
     ): JsonResponse {
-        // Check if user can update order
         Gate::authorize('updatePartnerOrder', $order);
 
-        // Get validated data
         $data = $request->validated();
 
         try {
-            DB::transaction(function () use ($order, $data) {
-                // TODO: move to rider order controller
-                if ($data['status'] === OrderStatus::CANCELLED->value) {
-                    $order?->delivery->update([
-                        'cancelled_at' => now(),
-                    ]);
-                }
+            $order = $this->orderService->updateOrderStatus($order, $data);
 
-                if ($data['status'] === OrderStatus::PREPARING->value) {
-                    $rider = $order->restaurant->riders()
-                        ->where('is_active', true)
-                        ->get()
-                        ->first(fn($rider) => ! Delivery::isRiderBusy($rider->id));
-
-                    if (! $rider) {
-                        throw new Exception('All active riders are currently busy.');
-                    }
-
-                    Delivery::create([
-                        'order_id' => $order->id,
-                        'rider_id' => $rider->id,
-                        'rider_first_name' => $rider->first_name,
-                        'rider_last_name' => $rider->last_name,
-                        'rider_phone_number' => $rider->phone_number,
-                    ]);
-                }
-
-                $order->update($data);
-            });
-
-            return response()->json(['message' => 'Order status updated successfully.'], 200);
+            return response()->json([
+                'order' => $order,
+                'message' => 'Order status updated successfully.',
+            ], 200);
         } catch (Exception $e) {
-            return response()->json(['message' => $e->getMessage()], 400);
+            return response()->json([
+                'message' => $e->getMessage(),
+            ], $e->getCode());
         } catch (Throwable $e) {
-            return response()->json(['message' => 'Could not update order status.'], 500);
+            return response()->json([
+                'message' => 'Could not update order status.',
+            ], 500);
         }
     }
 }
