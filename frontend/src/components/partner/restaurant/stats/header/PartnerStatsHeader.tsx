1d8b503cbcc4c7d552662fa31ab78bc01e2499ce
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-09-05 16:50:42 +0200

    feat: add PartnerStatsHeaderRight with PartnerStatsHeaderRightStatusCard

--- frontend/src/components/partner/restaurant/stats/header/PartnerStatsHeader.tsx
+++ frontend/src/components/partner/restaurant/stats/header/PartnerStatsHeader.tsx
@@ -1,11 +1,13 @@
 import { Stack } from "@mui/material";
 
-import PartnerStatsHeaderLeft from "./PartnerStatsHeaderLeft";
+import PartnerStatsHeaderLeft from "./left/PartnerStatsHeaderLeft";
+import PartnerStatsHeaderRight from "./right/PartnerStatsHeaderRight";
 
 export default function PartnerStatsHeader() {
   return (
     <Stack direction="row" sx={{ justifyContent: "space-between" }}>
       <PartnerStatsHeaderLeft />
+      <PartnerStatsHeaderRight />
     </Stack>
   );
 }
