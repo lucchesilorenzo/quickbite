3189c435559eff646d6fb3b01e2245795b33e4b0
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-09-06 14:49:41 +0200

    style: add darker background color to partner layouts

--- frontend/src/layouts/PartnerLayout.tsx
+++ frontend/src/layouts/PartnerLayout.tsx
@@ -1,11 +1,12 @@
 import { Stack } from "@mui/material";
+import { grey } from "@mui/material/colors";
 import { Outlet } from "react-router-dom";
 
 import PartnerHeader from "@/components/partner/restaurants/PartnerHeader";
 
 export default function PartnerLayout() {
   return (
-    <Stack sx={{ minHeight: "100vh" }}>
+    <Stack sx={{ minHeight: "100vh", bgcolor: grey[100] }}>
       <PartnerHeader />
 
       <Outlet />
--- frontend/src/layouts/PartnerRestaurantLayout.tsx
+++ frontend/src/layouts/PartnerRestaurantLayout.tsx
@@ -1,4 +1,5 @@
 import { Stack } from "@mui/material";
+import { grey } from "@mui/material/colors";
 import { Outlet } from "react-router-dom";
 
 import PartnerRestaurantHeader from "@/components/partner/restaurant/PartnerRestaurantHeader";
@@ -7,7 +8,7 @@ import PartnerRestaurantProvider from "@/contexts/PartnerRestaurantProvider";
 export default function PartnerRestaurantLayout() {
   return (
     <PartnerRestaurantProvider>
-      <Stack sx={{ minHeight: "100vh" }}>
+      <Stack sx={{ minHeight: "100vh", bgcolor: grey[100] }}>
         <PartnerRestaurantHeader />
 
         <Outlet />
