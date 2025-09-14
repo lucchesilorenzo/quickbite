1e33edc3505e8e944399e68ce88d68ffe2600291
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-09-14 23:48:45 +0200

    refactor: replace PartnerStatsDetailsAcceptedOrdersChart with PartnerStatsDetailsBarChart for accepted orders visualization

--- frontend/src/components/partner/restaurant/stats/overview/details/accepted-orders/PartnerStatsDetailsAcceptedOrders.tsx
+++ frontend/src/components/partner/restaurant/stats/overview/details/accepted-orders/PartnerStatsDetailsAcceptedOrders.tsx
@@ -1,13 +1,18 @@
 import { Box } from "@mui/material";
 
+import PartnerStatsDetailsBarChart from "../common/PartnerStatsDetailsBarChart";
 import PartnerStatsDetailsHeader from "../common/PartnerStatsDetailsHeader";
-import PartnerStatsDetailsAcceptedOrdersChart from "./PartnerStatsDetailsAcceptedOrdersChart";
 
 export default function PartnerStatsDetailsAcceptedOrders() {
   return (
     <Box component="section" sx={{ bgcolor: "white", px: 4, py: 2 }}>
       <PartnerStatsDetailsHeader title="Accepted orders" />
-      <PartnerStatsDetailsAcceptedOrdersChart />
+      <PartnerStatsDetailsBarChart
+        barPrimaryColor="#007840"
+        barSecondaryColor="#E0E0E0"
+        barId="accepted"
+        barLabel="Accepted"
+      />
     </Box>
   );
 }
