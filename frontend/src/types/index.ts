46b3f32b5e4c26d958c8868af1bcb07e64ddc2ff
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-09-17 12:45:34 +0200

    feat: add NewOrderReceived type in notification-types.ts

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -9,3 +9,4 @@ export * from "./menu-types";
 export * from "./offer-types";
 export * from "./delivery-day-types";
 export * from "./stat-types";
+export * from "./notification-types";
---
+++ frontend/src/types/notification-types.ts
@@ -0,0 +1,5 @@
+export type NewOrderReceived = {
+  order_id: string;
+  title: string;
+  description: string;
+};
