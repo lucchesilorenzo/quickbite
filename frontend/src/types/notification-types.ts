b789ff258138e4025f5ee8f65f3b3c92aac1ccca
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-09-18 15:37:47 +0200

    feat: add UserNotification type

--- frontend/src/types/notification-types.ts
+++ frontend/src/types/notification-types.ts
@@ -1,3 +1,23 @@
+export type UserNotification = {
+  notifications: {
+    id: string;
+    type: string;
+    notifiable_type: string;
+    notifiable_id: string;
+    data: {
+      order_id: string;
+      order_code: number;
+      customer_name: string;
+      total: number;
+      created_at: string;
+    };
+    read_at: string | null;
+    created_at: string;
+    updated_at: string;
+  }[];
+  unread_count: number;
+};
+
 export type NewOrderReceived = {
   order_id: string;
   title: string;
