3f1b748ad40702c88e17bb697747ce1593d5e374
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-09-20 12:37:52 +0200

    fix: rating field in AddReviewForm not displaying the right validation message

--- frontend/src/validations/review-validations.ts
+++ frontend/src/validations/review-validations.ts
@@ -2,7 +2,7 @@ import z from "zod";
 
 export const reviewFormSchema = z.object({
   comment: z.string().trim().max(200, "Comment is too long."),
-  rating: z
+  rating: z.coerce
     .number()
     .min(1, "Rating is required.")
     .max(5, "Rating must be between 1 and 5."),
