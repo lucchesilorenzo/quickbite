00ac0e9f5cb1b847ec674ae776626f27d2cfcb6c
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-09-21 15:10:26 +0200

    feat: add conditional rendering for "mark all as read" button in PartnerNotificationsActionBar

--- frontend/src/components/partner/restaurant/notifications/PartnerNotificationsActionBar.tsx
+++ frontend/src/components/partner/restaurant/notifications/PartnerNotificationsActionBar.tsx
@@ -4,18 +4,24 @@ import { Button, Stack } from "@mui/material";
 
 import PartnerNotificationsMarkAsReadDialog from "./PartnerNotificationsMarkAsReadDialog";
 
+import { useAuth } from "@/hooks/contexts/useAuth";
+
 export default function PartnerNotificationsActionBar() {
+  const { userNotifications } = useAuth();
+
   const [openMarkUserNotificationsAsRead, setOpenMarkUserNotificationsAsRead] =
     useState(false);
 
   return (
     <Stack direction="row">
-      <Button
-        sx={{ ml: "auto" }}
-        onClick={() => setOpenMarkUserNotificationsAsRead(true)}
-      >
-        Mark all as read
-      </Button>
+      {userNotifications.unread_count > 0 && (
+        <Button
+          sx={{ ml: "auto" }}
+          onClick={() => setOpenMarkUserNotificationsAsRead(true)}
+        >
+          Mark all as read
+        </Button>
+      )}
 
       <PartnerNotificationsMarkAsReadDialog
         openMarkUserNotificationsAsRead={openMarkUserNotificationsAsRead}
