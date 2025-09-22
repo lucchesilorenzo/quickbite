38f8fb8ad937a8f29853c65484d5d5f02fe6eb2b
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-09-22 21:47:52 +0200

    feat: add PartnerNoAvailableRidersException

---
+++ backend/app/Exceptions/Private/Partner/PartnerNoAvailableRidersException.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Exceptions\Private\Partner;
+
+use Exception;
+
+class PartnerNoAvailableRidersException extends Exception
+{
+    public function __construct(
+        string $message = 'All active riders are currently busy.',
+        int $code = 409
+    ) {
+        parent::__construct($message, $code);
+    }
+}
