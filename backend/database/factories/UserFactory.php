a8a1c0b794d02ca597f231ce2a890d85d0873884
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-09-23 19:31:24 +0200

    refactor: update phone_number field to Italian standard in UserFactory and RestaurantFactory

--- backend/database/factories/UserFactory.php
+++ backend/database/factories/UserFactory.php
@@ -35,7 +35,7 @@ class UserFactory extends Factory
             'password' => static::$password ??= Hash::make('test'),
             'profile_picture' => null,
             'date_of_birth' => fake()->date(),
-            'phone_number' => fake()->e164PhoneNumber(),
+            'phone_number' => '+39 ' . fake()->numerify('###') . ' ' . fake()->numerify('###') . ' ' . fake()->numerify('####'),
             'street_address' => fake()->streetAddress(),
             'building_number' => fake()->buildingNumber(),
             'postcode' => fake()->postcode(),
