3966e8d428531449aca4b10bdbec2a6dd8b52ebb
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-09-27 23:14:58 +0200

    feat: add useUpdatePartnerProfileGeneralInformation hook

---
+++ frontend/src/hooks/react-query/private/partners/profile/useUpdatePartnerProfileGeneralInformation.ts
@@ -0,0 +1,31 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useNotifications } from "@toolpad/core/useNotifications";
+
+import { updateData } from "@/lib/api-client";
+import { TPartnerProfileGeneralFormSchema } from "@/validations/partner-profile-general-validations";
+
+export function useUpdatePartnerProfileGeneralInformation() {
+  const queryClient = useQueryClient();
+  const notifications = useNotifications();
+
+  return useMutation({
+    mutationFn: (data: TPartnerProfileGeneralFormSchema) =>
+      updateData("/partner/profile/general", data),
+    onSuccess: (response) => {
+      queryClient.invalidateQueries({
+        queryKey: ["auth"],
+      });
+
+      notifications.show(response.message, {
+        key: "update-partner-profile-general-information-success",
+        severity: "success",
+      });
+    },
+    onError: (error) => {
+      notifications.show(error.message, {
+        key: "update-partner-profile-general-information-error",
+        severity: "error",
+      });
+    },
+  });
+}
