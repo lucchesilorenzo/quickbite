c92648c31a07aeb376ff86ca6e071198672d614d
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-10-01 16:17:22 +0200

    refactor: move notification handling logic to PartnerRestaurantLayout

--- frontend/src/components/partner/restaurant/PartnerRestaurantHeader.tsx
+++ frontend/src/components/partner/restaurant/PartnerRestaurantHeader.tsx
@@ -1,4 +1,3 @@
-import { useEchoNotification } from "@laravel/echo-react";
 import LogoutOutlinedIcon from "@mui/icons-material/LogoutOutlined";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import RestaurantMenuIcon from "@mui/icons-material/RestaurantMenu";
@@ -12,90 +11,24 @@ import {
   Typography,
   useMediaQuery,
 } from "@mui/material";
-import { useQueryClient } from "@tanstack/react-query";
-import { useNotifications } from "@toolpad/core/useNotifications";
-import { Link, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import PartnerProfileMenu from "../profile/PartnerProfileMenu";
 import PartnerNavigation from "./PartnerNavigation";
 
-import NotificationToast from "@/components/common/NotificationToast";
-import { useAuth } from "@/hooks/contexts/useAuth";
 import { usePartnerRestaurant } from "@/hooks/contexts/usePartnerRestaurant";
 import { useLogoutPartner } from "@/hooks/react-query/private/partners/auth/useLogoutPartner";
-import {
-  NewOrderReceivedToBroadcast,
-  NewReviewReceivedToBroadcast,
-} from "@/types";
 
 export default function PartnerRestaurantHeader() {
-  const { user, userNotifications } = useAuth();
+  const { partnerNotifications } = usePartnerRestaurant();
   const { restaurant } = usePartnerRestaurant();
   const { mutateAsync: logoutPartner } = useLogoutPartner();
-
-  const { pathname } = useLocation();
-  const notifications = useNotifications();
-  const queryClient = useQueryClient();
 
   const isMobile = useMediaQuery((theme) => theme.breakpoints.down("lg"));
 
   async function handleLogoutPartner() {
     await logoutPartner();
   }
-
-  useEchoNotification<NewOrderReceivedToBroadcast>(
-    `App.Models.User.${user?.id}`,
-    (notification) => {
-      notifications.show(
-        <NotificationToast
-          title={notification.title}
-          description={notification.description}
-        />,
-        { severity: "info" },
-      );
-
-      queryClient.invalidateQueries({
-        queryKey: ["user-notifications", user?.id, 1],
-      });
-
-      if (pathname.includes("orders")) {
-        queryClient.invalidateQueries({
-          queryKey: ["partner-orders", restaurant.id, 1],
-        });
-      }
-
-      if (pathname.includes("dashboard")) {
-        queryClient.invalidateQueries({
-          queryKey: ["partner-dashboard-stats", restaurant.id],
-        });
-      }
-    },
-    "new-order-received",
-  );
-
-  useEchoNotification<NewReviewReceivedToBroadcast>(
-    `App.Models.User.${user?.id}`,
-    (notification) => {
-      notifications.show(
-        <NotificationToast
-          title={notification.title}
-          description={notification.description}
-        />,
-        { severity: "info" },
-      );
-
-      queryClient.invalidateQueries({
-        queryKey: ["user-notifications", user?.id, 1],
-      });
-
-      if (pathname.includes("reviews") || pathname.includes("dashboard")) {
-        queryClient.invalidateQueries({
-          queryKey: ["partner-reviews", restaurant.id, 1],
-        });
-      }
-    },
-    "new-review-received",
-  );
 
   return (
     <AppBar position="relative" color="inherit" elevation={3}>
@@ -130,7 +63,7 @@ export default function PartnerRestaurantHeader() {
             <Link to={`/partner/restaurants/${restaurant.id}/notifications`}>
               <IconButton aria-label="notifications">
                 <Badge
-                  badgeContent={userNotifications.unread_count}
+                  badgeContent={partnerNotifications.unread_count}
                   color="error"
                   max={20}
                 >
