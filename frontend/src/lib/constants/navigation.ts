0743a80101e83ca70af1ad62c46e02ee994d6ccb
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-10-21 23:29:16 +0200

    refactor: move headerDialogOptions constant to navigation.ts

--- frontend/src/lib/constants/navigation.ts
+++ frontend/src/lib/constants/navigation.ts
@@ -1,3 +1,19 @@
+import DeliveryDiningOutlinedIcon from "@mui/icons-material/DeliveryDiningOutlined";
+import RestaurantOutlinedIcon from "@mui/icons-material/RestaurantOutlined";
+
+export const headerDialogOptions = [
+  {
+    href: "/become-a-rider",
+    label: "Become a rider",
+    icon: DeliveryDiningOutlinedIcon,
+  },
+  {
+    href: "/partner/auth/register",
+    label: "Become a partner",
+    icon: RestaurantOutlinedIcon,
+  },
+];
+
 export const footerLinks = [
   { href: "/become-a-rider", label: "Jobs" },
   { href: "/partner/auth/register", label: "Sign up a restaurant" },
