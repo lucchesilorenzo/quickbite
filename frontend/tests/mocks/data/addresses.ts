af9f7ca0a31c648a2d5895a798c20e0315262a9d
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-10-22 22:32:53 +0200

    test: add mocked address data

---
+++ frontend/tests/mocks/data/addresses.ts
@@ -0,0 +1,29 @@
+import { Address } from "@/types/address-types";
+
+export const address: Address = {
+  place_id: "322783584703",
+  osm_id: "18017026",
+  osm_type: "relation",
+  licence: "https://locationiq.com/attribution",
+  lat: "45.4641649",
+  lon: "9.19162145",
+  boundingbox: ["45.4637711", "45.4645902", "9.1906165", "9.1926127"],
+  class: "amenity",
+  type: "place_of_worship",
+  display_name:
+    "Milan Cathedral, Cerchia dei Navigli, Municipio 1, Milan, Milan, Lombardy, 20122, Italy",
+  display_place: "Milan Cathedral",
+  display_address:
+    "Cerchia dei Navigli, Municipio 1, Milan, Milan, Lombardy, 20122, Italy",
+  address: {
+    name: "Milan Cathedral",
+    neighbourhood: "Cerchia dei Navigli",
+    suburb: "Municipio 1",
+    city: "Milan",
+    county: "Milan",
+    state: "Lombardy",
+    postcode: "20122",
+    country: "Italy",
+    country_code: "it",
+  },
+};
