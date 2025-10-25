7f4eceeda1cf624375ac9d7af6a4fd7567632340
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-10-25 15:29:58 +0200

    test: add mock for https://eu1.locationiq.com/v1/autocomplete endpoint

--- frontend/tests/mocks/handlers.ts
+++ frontend/tests/mocks/handlers.ts
@@ -1,7 +1,19 @@
 import { http, HttpResponse } from "msw";
 
+import { address } from "tests/mocks/data/addresses";
+
 export const handlers = [
-  http.get("/test", () => {
-    return HttpResponse.json({ name: "John Maverick" });
-  }),
+  http.get(
+    "https://eu1.locationiq.com/v1/autocomplete",
+    async ({ request }) => {
+      const url = new URL(request.url);
+      const query = url.searchParams.get("q");
+
+      if (!query) {
+        return new HttpResponse(null, { status: 400 });
+      }
+
+      return HttpResponse.json([address]);
+    }
+  ),
 ];
