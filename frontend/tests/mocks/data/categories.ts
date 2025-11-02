e3ed47138d5d92d232934b715f5daf6fa38475b0
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-11-02 23:39:09 +0100

    test: add mocked category data

---
+++ frontend/tests/mocks/data/categories.ts
@@ -0,0 +1,13 @@
+export const category = {
+  id: "0199e1dc-793e-7228-a014-9d989b34081a",
+  name: "Thai",
+  slug: "thai",
+  image: "/storage/category-filters/default/thai.jpg",
+  is_default: true,
+  created_at: "2025-10-14T08:35:46.000000Z",
+  updated_at: "2025-10-14T08:35:46.000000Z",
+  pivot: {
+    restaurant_id: "a01c3a1f-5f95-45e7-8cc8-4c7bcda56d46",
+    category_id: "0199e1dc-793e-7228-a014-9d989b34081a",
+  },
+};
