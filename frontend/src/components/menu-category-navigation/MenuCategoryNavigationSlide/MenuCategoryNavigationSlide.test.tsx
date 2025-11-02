23aee562f2e34c3569e030ca2376ff2fb2f5d891
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-11-02 15:07:01 +0100

    test: add test for MenuCategoryNavigationSlide component

---
+++ frontend/src/components/menu-category-navigation/MenuCategoryNavigationSlide/MenuCategoryNavigationSlide.test.tsx
@@ -0,0 +1,41 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { menuCategories } from "tests/mocks/data/menu";
+
+import MenuCategoryNavigationSlide from "./MenuCategoryNavigationSlide";
+
+describe("MenuCategoryNavigationSlide", () => {
+  function renderComponent() {
+    const mockOnSlideClick = vi.fn();
+
+    render(
+      <MenuCategoryNavigationSlide
+        menuCategory={menuCategories[0]}
+        selectedMenuCategoryId={menuCategories[0].id}
+        onSlideClick={mockOnSlideClick}
+      />,
+    );
+
+    return {
+      user: userEvent.setup(),
+      getSlideButton: () =>
+        screen.queryByRole("button", { name: menuCategories[0].name }),
+      mockOnSlideClick,
+    };
+  }
+
+  it("should render the slide button", () => {
+    const { getSlideButton } = renderComponent();
+
+    expect(getSlideButton()).toBeInTheDocument();
+  });
+
+  it("should call onSlideClick when slide button is clicked", async () => {
+    const { user, mockOnSlideClick, getSlideButton } = renderComponent();
+
+    await user.click(getSlideButton()!);
+
+    expect(mockOnSlideClick).toHaveBeenCalledWith(menuCategories[0].id);
+    expect(mockOnSlideClick).toHaveBeenCalledTimes(1);
+  });
+});
