f8b9fece910a95f31cd84b5fa1215dcf209ab8b5
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-11-04 16:05:09 +0100

    feat: add PersonalInfoNameSection component

---
+++ frontend/src/features/private/rider/auth/register/form/steps/personal-info/PersonalInfoNameSection.tsx
@@ -0,0 +1,65 @@
+import { Box, Stack, TextField, Typography } from "@mui/material";
+import { TRegisterFormSchema } from "@rider/validations/auth-validations";
+import { Controller, useFormContext } from "react-hook-form";
+
+import FormHelperTextError from "@/components/common/FormHelperTextError";
+
+export default function PersonalInfoNameSection() {
+  const {
+    control,
+    formState: { errors },
+  } = useFormContext<TRegisterFormSchema>();
+
+  return (
+    <Box>
+      <Typography variant="h5" sx={{ fontWeight: 600, mb: 2 }}>
+        What's your name?
+      </Typography>
+
+      <Stack spacing={4}>
+        <Controller
+          name="first_name"
+          control={control}
+          render={({ field }) => (
+            <TextField
+              {...field}
+              required
+              autoComplete="off"
+              label="First name"
+              placeholder="eg. Mario"
+              error={!!errors.first_name}
+              helperText={
+                errors.first_name?.message && (
+                  <FormHelperTextError message={errors.first_name.message} />
+                )
+              }
+              fullWidth
+              sx={{ minWidth: 150 }}
+            />
+          )}
+        />
+
+        <Controller
+          name="last_name"
+          control={control}
+          render={({ field }) => (
+            <TextField
+              {...field}
+              required
+              label="Last name"
+              placeholder="eg. Rossi"
+              error={!!errors.last_name}
+              helperText={
+                errors.last_name?.message && (
+                  <FormHelperTextError message={errors.last_name.message} />
+                )
+              }
+              fullWidth
+              sx={{ minWidth: 150 }}
+            />
+          )}
+        />
+      </Stack>
+    </Box>
+  );
+}
