625306548ddeed8da1083d69dceba412c994e275
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-11-04 14:46:14 +0100

    refactor: move rider registration requirements data into dedicated helper file

--- frontend/src/features/private/rider/lib/utils/requirements.ts
+++ frontend/src/features/private/rider/lib/utils/requirements.ts
@@ -1,8 +1,4 @@
-import { Box, Stack, Typography } from "@mui/material";
-
-import RequirementCard from "./RequirementCard";
-
-const requirements = [
+export const requirements = [
   {
     backgroundColor: "#fba7b0",
     title: "You must be at least 18 years old",
@@ -27,23 +23,3 @@ const requirements = [
       "If you plan to use a scooter, motorcycle, or car, you must have a valid driver's license and active insurance. For bicycles, no license is required.",
   },
 ];
-
-export default function RequirementsStep() {
-  return (
-    <Box>
-      <Typography variant="h5" sx={{ fontWeight: 600, mb: 1 }}>
-        Requirements
-      </Typography>
-
-      <Typography variant="body1" sx={{ mb: 4 }}>
-        To apply, you must meet the following requirements:
-      </Typography>
-
-      <Stack spacing={2}>
-        {requirements.map((requirement) => (
-          <RequirementCard key={requirement.title} requirement={requirement} />
-        ))}
-      </Stack>
-    </Box>
-  );
-}
