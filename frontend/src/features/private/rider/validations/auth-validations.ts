7580765455e693d4bf5e55cba01500feeaade417
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-11-04 16:03:32 +0100

    feat: add first_name, last_name, email and phone_number fields to registerFormSchema

--- frontend/src/features/private/rider/validations/auth-validations.ts
+++ frontend/src/features/private/rider/validations/auth-validations.ts
@@ -1,5 +1,31 @@
+import { matchIsValidTel } from "mui-tel-input";
 import z from "zod";
 
-export const registerFormSchema = z.object({});
+export const registerFormSchema = z.object({
+  first_name: z
+    .string()
+    .trim()
+    .min(1, "Please fill out your first name.")
+    .max(50, "First name is too long."),
+  last_name: z
+    .string()
+    .trim()
+    .min(1, "Please fill out your last name.")
+    .max(50, "Last name is too long."),
+  email: z
+    .email({ error: "Please enter a valid email address." })
+    .min(1, "Email is required."),
+  phone_number: z
+    .string()
+    .trim()
+    .min(1, "Please fill out your phone number.")
+    .refine(
+      (phone_number) =>
+        matchIsValidTel(phone_number, { onlyCountries: ["IT"] }),
+      {
+        error: "Please enter a valid phone number.",
+      },
+    ),
+});
 
 export type TRegisterFormSchema = z.infer<typeof registerFormSchema>;
