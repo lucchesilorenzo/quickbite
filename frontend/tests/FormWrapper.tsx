1711f0bce565ae59170296782fb536b6c7f71c60
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-11-05 13:11:31 +0100

    test: add formOptions support to FormWrapper

--- frontend/tests/FormWrapper.tsx
+++ frontend/tests/FormWrapper.tsx
@@ -1,22 +1,30 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { ReactNode } from "react";
-import { FieldValues, FormProvider, useForm } from "react-hook-form";
+import {
+  FieldValues,
+  FormProvider,
+  useForm,
+  UseFormProps,
+} from "react-hook-form";
 import { ZodType } from "zod";
 
 type FormWrapperProps<T extends FieldValues> = {
   children: ReactNode;
   schema: ZodType<T>;
   defaultValues?: Partial<T>;
+  formOptions?: Omit<UseFormProps<T>, "resolver" | "defaultValues">;
 };
 
 export default function FormWrapper<T extends FieldValues>({
   children,
   schema,
   defaultValues,
+  formOptions,
 }: FormWrapperProps<T>) {
   const methods = useForm<T>({
     resolver: zodResolver(schema as any),
     defaultValues: defaultValues as any,
+    ...formOptions,
   });
 
   return <FormProvider {...methods}>{children}</FormProvider>;
