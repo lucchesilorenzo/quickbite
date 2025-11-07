7dde5368d35abb521bc9600a700e7b165a4e201e
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-11-07 12:38:49 +0100

    refactor: add Stepper and MobileStepper components in RegisterWizard

--- frontend/src/features/private/rider/auth/register/form/RegisterWizard.tsx
+++ frontend/src/features/private/rider/auth/register/form/RegisterWizard.tsx
@@ -1,45 +1,16 @@
 import { useState } from "react";
 
 import { zodResolver } from "@hookform/resolvers/zod";
-import {
-  Button,
-  Container,
-  Stack,
-  Step,
-  StepLabel,
-  Stepper,
-  Typography,
-} from "@mui/material";
+import { Container } from "@mui/material";
+import { stepFields } from "@rider/lib/constants/register-wizard/steps";
 import {
   TRegisterFormSchema,
   registerFormSchema,
 } from "@rider/validations/auth-validations";
 import { FormProvider, useForm } from "react-hook-form";
 
-import AccountSecurityStep from "./steps/account-security/AccountSecurityStep";
-import FinishYourRegistrationStep from "./steps/finish-your-registration/FinishYourRegistrationStep";
-import LocationStep from "./steps/location/LocationStep";
-import PersonalInfoStep from "./steps/personal-info/PersonalInfoStep";
-import RequirementsStep from "./steps/requirements/RequirementsStep";
-import VehicleStep from "./steps/vehicle/VehicleStep";
-
-const steps = [
-  { title: "Step 1", subtitle: "Requirements" },
-  { title: "Step 2", subtitle: "Personal info" },
-  { title: "Step 3", subtitle: "Location" },
-  { title: "Step 4", subtitle: "Vehicle" },
-  { title: "Step 5", subtitle: "Account security" },
-  { title: "Step 6", subtitle: "Finish your registration" },
-];
-
-const stepFields: Record<number, (keyof TRegisterFormSchema)[]> = {
-  0: [],
-  1: ["first_name", "last_name", "email", "phone_number"],
-  2: ["street_address", "building_number", "postcode", "city", "state"],
-  3: ["vehicle_type"],
-  4: ["password", "password_confirmation"],
-  5: [],
-};
+import Stepper from "./Stepper";
+import MobileStepper from "./mobile/MobileStepper";
 
 export default function RegisterWizard() {
   const [activeStep, setActiveStep] = useState(0);
@@ -63,20 +34,12 @@ export default function RegisterWizard() {
     },
   });
 
-  const {
-    handleSubmit,
-    trigger,
-    formState: { isSubmitting },
-  } = methods;
-
-  const isLastStep = activeStep === steps.length - 1;
-
   async function onSubmit(data: TRegisterFormSchema) {
     console.log(data);
   }
 
   async function handleNext() {
-    const isValid = await trigger(stepFields[activeStep]);
+    const isValid = await methods.trigger(stepFields[activeStep]);
     if (!isValid) return;
 
     setActiveStep((prev) => prev + 1);
@@ -89,58 +52,19 @@ export default function RegisterWizard() {
   return (
     <FormProvider {...methods}>
       <Container maxWidth="lg" sx={{ my: 4 }}>
-        <Stepper activeStep={activeStep} sx={{ mb: 4 }}>
-          {steps.map(({ title, subtitle }) => (
-            <Step key={title}>
-              <StepLabel>{title}</StepLabel>
-              <Typography variant="caption">{subtitle}</Typography>
-            </Step>
-          ))}
-        </Stepper>
+        <Stepper
+          activeStep={activeStep}
+          onNext={handleNext}
+          onBack={handleBack}
+          onSubmit={onSubmit}
+        />
 
-        <Container maxWidth="md">
-          <Stack
-            component="form"
-            onSubmit={handleSubmit(onSubmit)}
-            autoComplete="off"
-            noValidate
-          >
-            {activeStep === 0 && <RequirementsStep />}
-            {activeStep === 1 && <PersonalInfoStep />}
-            {activeStep === 2 && <LocationStep />}
-            {activeStep === 3 && <VehicleStep />}
-            {activeStep === 4 && <AccountSecurityStep />}
-            {activeStep === 5 && <FinishYourRegistrationStep />}
-
-            <Stack
-              direction="row"
-              sx={{ justifyContent: "space-between", mt: 6 }}
-            >
-              <Button
-                variant="outlined"
-                color="inherit"
-                size="large"
-                sx={{ px: 5, fontWeight: 700 }}
-                disabled={activeStep === 0}
-                onClick={handleBack}
-              >
-                Previous
-              </Button>
-
-              <Button
-                variant="contained"
-                color="primary"
-                size="large"
-                sx={{ px: 5, fontWeight: 700 }}
-                loading={isLastStep && isSubmitting}
-                loadingIndicator="Submitting..."
-                onClick={isLastStep ? handleSubmit(onSubmit) : handleNext}
-              >
-                {isLastStep ? "Submit" : "Next"}
-              </Button>
-            </Stack>
-          </Stack>
-        </Container>
+        <MobileStepper
+          activeStep={activeStep}
+          onNext={handleNext}
+          onBack={handleBack}
+          onSubmit={onSubmit}
+        />
       </Container>
     </FormProvider>
   );
--- frontend/src/features/private/rider/auth/register/form/steps/requirements/RequirementsStep/RequirementsStep.tsx
+++ frontend/src/features/private/rider/auth/register/form/steps/requirements/RequirementsStep/RequirementsStep.tsx
@@ -1,5 +1,5 @@
 import { Box, Stack, Typography } from "@mui/material";
-import { requirements } from "@rider/lib/utils/requirements";
+import { requirements } from "@rider/lib/constants/register-wizard/requirements";
 
 import RequirementCard from "../RequirementCard";
 
