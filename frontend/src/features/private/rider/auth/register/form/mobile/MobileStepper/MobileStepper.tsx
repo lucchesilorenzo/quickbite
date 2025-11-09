d181f37bb7c5abda2170e11efcdaf432835a7bab
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-11-09 12:17:40 +0100

    test: add test for MobileStepper component

--- frontend/src/features/private/rider/auth/register/form/mobile/MobileStepper/MobileStepper.tsx
+++ frontend/src/features/private/rider/auth/register/form/mobile/MobileStepper/MobileStepper.tsx
@@ -6,12 +6,12 @@ import { steps } from "@rider/lib/constants/register-wizard/steps";
 import { TRegisterFormSchema } from "@rider/validations/auth-validations";
 import { useFormContext } from "react-hook-form";
 
-import AccountSecurityStep from "../steps/account-security/AccountSecurityStep";
-import FinishYourRegistrationStep from "../steps/finish-your-registration/FinishYourRegistrationStep";
-import LocationStep from "../steps/location/LocationStep";
-import PersonalInfoStep from "../steps/personal-info/PersonalInfoStep";
-import RequirementsStep from "../steps/requirements/RequirementsStep";
-import VehicleStep from "../steps/vehicle/VehicleStep";
+import AccountSecurityStep from "../../steps/account-security/AccountSecurityStep";
+import FinishYourRegistrationStep from "../../steps/finish-your-registration/FinishYourRegistrationStep";
+import LocationStep from "../../steps/location/LocationStep";
+import PersonalInfoStep from "../../steps/personal-info/PersonalInfoStep";
+import RequirementsStep from "../../steps/requirements/RequirementsStep";
+import VehicleStep from "../../steps/vehicle/VehicleStep";
 
 type MobileStepperProps = {
   activeStep: number;
