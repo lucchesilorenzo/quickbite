8288db50f29348bae6bf6980950b2f3585d1cba8
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-11-14 12:25:39 +0100

    feat: add initial JobPostsTable component

---
+++ frontend/src/features/private/partner/restaurant/job-posts/JobPostsTable.tsx
@@ -0,0 +1,90 @@
+import PlusIcon from "@mui/icons-material/Add";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
+import { Box, Button, Chip, IconButton, Stack } from "@mui/material";
+import { DataGrid, GridColDef, GridRowsProp } from "@mui/x-data-grid";
+
+const rows: GridRowsProp = [
+  {
+    id: 1,
+    title: "Frontend developer",
+    status: "open",
+    applicationsCount: 10,
+  },
+  {
+    id: 2,
+    title: "Backend developer",
+    status: "closed",
+    applicationsCount: 5,
+  },
+];
+
+const columns: GridColDef[] = [
+  {
+    field: "title",
+    headerName: "Title",
+    flex: 1,
+  },
+  {
+    field: "status",
+    headerName: "Status",
+    width: 120,
+    renderCell: ({ value }) => (
+      <Chip
+        label={value === "open" ? "Open" : "Closed"}
+        color={value === "open" ? "success" : "error"}
+        size="small"
+      />
+    ),
+  },
+  {
+    field: "applicationsCount",
+    headerName: "Applications count",
+    width: 200,
+  },
+  {
+    field: "actions",
+    headerName: "Actions",
+    width: 200,
+    filterable: false,
+    sortable: false,
+    disableExport: true,
+    disableColumnMenu: true,
+    hideSortIcons: true,
+    type: "actions",
+    renderCell: () => (
+      <>
+        <IconButton size="small" color="primary">
+          <EditIcon />
+        </IconButton>
+
+        <IconButton size="small" color="error">
+          <DeleteIcon />
+        </IconButton>
+      </>
+    ),
+  },
+];
+
+export default function JobPostsTable() {
+  return (
+    <Stack spacing={2}>
+      <Box>
+        <Button variant="contained" startIcon={<PlusIcon />}>
+          Add job post
+        </Button>
+      </Box>
+
+      <Stack>
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          checkboxSelection
+          disableRowSelectionOnClick
+          showToolbar
+          ignoreDiacritics
+        />
+      </Stack>
+    </Stack>
+  );
+}
