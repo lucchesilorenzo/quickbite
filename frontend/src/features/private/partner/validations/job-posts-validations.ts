c9fa524ef4b70b9a15a7d8145a67926e29e649fb
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-11-16 23:57:20 +0100

    refactor: update job posts validations to use {html, text} for description and set employment_type default to empty string

--- frontend/src/features/private/partner/validations/job-posts-validations.ts
+++ frontend/src/features/private/partner/validations/job-posts-validations.ts
@@ -8,13 +8,20 @@ export const addJobPostFormSchema = z.object({
     .trim()
     .min(1, "Please fill out your job post title.")
     .max(50, "Job post title is too long."),
-  description: z.string().trim().max(200, "Description is too long."),
-  employment_type: z.enum(
-    employmentTypes.map((type) => type.value),
-    {
-      error: "Please select a valid employment type.",
-    },
-  ),
+  description: z.object({
+    html: z.string(),
+    text: z
+      .string()
+      .trim()
+      .min(1, "Please fill out your job post description.")
+      .max(2000, "Description is too long."),
+  }),
+  employment_type: z
+    .enum(employmentTypes.map((type) => type.value))
+    .or(z.literal(""))
+    .refine((data) => data !== "", {
+      error: "Please select an employment type.",
+    }),
   salary: z.coerce
     .number({
       error: (issue) => {
