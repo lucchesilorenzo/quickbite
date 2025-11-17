999e3cfeec7e3df05a6ef1aa728467f15153d9ca
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-11-17 22:52:45 +0100

    test: add test for useCreateJobPost hook

---
+++ frontend/src/features/private/partner/hooks/restaurants/job-posts/useCreateJobPost/useCreateJobPost.test.ts
@@ -0,0 +1,76 @@
+import { renderHook, waitFor } from "@testing-library/react";
+import { useNotifications } from "@toolpad/core/useNotifications";
+import TestQueryWrapper from "tests/TestQueryWrapper";
+import {
+  addJobPostForm,
+  addJobPostFormResponse,
+} from "tests/mocks/data/forms/partner/add-job-post";
+import { simulateError } from "tests/utils/msw";
+
+import { useCreateJobPost } from "./useCreateJobPost";
+
+import env from "@/lib/env";
+
+const mockShow = vi.fn();
+
+vi.mocked(useNotifications).mockReturnValue({
+  show: mockShow,
+  close: vi.fn(),
+});
+
+describe("useCreateJobPost", () => {
+  it("should mutate and return data", async () => {
+    const { result } = renderHook(() => useCreateJobPost("1"), {
+      wrapper: TestQueryWrapper,
+    });
+
+    result.current.mutate(addJobPostForm);
+
+    await waitFor(() => expect(result.current.isSuccess).toBeTruthy());
+
+    expect(result.current.data).toEqual(addJobPostFormResponse);
+  });
+
+  it("should show notification on error", async () => {
+    simulateError(
+      `${env.VITE_BASE_URL}/api/partner/restaurants/1/job-posts`,
+      "post",
+    );
+
+    const { result } = renderHook(() => useCreateJobPost("1"), {
+      wrapper: TestQueryWrapper,
+    });
+
+    result.current.mutate(addJobPostForm);
+
+    await waitFor(() => expect(result.current.isError).toBeTruthy());
+
+    expect(result.current.error).toBeDefined();
+
+    expect(mockShow).toHaveBeenCalledWith(
+      expect.any(String),
+      expect.objectContaining({
+        key: "partner-create-job-post-error",
+        severity: "error",
+      }),
+    );
+  });
+
+  it("should show notification on success", async () => {
+    const { result } = renderHook(() => useCreateJobPost("1"), {
+      wrapper: TestQueryWrapper,
+    });
+
+    result.current.mutate(addJobPostForm);
+
+    await waitFor(() => expect(result.current.isSuccess).toBeTruthy());
+
+    expect(mockShow).toHaveBeenCalledWith(
+      expect.any(String),
+      expect.objectContaining({
+        key: "partner-create-job-post-success",
+        severity: "success",
+      }),
+    );
+  });
+});
