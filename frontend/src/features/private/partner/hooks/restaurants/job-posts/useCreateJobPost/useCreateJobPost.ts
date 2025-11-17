6a7446d4fd5927ee8619c701eac00e10a542e222
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-11-17 22:52:22 +0100

    feat: add useCreateJobPost hook

---
+++ frontend/src/features/private/partner/hooks/restaurants/job-posts/useCreateJobPost/useCreateJobPost.ts
@@ -0,0 +1,34 @@
+import {
+  CreateJobPostPayload,
+  CreateJobPostResponse,
+} from "@partner/types/job-posts/job-posts.api-types";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useNotifications } from "@toolpad/core/useNotifications";
+
+import { postData } from "@/lib/api-client";
+
+export function useCreateJobPost(restaurantId: string) {
+  const queryClient = useQueryClient();
+  const notifications = useNotifications();
+
+  return useMutation<CreateJobPostResponse, Error, CreateJobPostPayload>({
+    mutationFn: (data) =>
+      postData(`/partner/restaurants/${restaurantId}/job-posts`, data),
+    onSuccess: (response) => {
+      queryClient.invalidateQueries({
+        queryKey: ["partner-job-posts", restaurantId],
+      });
+
+      notifications.show(response.message, {
+        key: "partner-create-job-post-success",
+        severity: "success",
+      });
+    },
+    onError: (error) => {
+      notifications.show(error.message, {
+        key: "partner-create-job-post-error",
+        severity: "error",
+      });
+    },
+  });
+}
