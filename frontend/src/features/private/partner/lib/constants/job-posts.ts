5f077ea77cb5a17e28ba38c0378ac9ab3e81ad77
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-11-21 15:36:27 +0100

    feat: add type to employmentTypes data

--- frontend/src/features/private/partner/lib/constants/job-posts.ts
+++ frontend/src/features/private/partner/lib/constants/job-posts.ts
@@ -1,4 +1,9 @@
-export const employmentTypes = [
+import { EmploymentType } from "@partner/types/job-posts/job-posts.types";
+
+export const employmentTypes: {
+  value: EmploymentType | string;
+  label: string;
+}[] = [
   { value: "full_time", label: "Full-time" },
   { value: "part_time", label: "Part-time" },
   { value: "contract", label: "Contract" },
