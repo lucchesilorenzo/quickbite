0aed368ab87eab7f249bbfa93f734fe42c3450ba
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-11-22 19:52:38 +0100

    feat: add /api/partner/restaurants/{restaurant}/job-posts/{jobPost} endpoint

--- backend/routes/api/private/partners.php
+++ backend/routes/api/private/partners.php
@@ -86,5 +86,6 @@ Route::prefix('partner')->group(function (): void {
             // Job Posts
             Route::get('/{restaurant}/job-posts', [JobPostController::class, 'getJobPosts']);
             Route::post('/{restaurant}/job-posts', [JobPostController::class, 'createJobPost']);
+            Route::delete('/{restaurant}/job-posts/{jobPost}', [JobPostController::class, 'deleteJobPost']);
         });
 });
