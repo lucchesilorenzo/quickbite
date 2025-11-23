584dfabe21c923050c8a558b27a728089d919b88
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-11-23 11:37:52 +0100

    feat: add useDeleteJobPosts hook

---
+++ frontend/src/features/private/partner/hooks/restaurants/job-posts/useDeleteJobPosts/useDeleteJobPosts.ts
@@ -0,0 +1,41 @@
+import { GridRowId } from "@mui/x-data-grid";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useNotifications } from "@toolpad/core/useNotifications";
+
+import { deleteData } from "@/lib/api-client";
+
+export function useDeleteJobPosts(
+  restaurantId: string,
+  jobPostIds?: Set<GridRowId>,
+) {
+  const queryClient = useQueryClient();
+  const notifications = useNotifications();
+
+  return useMutation({
+    mutationFn: () => {
+      const params = new URLSearchParams();
+
+      jobPostIds?.forEach((id) => params.append("ids[]", id.toString()));
+
+      return deleteData(
+        `/partner/restaurants/${restaurantId}/job-posts?${params.toString()}`,
+      );
+    },
+    onSuccess: (response) => {
+      queryClient.invalidateQueries({
+        queryKey: ["partner-job-posts", restaurantId],
+      });
+
+      notifications.show(response.message, {
+        key: "partner-delete-job-posts-success",
+        severity: "success",
+      });
+    },
+    onError: (error) => {
+      notifications.show(error.message, {
+        key: "partner-delete-job-posts-error",
+        severity: "error",
+      });
+    },
+  });
+}
