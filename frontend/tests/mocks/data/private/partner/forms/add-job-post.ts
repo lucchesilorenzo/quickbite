dd48c7e2a3ee6b7778cb27f47acc110c3b2bfc67
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-11-26 23:55:57 +0100

    test: update mocked job posts data

--- frontend/tests/mocks/data/private/partner/forms/add-job-post.ts
+++ frontend/tests/mocks/data/private/partner/forms/add-job-post.ts
@@ -5,20 +5,19 @@ import {
 
 export const addJobPostForm: CreateJobPostPayload = {
   title: "test",
-  description: '<p style="text-align: left;">test</p>',
+  description_html: '<p style="text-align: left;">test</p>',
+  description_text: "test",
   employment_type: "part_time",
   salary: 10000,
 };
 
 export const addJobPostFormResponse: CreateJobPostResponse = {
   job_post: {
+    ...addJobPostForm,
     id: "019a9227-53ad-7271-90bd-2e88ab8ca909",
     restaurant_id: "a0611650-d7f4-481f-ac39-de9c8d1073ed",
-    title: "test",
-    description: '<p style="text-align: left;"><em>te</em></p>',
     employment_type: "full_time",
     status: "open",
-    salary: 20000,
     updated_at: "2025-11-17T14:10:42.000000Z",
     created_at: "2025-11-17T14:10:42.000000Z",
   },
