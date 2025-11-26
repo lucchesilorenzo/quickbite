5752f233380d338bc14508b06590a73ead649895
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-11-26 23:46:33 +0100

    feat: add useUpdateJobPost hook

---
+++ frontend/src/features/private/partner/hooks/restaurants/job-posts/useUpdateJobPost/useUpdateJobPost.ts
@@ -0,0 +1,49 @@
+import {
+  UpdateJobPostPayload,
+  UpdateJobPostResponse,
+} from "@partner/types/job-posts/job-posts.api-types";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useNotifications } from "@toolpad/core/useNotifications";
+
+import { updateData } from "@/lib/api-client";
+
+type UseUpdateJobPostOptions = {
+  restaurantId: string;
+  jobPostId?: string;
+  setOpenEditJobPostDialog: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+export function useUpdateJobPost({
+  restaurantId,
+  jobPostId,
+  setOpenEditJobPostDialog,
+}: UseUpdateJobPostOptions) {
+  const queryClient = useQueryClient();
+  const notifications = useNotifications();
+
+  return useMutation<UpdateJobPostResponse, Error, UpdateJobPostPayload>({
+    mutationFn: (data) =>
+      updateData(
+        `/partner/restaurants/${restaurantId}/job-posts/${jobPostId}`,
+        data,
+      ),
+    onSuccess: (response) => {
+      queryClient.invalidateQueries({
+        queryKey: ["partner-job-posts", restaurantId],
+      });
+
+      setOpenEditJobPostDialog(false);
+
+      notifications.show(response.message, {
+        key: "partner-update-job-post-success",
+        severity: "success",
+      });
+    },
+    onError: (error) => {
+      notifications.show(error.message, {
+        key: "partner-update-job-post-error",
+        severity: "error",
+      });
+    },
+  });
+}
