6d3aab3d0b59c0ba690bcf73277ff2cdfe25e8bb
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-11-30 22:43:24 +0100

    feat: add JobPostHasApplicationsException

---
+++ backend/app/Exceptions/Private/Partner/JobPostHasApplicationsException.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Exceptions\Private\Partner;
+
+use Exception;
+
+class JobPostHasApplicationsException extends Exception
+{
+    public function __construct(
+        string $message = 'Cannot delete job post with applications.',
+        int $code = 422
+    ) {
+        parent::__construct($message, $code);
+    }
+}
