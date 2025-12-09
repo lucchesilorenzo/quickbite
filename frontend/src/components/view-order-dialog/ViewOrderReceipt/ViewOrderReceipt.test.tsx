5498629aaa71c9cbf7b5462e3f4aa8e25530f569
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-12-09 23:18:48 +0100

    test: add loading and error handling cases in ViewOrderReceipt tests

--- frontend/src/components/view-order-dialog/ViewOrderReceipt/ViewOrderReceipt.test.tsx
+++ frontend/src/components/view-order-dialog/ViewOrderReceipt/ViewOrderReceipt.test.tsx
@@ -1,8 +1,11 @@
-import { screen } from "@testing-library/react";
+import { screen, waitForElementToBeRemoved } from "@testing-library/react";
 import { order } from "@tests/mocks/data/private/orders";
 import { customRender } from "@tests/utils/custom-render";
+import { simulateDelay, simulateError } from "@tests/utils/msw";
 
 import ViewOrderReceipt from "./ViewOrderReceipt";
+
+import env from "@/lib/env";
 
 let mockLoading = false;
 let mockError: Error | null = null;
@@ -24,26 +27,62 @@ beforeEach(() => {
 
 describe("ViewOrderReceipt", () => {
   function renderComponent() {
-    return customRender(<ViewOrderReceipt order={order} />);
+    customRender(<ViewOrderReceipt order={order} />);
+
+    return {
+      getLoadingText: () => screen.queryByRole("progressbar"),
+    };
   }
 
-  it("should render the 'View receipt' button", () => {
+  it("should render the 'View receipt' button", async () => {
     renderComponent();
 
-    expect(screen.getByRole("button", { name: /view/i })).toBeInTheDocument();
+    expect(
+      await screen.findByRole("button", { name: /view/i }),
+    ).toBeInTheDocument();
   });
 
-  it("should show loading state when saving", () => {
+  it("should show loading state when saving", async () => {
     mockLoading = true;
     renderComponent();
 
-    expect(screen.getByRole("button", { name: /saving/i })).toBeInTheDocument();
+    expect(
+      await screen.findByRole("button", { name: /saving/i }),
+    ).toBeInTheDocument();
   });
 
-  it("should show error message if saving fails", () => {
+  it("should show error message if saving fails", async () => {
     mockError = new Error();
     renderComponent();
 
-    expect(screen.getByRole("button", { name: /error/i })).toBeInTheDocument();
+    expect(
+      await screen.findByRole("button", { name: /error/i }),
+    ).toBeInTheDocument();
+  });
+
+  it("should render the loading indicator when fetching the logo", async () => {
+    simulateDelay(
+      `${env.VITE_BASE_URL}/api/restaurants/${order.restaurant.id}/base64-logo`,
+    );
+    const { getLoadingText } = renderComponent();
+
+    expect(getLoadingText()).toBeInTheDocument();
+  });
+
+  it("should not render the loading indicator after fetching the logo", async () => {
+    const { getLoadingText } = renderComponent();
+
+    await waitForElementToBeRemoved(getLoadingText);
+  });
+
+  it("should not render the loading indicator if fetching the logo fails", async () => {
+    simulateError(
+      `${env.VITE_BASE_URL}/api/restaurants/${order.restaurant.id}/base64-logo`,
+    );
+    const { getLoadingText } = renderComponent();
+
+    await waitForElementToBeRemoved(getLoadingText);
+
+    expect(screen.getByRole("alert")).toBeInTheDocument();
   });
 });
