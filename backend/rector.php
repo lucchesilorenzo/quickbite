8daf01c4a0e4617083ccb4ef81c4aeeea4ebca2c
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-12-12 23:41:58 +0100

    chore: update rector config

--- backend/rector.php
+++ backend/rector.php
@@ -50,6 +50,5 @@ return RectorConfig::configure()
         typeDeclarations: true,
         privatization: true,
         earlyReturn: true,
-        strictBooleans: true,
     )
     ->withPhpSets();
