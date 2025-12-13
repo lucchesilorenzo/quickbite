9e31e16cb4acfc4a7c3b8b513e96a4cfd39c4a0d
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-12-13 22:47:57 +0100

    chore: refactor ESLint configuration for improved clarity and organization

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,48 +1,30 @@
 import js from "@eslint/js";
-import globals from "globals";
+import pluginQuery from "@tanstack/eslint-plugin-query";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
+import { defineConfig, globalIgnores } from "eslint/config";
+import globals from "globals";
 import tseslint from "typescript-eslint";
-import pluginQuery from "@tanstack/eslint-plugin-query";
 
-export default tseslint.config(
-  ...pluginQuery.configs["flat/recommended"],
-  { ignores: ["dist"] },
+export default defineConfig([
+  globalIgnores(["dist"]),
+  pluginQuery.configs["flat/recommended"],
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ["**/*.{ts,tsx}"],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+    ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
-    plugins: {
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
-    },
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      "react-refresh/only-export-components": [
-        "warn",
-        { allowConstantExport: true },
-      ],
       "@typescript-eslint/ban-ts-comment": "off",
       "react-refresh/only-export-components": "off",
+      "@typescript-eslint/no-explicit-any": "off",
     },
   },
-  {
-    files: ["**/*.test.{ts,tsx}", "tests/**/*.{ts,tsx}"],
-    plugins: { vitest },
-    rules: {
-      ...vitest.configs.recommended.rules,
-      "vitest/max-nested-describe": ["error", { max: 3 }],
-    },
-    settings: {
-      vitest: { typecheck: true },
-    },
-    languageOptions: {
-      globals: {
-        ...vitest.environments.env.globals,
-      },
-    },
-  }
-);
+]);
