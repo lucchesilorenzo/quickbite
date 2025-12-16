d135512839b915168e7bdbd52cc027d2fba4211e
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-12-16 19:34:21 +0100

    feat: add RiderProfileLayout component

---
+++ frontend/src/layouts/private/rider/RiderProfileLayout.tsx
@@ -0,0 +1,34 @@
+import { Container, Grid, Stack, useMediaQuery } from "@mui/material";
+import { grey } from "@mui/material/colors";
+import Header from "@rider/header/Header";
+import ProfileSidebar from "@rider/header/profile/ProfileSidebar";
+import { Outlet } from "react-router-dom";
+
+export default function RiderProfileLayout() {
+  const isMobile = useMediaQuery((theme) => theme.breakpoints.down("lg"));
+
+  return (
+    <Stack sx={{ minHeight: "100vh", bgcolor: grey[100] }}>
+      <Header />
+
+      <Container component="main" maxWidth="md" sx={{ my: 3 }}>
+        {!isMobile ? (
+          <Grid container>
+            <Grid
+              size={4}
+              sx={{ borderRight: 1, borderColor: "divider", pr: 4 }}
+            >
+              <ProfileSidebar />
+            </Grid>
+
+            <Grid size={8} sx={{ pl: 4 }}>
+              <Outlet />
+            </Grid>
+          </Grid>
+        ) : (
+          <Outlet />
+        )}
+      </Container>
+    </Stack>
+  );
+}
