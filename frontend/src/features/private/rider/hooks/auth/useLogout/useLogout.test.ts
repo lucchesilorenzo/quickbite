23de3b2c849f91511c15629855a0cac1761bac6b
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-12-16 11:05:32 +0100

    test: add test for useLogout hook

---
+++ frontend/src/features/private/rider/hooks/auth/useLogout/useLogout.test.ts
@@ -0,0 +1,69 @@
+import { renderHook, waitFor } from "@testing-library/react";
+import TestQueryWrapper from "@tests/TestQueryWrapper";
+import { simulateError } from "@tests/utils/msw";
+import { useNotifications } from "@toolpad/core/useNotifications";
+
+import { useLogout } from "./useLogout";
+
+import env from "@/lib/env";
+
+const mockNavigate = vi.fn();
+const mockShow = vi.fn();
+
+vi.mock("react-router-dom", async () => ({
+  ...(await vi.importActual("react-router-dom")),
+  useNavigate: () => mockNavigate,
+}));
+
+describe("useLogout", () => {
+  it("should mutate and return data", async () => {
+    const { result } = renderHook(() => useLogout(), {
+      wrapper: TestQueryWrapper,
+    });
+
+    result.current.mutate();
+
+    await waitFor(() => expect(result.current.isSuccess).toBeTruthy());
+
+    expect(result.current.data).toBeDefined();
+
+    expect(mockNavigate).toHaveBeenCalledWith("/rider/auth/login");
+    expect(mockNavigate).toHaveBeenCalledTimes(1);
+  });
+
+  it("should show notification on error", async () => {
+    vi.mocked(useNotifications).mockReturnValue({
+      show: mockShow,
+      close: vi.fn(),
+    });
+    simulateError(`${env.VITE_BASE_URL}/api/rider/auth/logout`, "post");
+
+    const { result } = renderHook(() => useLogout(), {
+      wrapper: TestQueryWrapper,
+    });
+
+    result.current.mutate();
+
+    await waitFor(() => expect(result.current.isError).toBeTruthy());
+
+    expect(result.current.error).toBeDefined();
+
+    expect(mockShow).toHaveBeenCalledWith(
+      expect.any(String),
+      expect.objectContaining({
+        key: "rider-logout-error",
+        severity: "error",
+      }),
+    );
+  });
+
+  it("should not show notification on success", () => {
+    const { result } = renderHook(() => useLogout(), {
+      wrapper: TestQueryWrapper,
+    });
+
+    result.current.mutate();
+
+    expect(mockShow).not.toHaveBeenCalled();
+  });
+});
