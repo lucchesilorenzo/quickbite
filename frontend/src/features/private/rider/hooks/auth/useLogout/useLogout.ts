a7cfd82ee2fcfde05437851334a04cdf6d717bf8
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-12-16 11:05:20 +0100

    feat: add useLogout hook

---
+++ frontend/src/features/private/rider/hooks/auth/useLogout/useLogout.ts
@@ -0,0 +1,28 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useNotifications } from "@toolpad/core/useNotifications";
+import { useNavigate } from "react-router-dom";
+
+import { postData } from "@/lib/api-client";
+import { ApiResponse } from "@/types/api.types";
+
+export function useLogout() {
+  const queryClient = useQueryClient();
+  const notifications = useNotifications();
+  const navigate = useNavigate();
+
+  return useMutation<ApiResponse, Error, void>({
+    mutationFn: () => postData("/rider/auth/logout"),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["auth"] });
+      localStorage.removeItem("token");
+
+      navigate("/rider/auth/login");
+    },
+    onError: (error) => {
+      notifications.show(error.message, {
+        key: "rider-logout-error",
+        severity: "error",
+      });
+    },
+  });
+}
