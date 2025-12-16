19532e408ead1538262918ec1c0134d86b878c65
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-12-16 15:51:44 +0100

    feat: add initial RiderLoginPage structure

--- frontend/src/pages/public/rider/RiderLoginPage.tsx
+++ frontend/src/pages/public/rider/RiderLoginPage.tsx
@@ -1,5 +1,17 @@
+import { useEffect } from "react";
+
 import { Box } from "@mui/material";
+import { grey } from "@mui/material/colors";
+import LoginFormContainer from "@rider/auth/login/LoginFormContainer";
 
 export default function RiderLoginPage() {
-  return <Box component="main">RiderLoginPage</Box>;
+  useEffect(() => {
+    document.title = "Rider login | QuickBite";
+  }, []);
+
+  return (
+    <Box component="main" sx={{ bgcolor: grey[100] }}>
+      <LoginFormContainer />
+    </Box>
+  );
 }
