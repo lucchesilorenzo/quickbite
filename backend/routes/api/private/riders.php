c4abb0ed00ba7ebe7978b7df527bd885c7172de7
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-12-16 12:16:54 +0100

    feat: add /api/rider/auth/login endpoint

--- backend/routes/api/private/riders.php
+++ backend/routes/api/private/riders.php
@@ -9,6 +9,7 @@ Route::prefix('rider')->group(function (): void {
     // === AUTH ===
     Route::prefix('auth')->group(function (): void {
         Route::post('/register', [AuthController::class, 'register']);
+        Route::post('/login', [AuthController::class, 'login']);
         Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum', 'role:rider');
     });
 });
