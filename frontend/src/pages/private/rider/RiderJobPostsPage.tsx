93636342fb1ab329f1555b54e147bbf4cac60f77
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-12-19 22:50:59 +0100

    feat: add title and import JobPostFilters in RiderJobPostsPage

--- frontend/src/pages/private/rider/RiderJobPostsPage.tsx
+++ frontend/src/pages/private/rider/RiderJobPostsPage.tsx
@@ -1,3 +1,16 @@
+import { useEffect } from "react";
+
+import { Container } from "@mui/material";
+import JobPostFilters from "@private/rider/job-posts/JobPostFilters/JobPostFilters";
+
 export default function RiderJobPostsPage() {
-  return <div>RiderJobPostsPage</div>;
+  useEffect(() => {
+    document.title = "Job posts | QuickBite";
+  }, []);
+
+  return (
+    <Container component="main" maxWidth="md" sx={{ my: 3 }}>
+      <JobPostFilters />
+    </Container>
+  );
 }
