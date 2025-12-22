ac95a52905a2fd7cd652bc02ebcfbde21ab5f869
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-12-22 10:35:05 +0100

    feat: import JobPostsProvider into RiderLayout

--- frontend/src/layouts/private/rider/RiderLayout.tsx
+++ frontend/src/layouts/private/rider/RiderLayout.tsx
@@ -1,17 +1,20 @@
 import { Stack } from "@mui/material";
+import JobPostsProvider from "@rider/contexts/JobPostsProvider";
+import Header from "@rider/header/Header";
 import { Outlet } from "react-router-dom";
 
 import Footer from "@/components/common/Footer";
-import Header from "@/features/private/rider/header/Header";
 
 export default function RiderLayout() {
   return (
-    <Stack sx={{ minHeight: "100vh" }}>
-      <Header />
+    <JobPostsProvider>
+      <Stack sx={{ minHeight: "100vh" }}>
+        <Header />
 
-      <Outlet />
+        <Outlet />
 
-      <Footer />
-    </Stack>
+        <Footer />
+      </Stack>
+    </JobPostsProvider>
   );
 }
