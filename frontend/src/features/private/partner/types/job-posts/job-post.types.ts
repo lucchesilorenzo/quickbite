c5cdc28514328c1b5937eafc3afb6d9b1fd476c3
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-12-22 10:31:20 +0100

    refactor: move JobPost type to shared types

--- frontend/src/features/private/partner/types/job-posts/job-post.types.ts
+++ frontend/src/features/private/partner/types/job-posts/job-post.types.ts
@@ -1,22 +1,6 @@
-import {
-  EmploymentType,
-  JobPostStatus,
-} from "@private/shared/types/job-posts/job-post.types";
+import { JobPost } from "@private/shared/types/job-posts/job-post.types";
 
 import { BaseOffsetPagination } from "@/types/pagination.types";
-
-export type JobPost = {
-  id: string;
-  restaurant_id: string;
-  title: string;
-  description_html: string;
-  description_text: string;
-  employment_type: EmploymentType;
-  salary: number | null;
-  status: JobPostStatus;
-  created_at: string;
-  updated_at: string;
-};
 
 export type JobPostWithApplicationsCount = JobPost & {
   job_applications_count: number;
