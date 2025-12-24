0ae546b5481dc67fe1b3d4340214f535fc93d4b5
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-12-24 11:50:46 +0100

    refactor: update JobPostsWithRestaurantAndPagination type to use BaseCursorPagination instead of BaseOffsetPagination

--- frontend/src/features/private/rider/types/job-posts/job-post.types.ts
+++ frontend/src/features/private/rider/types/job-posts/job-post.types.ts
@@ -1,12 +1,12 @@
 import { JobPost } from "@private/shared/types/job-posts/job-post.types";
 
-import { BaseOffsetPagination } from "@/types/pagination.types";
+import { BaseCursorPagination } from "@/types/pagination.types";
 import { BaseRestaurant } from "@/types/restaurants/restaurant.types";
 
 export type JobPostWithRestaurant = JobPost & {
   restaurant: BaseRestaurant;
 };
 
-export type JobPostsWithRestaurantAndPagination = BaseOffsetPagination & {
+export type JobPostsWithRestaurantAndPagination = BaseCursorPagination & {
   data: JobPostWithRestaurant[];
 };
