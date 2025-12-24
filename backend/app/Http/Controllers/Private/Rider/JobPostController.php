bc760141f88e23556bbcaa2c23c8d4057de79838
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-12-25 00:59:07 +0100

    feat: add getJobPost method in JobPostController

--- backend/app/Http/Controllers/Private/Rider/JobPostController.php
+++ backend/app/Http/Controllers/Private/Rider/JobPostController.php
@@ -6,6 +6,7 @@ namespace App\Http\Controllers\Private\Rider;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Private\Rider\JobPost\GetJobPostsRequest;
+use App\Models\JobPost;
 use App\Services\Private\Rider\JobPostService;
 use Dedoc\Scramble\Attributes\Group;
 use Illuminate\Http\JsonResponse;
@@ -18,6 +19,9 @@ class JobPostController extends Controller
         private readonly JobPostService $jobPostService
     ) {}
 
+    /**
+     * Get all job posts.
+     */
     public function getJobPosts(GetJobPostsRequest $request): JsonResponse
     {
         try {
@@ -34,6 +38,29 @@ class JobPostController extends Controller
             return response()->json([
                 'success' => false,
                 'message' => 'Could not get job posts.',
+            ], 500);
+        }
+    }
+
+    /**
+     * Get job post.
+     */
+    public function getJobPost(JobPost $jobPost): JsonResponse
+    {
+        try {
+            $jobPost = $this->jobPostService->getJobPost(
+                $jobPost
+            );
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Job post retrieved successfully.',
+                'job_post' => $jobPost,
+            ], 200);
+        } catch (Throwable) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Could not get job post.',
             ], 500);
         }
     }
