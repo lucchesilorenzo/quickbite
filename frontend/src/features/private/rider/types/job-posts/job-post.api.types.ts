e2879c63323cf7d61b002b1b3726891e937302a2
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-12-25 01:01:08 +0100

    feat: add GetJobPostResponse type

--- frontend/src/features/private/rider/types/job-posts/job-post.api.types.ts
+++ frontend/src/features/private/rider/types/job-posts/job-post.api.types.ts
@@ -1,7 +1,14 @@
-import { JobPostsWithRestaurantAndPagination } from "./job-post.types";
+import {
+  JobPostWithRestaurant,
+  JobPostsWithRestaurantAndPagination,
+} from "./job-post.types";
 
 import { ApiResponse } from "@/types/api.types";
 
 export type GetJobPostsResponse = {
   job_posts: JobPostsWithRestaurantAndPagination;
+} & ApiResponse;
+
+export type GetJobPostResponse = {
+  job_post: JobPostWithRestaurant;
 } & ApiResponse;
