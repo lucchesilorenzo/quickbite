704063105be8d2b7acce9f605514d8a2f9fcdbc6
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-12-27 12:28:12 +0100

    refactor: replace mock data with real data in JobPostDetails

--- frontend/src/features/private/rider/job-posts/JobPostDetails/JobPostDetails.tsx
+++ frontend/src/features/private/rider/job-posts/JobPostDetails/JobPostDetails.tsx
@@ -1,6 +1,5 @@
 import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
 import CheckIcon from "@mui/icons-material/Check";
-import OpenInNewIcon from "@mui/icons-material/OpenInNew";
 import PaymentsIcon from "@mui/icons-material/Payments";
 import {
   Box,
@@ -10,49 +9,46 @@ import {
   CardHeader,
   Chip,
   Divider,
-  Link as MuiLink,
   Stack,
   Typography,
 } from "@mui/material";
-import { Link } from "react-router-dom";
+import { employmentTypes } from "@private/shared/lib/constants/job-posts";
+import { JobPostWithRestaurant } from "@rider/types/job-posts/job-post.types";
 
-export default function JobPostDetails() {
+import JobPostDescription from "../JobPostDescription";
+
+import { formatCurrency } from "@/lib/utils/formatting";
+
+type JobPostDetailsProps = {
+  jobPost?: JobPostWithRestaurant;
+};
+
+export default function JobPostDetails({ jobPost }: JobPostDetailsProps) {
+  const employmentType = employmentTypes.find(
+    (option) => option.value === jobPost?.employment_type,
+  )?.label;
+
   return (
     <Card variant="outlined">
       <CardHeader
         title={
           <Typography variant="h5" sx={{ my: 1, fontWeight: 500 }}>
-            Full Stack Developer
+            {jobPost?.title}
           </Typography>
         }
         subheader={
           <Box>
-            <MuiLink
-              component={Link}
-              to="/"
-              underline="hover"
-              gutterBottom
-              color="textSecondary"
-              sx={{
-                display: "inline-flex",
-                alignItems: "center",
-                textDecoration: "underline",
-                gap: 0.5,
-                "&:hover": { color: "info.main" },
-              }}
-            >
-              <Typography variant="body2">Company name</Typography>
-
-              <OpenInNewIcon fontSize="inherit" />
-            </MuiLink>
+            <Typography variant="body2">{jobPost?.restaurant.name}</Typography>
 
             <Typography variant="body2" color="textSecondary" gutterBottom>
-              Postcode City
+              {jobPost?.restaurant.postcode} - {jobPost?.restaurant.city}
             </Typography>
 
-            <Typography variant="body1" color="textPrimary" gutterBottom>
-              Salary
-            </Typography>
+            {jobPost?.salary && (
+              <Typography variant="body1" color="textPrimary" gutterBottom>
+                {formatCurrency(jobPost.salary)} / year
+              </Typography>
+            )}
 
             <Button variant="contained" color="info">
               Apply now
@@ -74,19 +70,21 @@ export default function JobPostDetails() {
               <Stack direction="row" spacing={1}>
                 <PaymentsIcon fontSize="inherit" />
 
-                <Stack spacing={1}>
-                  <Typography variant="body2" sx={{ fontWeight: 500 }}>
-                    Salary
-                  </Typography>
+                {jobPost?.salary && (
+                  <Stack spacing={1}>
+                    <Typography variant="body2" sx={{ fontWeight: 500 }}>
+                      Salary
+                    </Typography>
 
-                  <Chip
-                    icon={<CheckIcon />}
-                    color="success"
-                    label="30.000 € a year"
-                    size="small"
-                    sx={{ fontWeight: 500 }}
-                  />
-                </Stack>
+                    <Chip
+                      icon={<CheckIcon />}
+                      color="success"
+                      label={`${formatCurrency(jobPost.salary)} / year`}
+                      size="small"
+                      sx={{ fontWeight: 500 }}
+                    />
+                  </Stack>
+                )}
               </Stack>
 
               <Stack direction="row" spacing={1}>
@@ -100,7 +98,7 @@ export default function JobPostDetails() {
                   <Chip
                     icon={<CheckIcon />}
                     color="success"
-                    label="Full-time"
+                    label={employmentType}
                     size="small"
                     sx={{ fontWeight: 500 }}
                   />
@@ -118,46 +116,7 @@ export default function JobPostDetails() {
               Complete description of the position
             </Typography>
 
-            <Typography variant="body1" gutterBottom>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit
-              dolores vitae fugit reprehenderit aut dignissimos commodi ea ipsa
-              molestias aliquid quibusdam, assumenda quaerat obcaecati
-              consequatur repellat tenetur nemo! Dolor, nam distinctio enim nemo
-              voluptatibus excepturi consequuntur sequi ea quisquam temporibus?
-              Unde optio nobis, quam nesciunt harum eos. Earum iusto, laborum
-              fugit at repellat perspiciatis, quibusdam illo ut reprehenderit
-              porro consectetur. Perferendis aliquam distinctio consequuntur
-              pariatur ipsum eius, neque iusto blanditiis molestias asperiores!
-              Quod facere voluptatibus blanditiis culpa sunt ipsa. Aut
-              repellendus commodi placeat eum cum accusantium earum at
-              consequuntur suscipit molestiae aliquid dolore ex tenetur dicta
-              laudantium repellat tempora reiciendis magni, maiores, rem officia
-              eveniet. Magni incidunt explicabo eligendi. Repellendus
-              necessitatibus quaerat, dolores repellat quis corporis animi
-              similique in iste voluptas rem dignissimos, commodi excepturi
-              dicta facilis quod at. Et cum perspiciatis, modi nulla assumenda
-              rerum laboriosam magni impedit, est veniam delectus sunt expedita
-              nobis eos repellendus praesentium ab rem alias ea suscipit
-              similique earum. Alias voluptates doloremque temporibus neque est
-              at rerum necessitatibus nesciunt, commodi voluptatum magnam dolore
-              optio quibusdam magni vero rem tempora facere omnis esse veniam
-              quasi voluptatem nisi? Recusandae alias ab modi, impedit
-              asperiores laboriosam illum culpa dignissimos debitis molestiae
-              saepe quam, voluptatum accusamus enim eligendi nam excepturi.
-              Corporis debitis nam impedit velit repellat unde id amet. Quo quis
-              repellendus voluptas explicabo beatae animi necessitatibus
-              doloremque? Praesentium optio autem, ipsam impedit cupiditate
-              commodi unde numquam harum fuga, dolor sunt magnam consequatur
-              nostrum nesciunt incidunt rem assumenda totam earum doloremque
-              similique debitis voluptatum suscipit. Labore iste nobis
-              perspiciatis? Doloremque quasi ad facilis explicabo officiis illum
-              necessitatibus architecto repudiandae aspernatur adipisci laborum
-              nostrum exercitationem ducimus, asperiores provident sint labore
-              iste hic rem, similique obcaecati commodi expedita? Quidem,
-              incidunt illum molestiae cum magnam fugit necessitatibus
-              laboriosam adipisci aliquid labore, quos nisi ea rem, consequuntur
-              ratione impedit temporibus facilis iure!
-            </Typography>
+            <JobPostDescription description={jobPost?.description_html} />
           </Box>
         </CardContent>
       </Box>
