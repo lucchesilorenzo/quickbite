f914caa9ea37869c6fc00c6abfbad5336a80cf50
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-12-30 16:24:02 +0100

    test: add vehicle type detail in JobPostDetails

--- frontend/src/features/private/rider/job-posts/JobPostDetails/JobPostDetails.test.tsx
+++ frontend/src/features/private/rider/job-posts/JobPostDetails/JobPostDetails.test.tsx
@@ -1,11 +1,11 @@
+import { employmentTypes } from "@private/shared/lib/constants/job-posts";
+import { vehicles } from "@private/shared/lib/constants/vehicles";
 import { screen, within } from "@testing-library/react";
 import { jobPostsWithRestaurant } from "@tests/mocks/data/private/rider/job-posts";
 import { customRender } from "@tests/utils/custom-render";
 
 import { JobPostWithRestaurant } from "../../types/job-posts/job-post.types";
 import JobPostDetails from "./JobPostDetails";
-
-import { employmentTypes } from "@/features/private/shared/lib/constants/job-posts";
 
 vi.mock("../JobPostDescription", () => ({
   default: () => <div data-testid="job-post-description" />,
@@ -65,11 +65,18 @@ describe("JobPostDetails", () => {
         (option) => option.value === jobPostsWithRestaurant[0].employment_type,
       )?.label;
 
+      const vehicleType = vehicles.find(
+        (option) => option.value === jobPostsWithRestaurant[0].vehicle_type,
+      )?.label;
+
       expect(
         within(jobPostDetails).queryByText(/\/ year/i),
       ).not.toBeInTheDocument();
       expect(
         within(jobPostDetails).getByText(employmentType!),
+      ).toBeInTheDocument();
+      expect(
+        within(jobPostDetails).getByText(vehicleType!),
       ).toBeInTheDocument();
       expect(
         within(jobPostDetails).getByTestId("job-post-description"),
