04d544166dbe9fe4765230d54621f4a7e5bd10c4
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-12-31 16:59:05 +0100

    feat: add RiderJobPostApplicationPage route and page

---
+++ frontend/src/pages/private/rider/RiderJobPostApplicationPage.tsx
@@ -0,0 +1,19 @@
+import { useEffect } from "react";
+
+import { Box } from "@mui/material";
+import JobPostApplicationProvider from "@rider/contexts/JobPostApplicationProvider";
+import JobApplicationWizard from "@rider/job-post-application/JobApplicationWizard";
+
+export default function RiderJobPostApplicationPage() {
+  useEffect(() => {
+    document.title = "Job post application | QuickBite";
+  }, []);
+
+  return (
+    <JobPostApplicationProvider>
+      <Box component="main">
+        <JobApplicationWizard />
+      </Box>
+    </JobPostApplicationProvider>
+  );
+}
