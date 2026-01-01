453c69b125647791997ec6338618a3bffc0ef7bc
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2026-01-02 00:20:43 +0100

    feat: add JobApplication type

---
+++ frontend/src/features/private/rider/types/job-posts/job-applications/job-application.types.ts
@@ -0,0 +1,14 @@
+export type JobApplication = {
+  id: string;
+  job_post_id: string;
+  rider_id: string;
+  first_name: string;
+  last_name: string;
+  email: string;
+  phone_number: string;
+  resume: string;
+  status: string;
+  declaration_accepted_at: string;
+  created_at: string;
+  updated_at: string;
+};
