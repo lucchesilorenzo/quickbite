0f614f277d35affae7ef3e4e8f70b23dfbf9373c
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2026-01-02 22:44:50 +0100

    feat: add createJobApplication method in JobApplicationController

--- backend/app/Http/Controllers/Private/Rider/JobApplicationController.php
+++ backend/app/Http/Controllers/Private/Rider/JobApplicationController.php
@@ -5,10 +5,44 @@ declare(strict_types=1);
 namespace App\Http\Controllers\Private\Rider;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Private\Rider\JobApplication\CreateJobApplicationRequest;
+use App\Models\JobPost;
+use App\Services\Private\Rider\JobApplicationService;
 use Dedoc\Scramble\Attributes\Group;
+use Illuminate\Http\JsonResponse;
+use Throwable;
 
 #[Group('Rider Job Applications')]
 class JobApplicationController extends Controller
 {
-    //
+    public function __construct(
+        private readonly JobApplicationService $jobApplicationService
+    ) {}
+
+    /**
+     * Create a job application.
+     */
+    public function createJobApplication(
+        CreateJobApplicationRequest $request,
+        JobPost $jobPost
+    ): JsonResponse {
+        try {
+            $jobApplication = $this->jobApplicationService->createJobApplication(
+                auth()->user(),
+                $request->validated(),
+                $jobPost
+            );
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Job application created successfully.',
+                'job_application' => $jobApplication,
+            ], 201);
+        } catch (Throwable) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Could not create job application.',
+            ], 500);
+        }
+    }
 }
