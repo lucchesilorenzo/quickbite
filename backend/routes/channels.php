275e86ddc289271e70b8107380cda8cbec039b20
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2026-01-08 18:13:51 +0100

    feat: integrate App.Models.User.{userId} private channel in channels.php

--- backend/routes/channels.php
+++ backend/routes/channels.php
@@ -3,6 +3,8 @@
 declare(strict_types=1);
 
 use App\Broadcasting\PartnerChannel;
+use App\Broadcasting\UserChannel;
 use Illuminate\Support\Facades\Broadcast;
 
+Broadcast::channel('App.Models.User.{userId}', UserChannel::class);
 Broadcast::channel('App.Models.User.{userId}.Restaurant.{restaurantId}', PartnerChannel::class);
