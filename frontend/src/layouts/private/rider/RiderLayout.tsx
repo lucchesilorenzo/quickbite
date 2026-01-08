346e7cdbdad753ceecc0bdf379f7ab6bf66d8dc9
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2026-01-08 18:20:36 +0100

    feat: implement real-time notifications for new deliveries in RiderLayout

--- frontend/src/layouts/private/rider/RiderLayout.tsx
+++ frontend/src/layouts/private/rider/RiderLayout.tsx
@@ -1,10 +1,49 @@
+import { useEffect } from "react";
+
+import { useEchoNotification } from "@laravel/echo-react";
 import { Stack } from "@mui/material";
 import JobPostsProvider from "@rider/contexts/JobPostsProvider";
 import NotificationsProvider from "@rider/contexts/NotificationsProvider";
 import Header from "@rider/header/Header";
+import { NewDeliveryReceivedToBroadcast } from "@rider/types/notifications/notification.types";
+import { useQueryClient } from "@tanstack/react-query";
+import { useNotifications } from "@toolpad/core/useNotifications";
 import { Outlet } from "react-router-dom";
 
+import NotificationToast from "@/components/common/NotificationToast";
+import { useAuth } from "@/contexts/AuthProvider";
+
 export default function RiderLayout() {
+  const { user } = useAuth();
+
+  const notifications = useNotifications();
+  const queryClient = useQueryClient();
+
+  const { leave: leaveDelivery } =
+    useEchoNotification<NewDeliveryReceivedToBroadcast>(
+      user?.id ? `App.Models.User.${user.id}` : "",
+      (notification) => {
+        notifications.show(
+          <NotificationToast
+            title={notification.title}
+            description={notification.description}
+          />,
+          { severity: "info" },
+        );
+
+        queryClient.invalidateQueries({
+          queryKey: ["rider-notifications", user?.id, 1],
+        });
+      },
+      "new.delivery.received",
+    );
+
+  useEffect(() => {
+    return () => {
+      leaveDelivery();
+    };
+  }, [leaveDelivery]);
+
   return (
     <NotificationsProvider>
       <JobPostsProvider>
