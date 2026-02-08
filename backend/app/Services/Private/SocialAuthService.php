6d6843db9039740224ab195795b05be317c0e265
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2026-02-08 15:17:46 +0100

    feat: add handleSocialLogin method in SocialAuthService

---
+++ backend/app/Services/Private/SocialAuthService.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services\Private;
+
+use App\Enums\UserRole;
+use App\Models\User;
+use Laravel\Socialite\Contracts\User as SocialiteUser;
+
+class SocialAuthService
+{
+    public function handleSocialLogin(SocialiteUser $providerUser, string $provider): string
+    {
+        $user = User::query()->where('email', $providerUser->getEmail())->first();
+
+        if (! $user) {
+            $user = User::query()->create([
+                'first_name' => explode(' ', (string) $providerUser->getName())[0],
+                'last_name' => explode(' ', (string) $providerUser->getName())[1],
+                'email' => $providerUser->getEmail(),
+                'profile_picture' => $providerUser->getAvatar(),
+            ]);
+        }
+
+        $user->socialProviders()->firstOrCreate([
+            'provider' => $provider,
+            'provider_id' => $providerUser->getId(),
+        ]);
+
+        $user->assignRole(UserRole::CUSTOMER);
+
+        return $user->createToken('customer_web_token')->plainTextToken;
+    }
+}
