944f8be97854830bda1bfd667fed9d4556ff9f85
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2026-02-14 23:02:42 +0100

    feat: show loading indicator and handle visible route logic in HeaderDrawer

--- frontend/src/features/private/rider/header/HeaderDrawer.tsx
+++ frontend/src/features/private/rider/header/HeaderDrawer.tsx
@@ -1,13 +1,22 @@
 import { useState } from "react";
 
 import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
-import { Box, Drawer, IconButton, List } from "@mui/material";
+import { Box, Drawer, IconButton, List, Skeleton, Stack } from "@mui/material";
 
-import { riderUnemployedRoutes } from "../lib/constants/navigation";
+import { useRestaurant } from "../contexts/RestaurantProvider";
+import { riderRoutes } from "../lib/constants/navigation";
 import NavigationItemMobile from "./mobile/NavigationItemMobile";
 
 export default function HeaderDrawer() {
+  const { restaurantData, isLoadingRestaurant } = useRestaurant();
+
   const [openDrawer, setOpenDrawer] = useState(false);
+
+  const visibleRoutes = riderRoutes.filter(
+    ({ href }) =>
+      href !== "/rider/my-restaurant" ||
+      restaurantData?.restaurant?.pivot.is_active,
+  );
 
   return (
     <Box>
@@ -17,13 +26,26 @@ export default function HeaderDrawer() {
 
       <Drawer open={openDrawer} onClose={() => setOpenDrawer(false)}>
         <List dense sx={{ width: 200 }}>
-          {riderUnemployedRoutes.map((route) => (
-            <NavigationItemMobile
-              key={route.href}
-              route={route}
-              setOpenDrawer={setOpenDrawer}
-            />
-          ))}
+          {isLoadingRestaurant ? (
+            <Stack aria-busy="true" aria-label="Loading tabs" spacing={2}>
+              {Array.from({ length: 2 }).map((_, index) => (
+                <Skeleton
+                  key={index}
+                  variant="rectangular"
+                  animation="wave"
+                  height={40}
+                />
+              ))}
+            </Stack>
+          ) : (
+            visibleRoutes.map((route) => (
+              <NavigationItemMobile
+                key={route.href}
+                route={route}
+                setOpenDrawer={setOpenDrawer}
+              />
+            ))
+          )}
         </List>
       </Drawer>
     </Box>
