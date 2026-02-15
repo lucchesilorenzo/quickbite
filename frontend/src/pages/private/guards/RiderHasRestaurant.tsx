b7a90edfa87cbb35268f5a3a0cae31a7ee692182
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2026-02-15 21:52:07 +0100

    refactor: replace null with Spinner component in RiderHasRestaurant

--- frontend/src/pages/private/guards/RiderHasRestaurant.tsx
+++ frontend/src/pages/private/guards/RiderHasRestaurant.tsx
@@ -2,10 +2,14 @@ import { useRestaurant } from "@rider/contexts/RestaurantProvider";
 import { riderRoutes } from "@rider/lib/constants/navigation";
 import { Navigate, Outlet } from "react-router-dom";
 
+import Spinner from "@/components/common/Spinner";
+
 export default function RiderHasRestaurant() {
   const { restaurantData, isLoadingRestaurant } = useRestaurant();
 
-  if (isLoadingRestaurant) return null;
+  if (isLoadingRestaurant) {
+    return <Spinner />;
+  }
 
   const isRouteVisible = riderRoutes.some(
     ({ href }) =>
