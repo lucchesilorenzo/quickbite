2e22f5d99478771906b099d63432650f1d9a11d5
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2026-02-15 11:27:14 +0100

    refactor: ensure RestaurantDescription is only displayed if description is available in InfoTab

--- frontend/src/features/public/restaurant/restaurant-overview/tabs/info/InfoTab.tsx
+++ frontend/src/features/public/restaurant/restaurant-overview/tabs/info/InfoTab.tsx
@@ -5,13 +5,17 @@ import RestaurantDeliveryTimes from "./RestaurantDeliveryTimes";
 import RestaurantDescription from "./RestaurantDescription";
 import RestaurantLocationDisplay from "./RestaurantLocationDisplay";
 
+import { useRestaurant } from "@/contexts/RestaurantProvider";
+
 export default function InfoTab() {
+  const { restaurantData } = useRestaurant();
+
   return (
     <Box>
       <RestaurantLocationDisplay />
 
       <Box sx={{ p: 2 }}>
-        <RestaurantDescription />
+        {restaurantData.restaurant.description && <RestaurantDescription />}
         <RestaurantDeliveryTimes />
         <RestaurantDeliveryFee />
       </Box>
