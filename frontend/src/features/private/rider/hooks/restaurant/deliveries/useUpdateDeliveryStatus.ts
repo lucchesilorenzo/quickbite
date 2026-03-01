f6a733a7692c53ee80932a047211b6a156b49c35
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2026-03-01 18:45:40 +0100

    feat: add useUpdateDeliveryStatus hook

---
+++ frontend/src/features/private/rider/hooks/restaurant/deliveries/useUpdateDeliveryStatus.ts
@@ -0,0 +1,49 @@
+import {
+  UpdateDeliveryStatusPayload,
+  UpdateDeliveryStatusResponse,
+} from "@rider/types/deliveries/delivery.api.types";
+import { OrderStatus } from "@rider/types/orders/order.types";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useNotifications } from "@toolpad/core/useNotifications";
+
+import { updateData } from "@/lib/api-client";
+
+type UseUpdateDeliveryStatusOptions = {
+  deliveryId: string;
+  setSelectedStatus: React.Dispatch<React.SetStateAction<OrderStatus>>;
+};
+
+export function useUpdateDeliveryStatus({
+  deliveryId,
+  setSelectedStatus,
+}: UseUpdateDeliveryStatusOptions) {
+  const queryClient = useQueryClient();
+  const notifications = useNotifications();
+
+  return useMutation<
+    UpdateDeliveryStatusResponse,
+    Error,
+    UpdateDeliveryStatusPayload
+  >({
+    mutationFn: (data) =>
+      updateData(`/rider/restaurant/deliveries/${deliveryId}/status`, data),
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({
+        queryKey: ["rider-deliveries"],
+      });
+
+      setSelectedStatus(data.delivery.order.status);
+
+      notifications.show(data.message, {
+        key: "rider-update-delivery-status-success",
+        severity: "success",
+      });
+    },
+    onError: (error) => {
+      notifications.show(error.message, {
+        key: "rider-update-delivery-status-error",
+        severity: "error",
+      });
+    },
+  });
+}
