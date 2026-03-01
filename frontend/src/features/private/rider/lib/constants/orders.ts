1e643f03800c55fd0bd86580603c50a76c4f72c9
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2026-03-01 18:45:31 +0100

    feat: add orders data

---
+++ frontend/src/features/private/rider/lib/constants/orders.ts
@@ -0,0 +1,15 @@
+import { OrderStatus } from "../../types/orders/order.types";
+
+export const orderStatuses = {
+  preparing: { value: "delivering", label: "Start delivery" },
+  delivering: { value: "delivered", label: "Flag as delivered" },
+  delivered: { value: "", label: "Delivery has been completed" },
+  cancelled: { value: "cancelled", label: "Delivery has been cancelled" },
+};
+
+export const statusTransitions: Record<OrderStatus, OrderStatus[]> = {
+  preparing: ["delivering", "cancelled"],
+  delivering: [],
+  delivered: [],
+  cancelled: [],
+};
