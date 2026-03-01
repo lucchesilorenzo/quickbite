bd51997fd1d64e186924f37df01e4d3231ac9cae
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2026-03-01 18:43:23 +0100

    feat: add updateDeliveryStatus method with policy checks in DeliveryController

--- backend/app/Http/Controllers/Private/Rider/DeliveryController.php
+++ backend/app/Http/Controllers/Private/Rider/DeliveryController.php
@@ -4,11 +4,17 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers\Private\Rider;
 
+use App\Exceptions\Private\Rider\InvalidDeliveryStatusException;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Private\Rider\Delivery\UpdateDeliveryStatusRequest;
+use App\Models\Delivery;
 use App\Services\Private\Rider\DeliveryService;
+use Dedoc\Scramble\Attributes\Group;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Gate;
 use Throwable;
 
+#[Group('Rider Deliveries')]
 class DeliveryController extends Controller
 {
     public function __construct(
@@ -20,6 +26,8 @@ class DeliveryController extends Controller
      */
     public function getDeliveries(): JsonResponse
     {
+        Gate::authorize('viewAny', Delivery::class);
+
         try {
             $deliveries = $this->deliveryService->getDeliveries(
                 auth()->user()
@@ -34,6 +42,39 @@ class DeliveryController extends Controller
             return response()->json([
                 'success' => false,
                 'message' => 'Could not get deliveries.',
+            ], 500);
+        }
+    }
+
+    /**
+     * Update delivery status.
+     */
+    public function updateDeliveryStatus(
+        UpdateDeliveryStatusRequest $request,
+        Delivery $delivery
+    ): JsonResponse {
+        Gate::authorize('update', [Delivery::class, $delivery]);
+
+        try {
+            $delivery = $this->deliveryService->updateDeliveryStatus(
+                $request->validated(),
+                $delivery,
+            );
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Delivery updated successfully.',
+                'delivery' => $delivery,
+            ], 200);
+        } catch (InvalidDeliveryStatusException $e) {
+            return response()->json([
+                'success' => false,
+                'message' => $e->getMessage(),
+            ], $e->getCode());
+        } catch (Throwable) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Could not update delivery.',
             ], 500);
         }
     }
