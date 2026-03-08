c1282867dcbd8da377525c9c1144a51f1d27db0b
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2026-03-08 14:49:25 +0100

    refactor: update guard logic in RiderHasRestaurant

--- frontend/src/pages/private/guards/RiderHasRestaurant.tsx
+++ frontend/src/pages/private/guards/RiderHasRestaurant.tsx
@@ -2,22 +2,16 @@ import { useRestaurant } from "@rider/contexts/RestaurantProvider";
 import { Navigate, Outlet } from "react-router-dom";
 
 import Spinner from "@/components/common/Spinner";
-import { riderRoutes } from "@/features/private/rider/lib/data/navigation.data";
 
 export default function RiderHasRestaurant() {
-  const { restaurantData, isLoadingRestaurant } = useRestaurant();
+  const { restaurantData, isLoadingRestaurant, restaurantError } =
+    useRestaurant();
 
   if (isLoadingRestaurant) {
     return <Spinner />;
   }
 
-  const isRouteVisible = riderRoutes.some(
-    ({ href }) =>
-      href === "/rider/my-restaurant" &&
-      restaurantData?.restaurant?.pivot.is_active,
-  );
-
-  if (!isRouteVisible) {
+  if (restaurantError || !restaurantData?.restaurant?.pivot?.is_active) {
     return <Navigate to="/rider/job-posts" replace />;
   }
 
