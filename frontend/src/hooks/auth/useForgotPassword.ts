28265edbbc8870c242aca0082bdac0a4ea495211
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2026-03-21 11:36:58 +0100

    feat: add useForgotPassword hook

---
+++ frontend/src/hooks/auth/useForgotPassword.ts
@@ -0,0 +1,36 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useNotifications } from "@toolpad/core/useNotifications";
+
+import { postData } from "@/lib/api-client";
+import { ApiResponse } from "@/types/api.types";
+import { ForgotPasswordPayload } from "@/types/auth/auth.api.types";
+
+type UseForgotPasswordOptions = {
+  setOpenForgotPasswordDialog: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+export function useForgotPassword({
+  setOpenForgotPasswordDialog,
+}: UseForgotPasswordOptions) {
+  const queryClient = useQueryClient();
+  const notifications = useNotifications();
+
+  return useMutation<ApiResponse, Error, ForgotPasswordPayload>({
+    mutationFn: (data) => postData("/auth/forgot-password", data),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["auth"] });
+      setOpenForgotPasswordDialog(false);
+
+      notifications.show("Email sent successfully.", {
+        key: "forgot-password-success",
+        severity: "success",
+      });
+    },
+    onError: (error) => {
+      notifications.show(error.message, {
+        key: "forgot-password-error",
+        severity: "error",
+      });
+    },
+  });
+}
