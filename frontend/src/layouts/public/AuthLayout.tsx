4072deb33bd36eda14a1b1867a107d91c19b1fb2
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2026-03-28 17:14:46 +0100

    feat: add AuthHeader and AuthHeaderDialog components

--- frontend/src/layouts/public/AuthLayout.tsx
+++ frontend/src/layouts/public/AuthLayout.tsx
@@ -1,14 +1,14 @@
 import { Box, Stack } from "@mui/material";
 import { grey } from "@mui/material/colors";
+import AuthHeader from "@public/auth/components/AuthHeader";
 import { Outlet } from "react-router-dom";
 
 import Footer from "@/components/common/Footer";
-import Header from "@/components/common/Header";
 
 export default function AuthLayout() {
   return (
     <Stack sx={{ minHeight: "100vh" }}>
-      <Header />
+      <AuthHeader />
 
       <Box
         sx={{
