aede28e245e969d6a12d18e0601a2c46f123b022
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2026-03-28 17:13:22 +0100

    refactor: improve tanstack query hooks

--- frontend/src/features/private/customer/hooks/carts/useGetCarts.ts
+++ frontend/src/features/private/customer/hooks/carts/useGetCarts.ts
@@ -2,16 +2,16 @@ import { useQuery } from "@tanstack/react-query";
 
 import { GetCartsResponse } from "../../types/carts/cart.api.types";
 
+import { useAuth } from "@/contexts/AuthProvider";
 import { fetchData } from "@/lib/api-client";
+import { isCustomer } from "@/lib/utils/auth.utils";
 
-type UseGetCartsOptions = {
-  isCustomer: boolean;
-};
+export function useGetCarts() {
+  const { user } = useAuth();
 
-export function useGetCarts({ isCustomer }: UseGetCartsOptions) {
   return useQuery<GetCartsResponse>({
     queryKey: ["customer-carts"],
     queryFn: () => fetchData("/customer/carts"),
-    enabled: !!isCustomer,
+    enabled: isCustomer(user) && !!user.email_verified_at,
   });
 }
--- frontend/src/features/private/customer/hooks/orders/useGetOrders.ts
+++ frontend/src/features/private/customer/hooks/orders/useGetOrders.ts
@@ -2,15 +2,20 @@ import { useQuery } from "@tanstack/react-query";
 
 import { GetOrdersResponse } from "../../types/orders/order.api.types";
 
+import { useAuth } from "@/contexts/AuthProvider";
 import { fetchData } from "@/lib/api-client";
+import { isCustomer } from "@/lib/utils/auth.utils";
 
 type UseGetOrdersOptions = {
   page?: number;
 };
 
 export function useGetOrders({ page = 1 }: UseGetOrdersOptions) {
+  const { user } = useAuth();
+
   return useQuery<GetOrdersResponse>({
     queryKey: ["customer-orders", page],
     queryFn: () => fetchData(`/customer/orders?page=${page}`),
+    enabled: isCustomer(user) && !!user.email_verified_at,
   });
 }
