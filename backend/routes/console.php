01ab9d00334cf840e15ca428952eb0b7815a5770
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2026-04-05 22:54:39 +0200

    feat: add sanctum cron job for deleting expired access tokens

--- backend/routes/console.php
+++ backend/routes/console.php
@@ -5,3 +5,4 @@ declare(strict_types=1);
 use Illuminate\Support\Facades\Schedule;
 
 Schedule::command('auth:clear-resets')->everyFifteenMinutes();
+Schedule::command('sanctum:prune-expired --hours=24')->daily();
