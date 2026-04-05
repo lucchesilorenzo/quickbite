db50e2cca804b57085fc2081c6cbf4cf96089304
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2026-04-05 22:59:18 +0200

    feat: add refreshToken service method

---
+++ frontend/src/services/auth.service.ts
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+import env from "@/lib/env";
+import {
+  RefreshTokenPayload,
+  RefreshTokenResponse,
+} from "@/types/auth/auth.api.types";
+
+export async function refreshToken() {
+  const refreshToken = localStorage.getItem("refresh_token");
+
+  if (!refreshToken) {
+    throw new Error("No refresh token available.");
+  }
+
+  const payload: RefreshTokenPayload = {
+    refresh_token: refreshToken,
+  };
+
+  const { data } = await axios.post<RefreshTokenResponse>(
+    `${env.VITE_BASE_URL}/api/auth/refresh`,
+    payload,
+  );
+
+  localStorage.setItem("token", data.token);
+  localStorage.setItem("refresh_token", data.refresh_token);
+
+  return data.token;
+}
