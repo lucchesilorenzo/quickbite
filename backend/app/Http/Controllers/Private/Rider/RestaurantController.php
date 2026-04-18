4506798bd936b893cebce3428fd213c31a9e423f
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2026-04-18 16:00:37 +0200

    feat: add leaveRestaurant method with its service in RestaurantController and update existing getRestaurant service

--- backend/app/Http/Controllers/Private/Rider/RestaurantController.php
+++ backend/app/Http/Controllers/Private/Rider/RestaurantController.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers\Private\Rider;
 
+use App\Exceptions\Private\Rider\ActiveDeliveryException;
+use App\Exceptions\Private\Rider\NoActiveRestaurantException;
 use App\Http\Controllers\Controller;
 use App\Services\Private\Rider\RestaurantService;
 use Dedoc\Scramble\Attributes\Group;
@@ -32,10 +34,42 @@ class RestaurantController extends Controller
                 'message' => 'Restaurant retrieved successfully.',
                 'restaurant' => $restaurant,
             ], 200);
+        } catch (NoActiveRestaurantException $e) {
+            return response()->json([
+                'success' => false,
+                'message' => $e->getMessage(),
+            ], $e->getCode());
         } catch (Throwable) {
             return response()->json([
                 'success' => false,
                 'message' => 'Could not get restaurant.',
+            ], 500);
+        }
+    }
+
+    /**
+     * Leave restaurant.
+     */
+    public function leaveRestaurant(): JsonResponse
+    {
+        try {
+            $this->restaurantService->leaveRestaurant(
+                auth()->user()
+            );
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Restaurant left successfully.',
+            ], 200);
+        } catch (NoActiveRestaurantException|ActiveDeliveryException $e) {
+            return response()->json([
+                'success' => false,
+                'message' => $e->getMessage(),
+            ], $e->getCode());
+        } catch (Throwable) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Could not leave restaurant.',
             ], 500);
         }
     }
